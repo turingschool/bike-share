75b5ee18d0f615690624f101f3d01928d321e95a
BRANDON RANDALL <brandon1985@BRANDONs-MacBook-Pro.local>
2017-01-28 12:47:41 -0700

    adds conditions, models and methods

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -1,3 +1,33 @@
 class BikeShareApp < Sinatra::Base
 
+  get '/conditions' do
+    @conditions = Condition.all#add functionality for chunking by 30 days
+    erb :"conditions/index"
+  end
+
+  get '/conditions/:id' do
+    @condition = Condition.find(params[:id])
+    redirect "/conditions/:id"
+  end
+
+  get '/conditions/new' do
+    @condition = Condition.new
+    erb :"/conditions/new"
+  end
+
+  get '/conditions/:id/edit' do
+    @condition = Condition.find(params[:id])
+    erb :"/conditions/edit"
+  end
+
+  put '/conditions/:id' do
+    @condition = Condition.find(params[:id])
+    redirect "/conditions/#{@condition.id}"
+  end
+
+  delete '/conditions/:id' do
+    @condition = Condition.destroy(params[:id])
+    redirect "/conditions"
+  end
+
 end

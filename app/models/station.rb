d5325642ceff1c4669b6b5375a553c9a838b3a90
Valerie Trudell <v.trudell@protonmail.com>
2017-01-30 19:31:05 -0700

    Adds i_3: total count, average, most dock_count, least dock_count, most recently installed. All in methods

--- app/models/station.rb
+++ app/models/station.rb
@@ -3,4 +3,28 @@ class Station < ActiveRecord::Base
   validates :dock_count, presence: true
   validates :city, presence: true
   validates :installation_date, presence: true
+
+  def self.total_count_of_stations
+    all.count
+  end
+
+  def self.average_bikes_available_per_station
+    average("dock_count").round(2)
+  end
+
+  def self.most_recently_installed_station
+    minimum("installation_date")
+  end
+
+  def self.oldest_station
+    maximum("installation_date")
+  end
+
+  def self.minimum_dock_count
+    minimum("dock_count")
+  end
+
+  def self.maximum_dock_count
+    maximum("dock_count")
+  end
 end

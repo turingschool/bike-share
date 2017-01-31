d81da434927aed6a586694b7c5cdd0b5fd2e717c
Valerie Trudell <v.trudell@protonmail.com>
2017-01-30 17:36:40 -0700

    Populates station table. Iteration 1 & 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,10 @@
 require 'csv'
 require './app/models/station.rb'
 
-
-def import_station_csv
-  CSV.foreach('db/csv/station.csv', :headers=> true) do |row|
-    Station.create!({
-      name: row["name"],
-      doc_count: row["dock_count"],
-      city: row["city"],
-      installation_date: row["installation_date"]})
-  end
+CSV.foreach('db/csv/station.csv', :headers=> true) do |row|
+  Station.create!({
+    name: row["name"],
+    dock_count: row["dock_count"],
+    city: row["city"],
+    installation_date: row["installation_date"]})
 end

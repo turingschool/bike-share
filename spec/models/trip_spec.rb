9280001717b7fbb7145390107a2bb2697f823b32
Valerie Trudell <v.trudell@protonmail.com>
2017-02-01 22:37:48 -0700

    Adds Alexs testing

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,39 +1,33 @@
 require_relative '../spec_helper'
-
 RSpec.describe Trip do
   describe "validations" do
     it "is valid with all requested parameters" do
       trip = Trip.create(duration: 600,
                          start_date: "01/01/2017",
-                         start_station: "Denver",
+                         start_station_id: 17,
                          end_date: "01/01/2017",
-                         end_station: "Lakewood",
-                         trip_bike_id: "444",
+                         end_station_id: 78,
+                         bike_id: 444,
                          subscription: "customer")
-
       expect(trip).to be_valid
     end
-
     it "validades presence of start_date" do
       trip = Trip.create(duration: 600,
                          start_date: "02/01/2017",
-                         start_station: "Thornton",
+                         start_station_id: 56,
                          end_date: "02/01/2017",
-                         end_station: "Denver",
-                         trip_bike_id: "222",
+                         end_station_id: 17,
+                         bike_id: 222,
                          subscription: "subscriber")
-
       expect(trip).to be_valid
     end
-
     it "validades presence of end location" do
       trip = Trip.create(duration: 600,
                          start_date: "02/01/2017",
-                         start_station: "Thornton",
+                         start_station_id: 22,
                          end_date: "02/01/2017",
-                         trip_bike_id: "222",
+                         bike_id: 222,
                          subscription: "subscriber")
-
       expect(trip).to_not be_valid
     end
   end

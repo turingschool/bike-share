6354ed7cfe7a3214a1e3e22744a6e87885335d34
bschwartz10 <bschwartz92@gmail.com>
2017-03-17 16:41:13 -0600

    add data cleanser in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,18 @@ ENV["RACK_ENV"] ||= "test"
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec'
 require 'capybara/dsl'
+require 'database_cleanser'
+
+DatabaseCleanser.strategy = :truncation
 
 Capybara.app = BikeShareApp
 
 RSpec.configure do |c|
+  c.before(:all) do
+    DatabaseCleanser.clean
+  end
+  c.after(:each) do
+    DatabaseCleanser.clean
+  end
   c.include Capybara::DSL
 end

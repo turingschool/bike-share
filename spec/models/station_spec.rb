8917f54982a0123c47c53638975d96a56ef2398d
Sam Landfried <samlandfried@gmail.com>
2017-03-17 17:52:45 -0600

    Pass model tests

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -1,29 +1,29 @@
 require 'Date'
 require_relative '../spec_helper'
 
-Rspec.describe Station do
+RSpec.describe Station do
   describe 'validations' do
 
     it 'is valid with a name, dock count, city and installation date' do
-      station = Station.create(name: 'Turing Station', dock_count: 12, city: 'Denver', installation_date: Date.new('8/6/2013'))
+      station = Station.create(name: 'Turing Station', dock_count: 12, city: 'Denver', installation_date:  Date.parse('8/6/2013'))
 
       expect(station).to be_valid
     end
 
     it 'is invalid if its missing a name' do
-      station = Station.create( dock_count: 12, city: 'Denver', installation_date: Date.new('8/6/2013'))
+      station = Station.create( dock_count: 12, city: 'Denver', installation_date: Date.parse('8/6/2013'))
 
       expect(station).to_not be_valid
     end
 
     it 'is invalid if its missing a dock count' do
-      station = Station.create(name: 'Turing Station', city: 'Denver', installation_date: Date.new('8/6/2013'))
+      station = Station.create(name: 'Turing Station', city: 'Denver', installation_date: Date.parse('8/6/2013'))
 
       expect(station).to_not be_valid
     end
 
     it 'is invalid if its missing a city' do
-      station = Station.create(name: 'Turing Station', dock_count: 12, installation_date: Date.new('8/6/2013'))
+      station = Station.create(name: 'Turing Station', dock_count: 12, installation_date: Date.parse('8/6/2013'))
 
       expect(station).to_not be_valid
     end
@@ -36,7 +36,7 @@ Rspec.describe Station do
 
   end
 
-  describe 'atributes' do
+  describe 'attributes' do
     it 'should have a name, dock count, city and installation date' do
       station = Station.new
 

b9480c827597d8493c45c0ea2b832a67e7c075f3
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-03-18 13:33:13 -0600

    added edit functionality to station

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class BikeShareApp < Sinatra::Base
 
   set :method_override, true 
@@ -9,11 +7,29 @@ class BikeShareApp < Sinatra::Base
     erb :"stations/index"
   end
 
+  get '/stations/new' do
+    erb :"stations/new"
+  end
+
+  post '/stations' do
+    station = Station.new(params[:station])
+    station.save
+    redirect "/stations"
+  end
+
   get '/stations/:id' do
     @station = Station.find(params[:id])
     erb :"stations/show"
   end
 
-  
+  get '/stations/:id/edit' do
+    @station = Station.find(params[:id])
+    erb :"stations/edit"
+  end
+
+  put '/stations/:id' do
+    @station = Station.update(params[:id], params[:station])
+    redirect "/stations/#{@station.id}"
+  end
 
 end

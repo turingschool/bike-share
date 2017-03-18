581c5dd8dac22a305113c7bfbb2cf562c71fe681
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-03-18 15:44:27 -0600

    added user feature tests for dashboard

--- spec/features/users_sees_station_dashboard_spec.rb
+++ spec/features/users_sees_station_dashboard_spec.rb
@@ -1,19 +1,43 @@
 require_relative "../spec_helper"
 
 RSpec.describe "When a user visits '/station-dashboard' " do
-  # it "they see a welcome message" do
-  #   visit '/'
-  #
-  #   within('h1') do
-  #     expect(page).to have_content('Welcome to Bike Share!!!')
-  #   end
-  #
-  #   within('a.bike_index') do
-  #     expect(page).to have_content("All Bike Stations")
-  #   end
-  #
-  #   within('a.new_station') do
-  #     expect(page).to have_content("Create a New Station")
-  #   end
-  # end
+  it "they see a dashboard message" do
+    visit '/station-dashboard'
+
+    within('h1') do
+      expect(page).to have_content('Welcome to the Station Dashboard!')
+    end
+
+    within('div.total_stations') do
+      expect(page).to have_content("Total count of stations:")
+    end
+
+    within('div.average_bikes') do
+      expect(page).to have_content("Average bikes available per station:")
+    end
+
+    within('div.most_bikes') do
+      expect(page).to have_content("Most bikes available at a station:")
+    end
+
+    within('div.stations_with_most_bikes') do
+      expect(page).to have_content("Station(s) where the most bikes are available:")
+    end
+
+    within('div.fewest_bikes') do
+      expect(page).to have_content("Fewest bikes available at a station:")
+    end
+
+    within('div.stations_with_fewest_bikes') do
+      expect(page).to have_content("Station(s) where the fewest bikes are available:")
+    end
+
+    within('div.newest_stations') do
+      expect(page).to have_content("Most recently installed station:")
+    end
+
+    within('div.oldest_stations') do
+      expect(page).to have_content("Oldest station:")
+    end
+  end
 end

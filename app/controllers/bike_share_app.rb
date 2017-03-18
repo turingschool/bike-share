edba0080ce6e21aec117c4313647f4e8bc6d796d
dannyradden <dradden@gmail.com>
2017-03-17 20:05:28 -0600

    finishing CRUD features

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -5,7 +5,29 @@ class BikeShareApp < Sinatra::Base
   end
 
   get '/stations' do
+    @stations = Station.all
     erb :"stations/index"
   end
 
+  get '/stations/:id' do
+    @station = Station.find(params[:id])
+    erb :"stations/show"
+  end
+
+  get '/stations/:id/edit' do
+    @station = Station.find(params[:id])
+    erb :"stations/edit"
+  end
+
+  put '/stations/:id' do
+    station = Station.update(params[:id], params[:station])
+
+    redirect "/stations/#{station.id}"
+  end
+
+  delete '/stations/:id' do
+    Station.destroy(params[:id])
+
+    redirect '/stations'
+  end
 end

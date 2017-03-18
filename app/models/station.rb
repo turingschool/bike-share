95ad738fd9346da250dc268a2095a42bf37cfa49
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-03-18 15:20:23 -0600

    finished station dashboard methods

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,9 +1,65 @@
+require 'pry'
+require 'time'
+require 'date'
+
 class Station < ActiveRecord::Base
   belongs_to :city
-  
+
   validates :name, presence: true, uniqueness: true
   validates :dock_count, presence: true
   validates :city_id, presence: true
   validates :installation_date, presence: true
+
+  def self.total_stations
+    Station.all.count
+  end
+
+  def self.dock_counts
+    Station.all.map(&:dock_count)
+  end
+
+  def self.average_bikes
+    dock_counts.reduce(:+) / total_stations
+  end
+
+  def self.most_bikes
+    dock_counts.max
+  end
+
+  def self.stations_with_most_bikes
+    Station.where(dock_count: most_bikes.to_s)
+  end
+
+  def self.fewest_bikes
+    dock_counts.min
+  end
+
+  def self.stations_with_fewest_bikes
+    Station.where(dock_count: fewest_bikes.to_s)
+  end
+
+  def self.date_converter(string_date)
+    DateTime.strptime(string_date, "%m/%d/%Y")
+  end
+
+  def self.install_dates
+    Station.all.map do |station|
+      date_converter(station.installation_date)
+    end
+  end
+
+  def self.date_finder(picker)
+    Station.all.find_all do |station|
+      date_converter(station.installation_date) == picker
+    end
+  end
+
+  def self.newest_stations
+    date_finder(install_dates.max)
+  end
+
+  def self.oldest_stations
+    date_finder(install_dates.min)
+  end
 
 end
---
+++ spec/models/stations_dashboard_spec.rb
@@ -0,0 +1,69 @@
+require_relative "../spec_helper"
+
+RSpec.describe Station do
+  before :each do
+    city = City.create(name: "Denver")
+    @station1 = city.stations.create(name: "LoDo", dock_count: 10, installation_date: "3/14/2017")
+    @station2 = city.stations.create(name: "Five Points", dock_count: 10, installation_date: "3/15/2017")
+    @station3 = city.stations.create(name: "Capital Hill", dock_count: 4, installation_date: "3/16/2017")
+  end
+
+  describe ".total_stations" do
+    it "returns total number of stations" do
+      expect( Station.total_stations ).to eq(3)
+    end
+  end
+
+  describe ".average_bikes_available" do
+    it "returns array of station dock counts" do
+      expect( Station.dock_counts ).to eq([10, 10, 4])
+    end
+    it "returns average number of bikes available" do
+      expect( Station.average_bikes ).to eq(8)
+    end
+  end
+
+  describe ".most_bikes" do
+    it "returns most bikes available" do
+      expect( Station.most_bikes ).to eq(10)
+    end
+  end
+
+  describe ".stations_with_most_bikes" do
+    it "returns stations with most bikes availale" do
+      expect( Station.stations_with_most_bikes ).to eq([@station1, @station2])
+    end
+  end
+
+  describe ".fewest_bikes" do
+    it "returns fewest bikes available" do
+      expect( Station.fewest_bikes ).to eq(4)
+    end
+  end
+
+  describe ".stations_with_fewest_bikes" do
+    it "returns stations with fewest bikes available" do
+      expect( Station.stations_with_fewest_bikes ).to eq([@station3])
+    end
+  end
+
+  describe ".newest_stations" do
+    it "returns date time object" do
+      expect( Station.date_converter("3/14/2017") ).to eq(DateTime.strptime(@station1.installation_date, "%m/%d/%Y"))
+    end
+    it "returns list of install dates" do
+      expect( Station.install_dates.count ).to eq(3)
+      expect( Station.install_dates.first.class ).to eq(DateTime)
+    end
+    it "returns newest stations" do
+      expect( Station.newest_stations ).to eq([@station3])
+    end
+  end
+
+  describe ".oldest_stations" do
+    it "returns oldest stations" do
+      expect( Station.oldest_stations ).to eq([@station1])
+    end
+  end
+
+end

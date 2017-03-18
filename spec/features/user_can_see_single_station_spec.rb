65cbb1c1d09fe8f5b45dac2a6d54771285c797ef
NatBee <nataliesbarron@gmail.com>
2017-03-17 18:46:08 -0600

    User story for user can see a single station

---
+++ spec/features/user_can_see_single_station_spec.rb
@@ -0,0 +1,13 @@
+require_relative "../spec_helper"
+
+RSpec.describe "when a user visits '/station/:id' " do
+  it "they can see a single station" do
+    # As a user
+    # When I visit /station/:id
+    # I expect to be on the /station/:id page
+    # I expect to see station name for a single station
+    # I expect to see station dock count for a single station
+    # I expect to see station city for a single station
+    # I expect to see station installation date for a single station 
+
+end

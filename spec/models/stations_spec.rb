8578e4b73443ec74e8996bffe4a2e72a46b61286
dannyradden <dradden@gmail.com>
2017-03-17 18:30:50 -0600

    Did 'Station' model tests for validations and attributes

--- spec/models/stations_spec.rb
+++ spec/models/stations_spec.rb
@@ -3,19 +3,48 @@ RSpec.describe Station do
   describe "validations" do
     it "is invalid without a name" do
       station = Station.new(dock_count: 100, city: "Denver", installation_date: "3/14/2017")
-      expect( station ).to_not be_valid
+      expect(station).to_not be_valid
     end
     it "should be valid with a name" do
       station = Station.new(name: "Turing", dock_count: 100, city: "Denver", installation_date: "3/14/2017")
-      expect( station ).to be_valid
+      expect(station).to be_valid
+    end
+
+    it "is invalid without a city" do
+      station = Station.new(name: "Turing", dock_count: 100, installation_date: "3/14/2017")
+      expect(station).to_not be_valid
+    end
+    it "should be valid with a city" do
+      station = Station.new(name: "Turing", dock_count: 100, city: "Denver", installation_date: "3/14/2017")
+      expect(station).to be_valid
+    end
+
+    it "is invalid without a dock count" do
+      station = Station.new(name: "Turing", city: "Denver", installation_date: "3/14/2017")
+      expect(station).to_not be_valid
+    end
+    it "should be valid with a dock count" do
+      station = Station.new(name: "Turing", dock_count: 100, city: "Denver", installation_date: "3/14/2017")
+      expect(station).to be_valid
+    end
+
+    it "is invalid without an installation date" do
+      station = Station.new(name: "Turing", dock_count: 100, city: "Denver")
+      expect(station).to_not be_valid
+    end
+    it "should be valid with an installation date" do
+      station = Station.new(name: "Turing", dock_count: 100, city: "Denver", installation_date: "3/14/2017")
+      expect(station).to be_valid
     end
   end
-  # describe "attributes" do
-  #   it "should have name, age, and total_winnings" do
-  #     horse = Horse.new
-  #     expect(horse).to respond_to(:name)
-  #     expect(horse).to respond_to(:age)
-  #     expect(horse).to respond_to(:total_winnings)
-  #   end
-  # end
+
+  describe "attributes" do
+    it "should have name, city, dock count, and an installation date" do
+      station = Station.new
+      expect(station).to respond_to(:name)
+      expect(station).to respond_to(:city)
+      expect(station).to respond_to(:dock_count)
+      expect(station).to respond_to(:installation_date)
+    end
+  end
 end

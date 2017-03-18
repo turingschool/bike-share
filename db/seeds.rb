8c53cad06e4ae22a09f832c6228e3103ae28f1bc
glassjoseph <glassjoseph@gmail.com>
2017-03-18 17:57:33 -0600

    populated seed

---
+++ db/seeds.rb
@@ -0,0 +1,31 @@
+require './app/models/city'
+require './app/models/station'
+require 'pry'
+require 'csv'
+
+# binding.pry
+
+CSV.foreach("db/csv/station.csv", headers: true, header_converters: :symbol) do |row|
+
+  row = row.to_hash
+  puts row
+    # if row[:id] == "22"
+      # binding.pry
+    # end
+  row.delete(:id)
+  row.delete(:lat)
+  row.delete(:long)
+  row[:installation_date] = Date.strptime(row[:installation_date], "%m/%d/%y")
+  #create city, get id, pass in
+  City.create(name: row[:city])
+  row[:city_id] = City.find_by(name: row[:city]).id
+  row.delete(:city)
+  Station.create(row)
+  puts Station.all.last.name
+  puts Station.all.last.city
+end
+
+
+
+
+""

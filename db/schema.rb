fabd2275cec685d1955233397aa66f6826e868ab
jk1dd <portajonk@gmail.com>
2017-03-18 13:02:50 -0600

    wip add migration for cities

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170317003831) do
+ActiveRecord::Schema.define(version: 20170318175920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.text "name"
+  end
 
   create_table "stations", force: :cascade do |t|
     t.text    "name"

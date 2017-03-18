2dfe73297a538e3a4bcf9fec0138a10d3a85eb8e
NatBee <nataliesbarron@gmail.com>
2017-03-18 15:11:01 -0600

    created a view and built controller for all stations

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -1,3 +1,9 @@
 class BikeShareApp < Sinatra::Base
 
+  get "/stations" do
+    @stations = Station.all
+
+    erb :"/stations/index"
+  end
+
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -1,6 +1,7 @@
 class Station < ActiveRecord::Base
   belongs_to :city
 
-  validates :name, :dock_count, :installation_date, presence: true
+  validates :name, presence: true, uniqueness: true
+  validates :dock_count, :installation_date, presence: true
 
 end

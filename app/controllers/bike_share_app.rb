bc4ab43e1bf098c93e45097585aaf56357a5bb08
somedayrainbows <ebassity@gmail.com>
2017-03-18 16:49:26 -0600

    passed feature test for see single station

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -2,8 +2,12 @@ class BikeShareApp < Sinatra::Base
 
   get "/stations" do
     @stations = Station.all
-
     erb :"stations/index"
+  end
+
+  get "/stations/:id" do
+    @station = Station.find(params[:id])
+    erb :"stations/show"
   end
 
 end

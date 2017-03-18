56f1228fa5085435a81c31338cbc016123ba3534
Ken Lee <wewert@gmail.com>
2017-03-18 16:32:57 -0600

    made dashboard and index file and routes for them

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -1,3 +1,12 @@
 class BikeShareApp < Sinatra::Base
+  set :root, File.expand_path("..", __dir__)
 
+  get '/' do
+    erb :dashboard
+  end
+
+  get '/stations' do
+    @stations = ["station1"]
+    erb :index
+  end
 end
--- config.ru
+++ config.ru
@@ -1,6 +1,8 @@
 require 'bundler'
 Bundler.require
 
+$LOAD_PATH.unshift(File.expand_path("app", __dir__))
+
 require File.expand_path('../config/environment',  __FILE__)
 
 run BikeShareApp

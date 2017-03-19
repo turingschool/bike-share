c9ad9db932ccedfa821ab101f7cce979aeca2664
lukyans <sukyans@gmail.com>
2017-03-19 15:29:04 -0600

    adding model test

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,8 @@ open_contents = CSV.open('./db/csv/station.csv', headers: true, header_converter
 
 
 open_contents.each do |row|
+  date = row[:installation_date]
+  row[:installation_date] = Date.strptime(date, '%m/%d/%Y')
   Station.create!(name: row[:name],
                   dock_count: row[:dock_count],
                   city: row[:city], installation_date: row[:installation_date])

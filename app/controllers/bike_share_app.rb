ba25d5f6599459bdbe28e56bc5f8f514e1b8dfaf
slague <stephanielague@gmail.com>
2017-03-18 21:20:14 -0600

    added super basic homepage, began I4 CRUD

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -3,7 +3,7 @@ require 'pry'
 class BikeShareApp < Sinatra::Base
 
   get '/' do
-    "Hi"
+    erb :dashboard
   end
 
   get '/stations' do
@@ -46,4 +46,44 @@ class BikeShareApp < Sinatra::Base
     @station = Station.destroy(params[:id])
     redirect '/stations'
   end
+
+#TRIPS STARTS HERE (ITERATION 4)
+  get '/trips' do
+    @trips = Trip.all
+    erb :"trips/index"
+  end
+
+  get '/trips/new' do
+    erb :"trips/new"
+  end
+
+#PROBLEM: LINKS WORK and FORM APPEARS, DOES NOT SAVE NEW ENTRIES
+  post '/trips' do
+    @trip = Trip.create(params[:trip])
+    redirect "/trips"
+  end
+
+  get '/trips/:id/edit' do
+    @trip = Trip.find(params[:id])
+    erb :'trips/edit'
+  end
+
+  put '/trips/:id' do
+    @trip = Trip.update(params[:id], params[:trip])
+    redirect "/trips/#{@trip.id}"
+  end
+
+  get '/trips/:id' do
+    @trip = Trip.find(params[:id])
+    erb :"trips/show"
+  end
+
+#WORKING :)
+  delete '/trips/:id' do
+    @trip = Trip.destroy(params[:id])
+    redirect '/trips'
+  end
+
+
+
 end

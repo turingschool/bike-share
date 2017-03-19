fe6b2ca7a8b7450d3eb161bcd6b3d7400545ee45
NatBee <nataliesbarron@gmail.com>
2017-03-18 21:40:14 -0600

    seeded our stations db

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,20 +2,20 @@ require 'csv'
 require_relative '../app/models/city'
 require_relative '../app/models/station'
 require 'database_cleaner'
+require 'date'
 
 DatabaseCleaner.strategy = :truncation
 DatabaseCleaner.clean
 
-contents = CSV.open("./csv/station.csv", headers: true, header_converters: :symbol)
+contents = CSV.open("db/csv/station.csv", headers: true, header_converters: :symbol)
 
 contents.each do |row|
+  city = City.find_or_create_by(city: row[:city])
 
-  city = City.find_or_create_by(row[:city])
-
+  # binding.pry
   Station.create(name: row[:name],
                  dock_count: row[:dock_count],
-                 city: row[:city],
-                 installation_date: Date.strptime(row[:installation_date]),
+                 installation_date: Date.strptime(row[:installation_date], "%m/%d/%y").to_s,
                  city_id: city.id
                  )
 

647a9c0d11acad0ce131e2b3d6366cb8bf6f7032
somedayrainbows <ebassity@gmail.com>
2017-03-18 18:02:00 -0600

    passed feature test for user can create a new station

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -5,6 +5,18 @@ class BikeShareApp < Sinatra::Base
     erb :"stations/index"
   end
 
+  get "/stations/new" do
+    @station = Station.new
+    @city = City.all
+    erb :"stations/new"
+  end
+
+  post "/stations" do
+    @station = Station.create(params[:station])
+    redirect "/stations"
+  end
+
+
   get "/stations/:id" do
     @station = Station.find(params[:id])
     erb :"stations/show"

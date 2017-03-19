5f0bb3005d13a64b84db311277061be0452563be
Sam Landfried <samlandfried@gmail.com>
2017-03-19 09:41:40 -0600

    Write seed file to work with new cities table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
 require './app/models/station.rb'
+require './app/models/city.rb'
 
-Station.create!(lat: 37.329732, long: -121.90178200000001, name: 'Turing Station', dock_count: 12, city_id: 1, installation_date: Date.parse('8/6/2013'))
+city = City.create!(name: 'Denver')
+city.stations.create!(lat: 37.329732, long: -121.90178200000001, name: 'Turing Station', dock_count: 12, installation_date: Date.parse('8/6/2013'))

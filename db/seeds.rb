af4c7425523f0e424ec434de0c96660c88c7759b
Beth Knight <bethknight1234@gmail.com>
2017-03-19 12:52:47 -0600

    seeded db with csv files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,22 @@
 require './app/models/station.rb'
 require './app/models/city.rb'
 require 'CSV'
+require 'Date'
 
  CSV.foreach("./db/csv/station.csv", :headers => true) do |row|
 
-  city_name = row['city']
+  city_name = row["city"]
+  date = row["installation_date"]
+
+
+  row['installation_date'] = Date.strptime(date, '%m/%d/%Y')
+
 
   city = City.find_or_create_by(name: city_name)
 
   row.delete("city")
   row.delete("id")
   city.stations.create!(row.to_h)
+
 
 end

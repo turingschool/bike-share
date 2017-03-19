b2d1e20b48de05c453257a5c237cf128f8d5c5d0
ryt11 <rileyt_2000@hotmail.com>
2017-03-19 16:14:45 -0600

    add station model test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require './app/models/station.rb'
+require 'pry'
 
 
 Station.destroy_all
@@ -9,6 +10,7 @@ open_contents = CSV.open('./db/csv/station.csv', headers: true, header_converter
 open_contents.each do |row|
   date = row[:installation_date]
   row[:installation_date] = Date.strptime(date, '%m/%d/%Y')
+  binding.pry
   Station.create!(name: row[:name],
                   dock_count: row[:dock_count],
                   city: row[:city], installation_date: row[:installation_date])

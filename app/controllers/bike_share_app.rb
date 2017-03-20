a517e846cbb4f026b100bf076234db97717be125
bschwartz10 <bschwartz92@gmail.com>
2017-03-20 17:10:14 -0600

    update trip model with update_trip and create_trip

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -30,12 +30,10 @@ class BikeShareApp < Sinatra::Base
   put '/stations/:id' do
     Station.update_station(params)
     redirect "/stations/#{params[:id]}"
-
   end
 
   delete '/stations/:id' do
     Station.destroy(params[:id])
-
     redirect '/stations'
   end
 
@@ -45,15 +43,7 @@ class BikeShareApp < Sinatra::Base
   end
 
   post '/trips' do
-    # require "pry"; binding.pry
-    start_station = params[:trip][:start_station]
-    end_station = params[:trip][:end_station]
-    subscription_type = params[:trip][:subscription_type]
-    zip_code = ZipCode.find_or_create_by(zip_code: params[:trip][:zip_code]).id
-    bike = Bike.find_by(bike_number: params[:trip][:bike_number]).id
-
-    @trip = Trip.create(duration: params[:trip][:duration], start_date: params[:trip][:start_date], start_station_id: start_station, end_date: params[:trip][:end_date], end_station_id: end_station, subscription_type_id: subscription_type, zip_code_id: zip_code, bike_id: bike)
-
+    @trip = Trip.create_trip(params)
     redirect "/trips/#{@trip.id}"
   end
 
@@ -69,16 +59,7 @@ class BikeShareApp < Sinatra::Base
   end
 
   put '/trips/:id' do
-    start_station = params[:trip][:start_station]
-    end_station = params[:trip][:end_station]
-    subscription_type = params[:trip][:subscription_type]
-    zip_code = ZipCode.find_or_create_by(zip_code: params[:trip][:zip_code]).id
-    bike = Bike.find_by(bike_number: params[:trip][:bike_number]).id
-
-
-    input = {duration: params[:trip][:duration], start_date: params[:trip][:start_date], start_station_id: start_station, end_date: params[:trip][:end_date], end_station_id: end_station, subscription_type_id: subscription_type, zip_code_id: zip_code, bike_id: bike}
-    Trip.find(params[:id]).update(input)
-
+    Trip.update_trip(params)
     redirect "/trips/#{params[:id]}"
   end
 

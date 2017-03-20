db1c8747032ccdbd9f0d935beb9f2134af3e49f7
ryt11 <rileyt_2000@hotmail.com>
2017-03-20 17:35:34 -0600

    add iteration 3

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -1,17 +1,28 @@
 require 'pry'
 
 class BikeShareApp < Sinatra::Base
+    set :method_override, true
 
   get '/' do
     erb :"home/index"
+  end
+
+  get '/station-dashboard' do
+    @station_count = Station.station_count
+    @bike_average = Station.bike_average
+    @most_bikes = Station.most_bikes
+    @stations_with_most_bikes = Station.top_bikes
+    @few_bikes = Station.few_bikes
+    @stations_with_fewest_bikes = Station.fewest_bikes
+    @newest_station = Station.newest_station
+    @oldest_station = Station.oldest_station
+    erb :"station/dashboard"
   end
 
   get '/stations' do
     @stations = Station.all
     erb :"station/index"
   end
-
-
 
   get '/stations/new' do
     erb :"station/new"
@@ -32,6 +43,17 @@ class BikeShareApp < Sinatra::Base
     @station = Station.find(params[:id])
 
     erb :"station/edit"
+  end
+
+  put '/stations/:id' do
+    @station = Station.update(params[:id].to_i, params[:station])
+    redirect '/stations'
+    #redirect '/stations/#{@station.id}' isn't working
+  end
+
+  delete '/stations/:id' do
+    @station = Station.destroy(params[:id])
+    redirect '/stations'
   end
 
 end
--- spec/features/user_can_view_station_spec.rb
+++ spec/features/user_can_view_station_spec.rb
@@ -2,19 +2,31 @@ require_relative '../spec_helper'
 require 'pry'
 
 RSpec.describe "A user can view station" do
+  before(:each) {Station.create(name:'San Jose Diridon Caltrain Station', city: "San Jose", dock_count: 27, installation_date: "8/6/2013")}
   it "and has all content" do
-    Station.create(name:'San Jose Diridon Caltrain Station', city: "San Jose", dock_count: 27, installation_date: "8/6/2013")
     visit '/stations/1'
-    expect(page.all('li')[0].text).to eq("City: San Jose")
-    expect(page.all('li')[1].text).to eq("Dock Count: 27")
-    expect(page.all('li')[2].text).to eq("Installation Date: 2013-06-08")
+    within("ul li:nth-child(1)") do
+      expect(page).to have_content("City: San Jose")
+    end
+    within("ul li:nth-child(2)") do
+      expect(page).to have_content("Dock Count: 27")
+    end
+    within("ul li:nth-child(3)") do
+      expect(page).to have_content("Installation Date: 2013-06-08")
+    end
   end
 
   it "has an edit button" do
-    Station.create(name:'San Jose Diridon Caltrain Station', city: "San Jose", dock_count: 27, installation_date: "8/6/2013")
     visit '/stations/1'
     expect(page).to have_button('Edit')
     page.click_link('', :href => '/stations/1/edit')
     expect(current_path).to eq('/stations/1/edit')
+  end
+
+  it "has an delete button" do
+    visit '/stations/1'
+    expect(page).to have_button('Delete')
+    click_on "Delete"
+    expect(current_path).to eq('/stations')
   end
 end

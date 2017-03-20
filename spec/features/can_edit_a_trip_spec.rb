42d434113d8a1bfb44f9e9e7a1aab4e708d8b288
jk1dd <portajonk@gmail.com>
2017-03-20 16:03:43 -0600

    fix trip tests

--- spec/features/can_edit_a_trip_spec.rb
+++ spec/features/can_edit_a_trip_spec.rb
@@ -2,14 +2,14 @@ require_relative "../spec_helper"
 
 RSpec.describe "Editing a trip from all trips index" do
   it "Can click button and be taken to corresponding page" do
+    City.create(name: "Denver")
+    Station.create(name: "Downtown", installation_date: "20140331", city_id: "1", dock_count: 4)
     Trip.create(duration: 103,
                 start_date: "2013-08-09",
-                start_station_id: 59,
-                start_station_name: "Golden Gate at Polk",
+                start_station_id: 1,
                 end_date: "2013-08-09",
                 bike_id: 527,
-                end_station_name: "Golden Gate at Polk",
-                end_station_id: 59,
+                end_station_id: 1,
                 subscription_type: "Subscriber"
                 )
 
@@ -21,13 +21,14 @@ RSpec.describe "Editing a trip from all trips index" do
   end
 
   it "Can edit a trip's attributes" do
+    City.create(name: "Denver")
+    Station.create(id: 59, name: "Downtown", installation_date: "20140331", city_id: "1", dock_count: 4)
+    Station.create(id: 34, name: "Anything", installation_date: "20140331", city_id: "1", dock_count: 4)
     Trip.create(duration: 103,
                 start_date: "2013-08-09",
                 start_station_id: 59,
-                start_station_name: "Golden Gate at Polk",
                 end_date: "2013-08-09",
                 bike_id: 527,
-                end_station_name: "Golden Gate at Polk",
                 end_station_id: 59,
                 subscription_type: "Subscriber"
                 )
@@ -35,10 +36,10 @@ RSpec.describe "Editing a trip from all trips index" do
     visit "/trips/1/edit"
 
     fill_in "trip[start_date]", with: "2013-08-09"
-    fill_in "trip[start_station_name]", with: "Downtown"
+    fill_in "start_station[name]", with: "Anything"
     fill_in "trip[duration]", with: 45
     fill_in "trip[end_date]", with: "2013-08-09"
-    fill_in "trip[end_station_name]", with: "Downtown"
+    fill_in "end_station[name]", with: "Downtown"
     fill_in "trip[bike_id]", with: 345
     fill_in "trip[subscription_type]", with: "Subscriber"
     fill_in "trip[zip_code]", with: 53211
@@ -47,6 +48,6 @@ RSpec.describe "Editing a trip from all trips index" do
 
     expect(current_path).to eq("/trips/1")
 
-    expect(page).to have_content("Downtown")
+    expect(page).to have_content("Anything")
   end
 end

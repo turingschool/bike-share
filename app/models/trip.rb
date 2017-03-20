e996a45fb3212330fc86d2bc09de76cbdb791161
Christopher Dale <christopher.john.dale3@gmail.com>
2017-03-20 17:13:28 -0600

    Add start/end date attrs to before

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -7,4 +7,5 @@ class Trip < ActiveRecord::Base
 
   validates :duration, presence: true
   validates :start_date, presence: true
+  validates :end_date, presence: true
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -2,10 +2,17 @@ require_relative '../spec_helper'
 
 RSpec.describe Trip do
 
+  attr_reader :start_date, :end_date
+
+  before do
+    @start_date = DateTime.strptime('8/29/2013 14:01', '%m/%d/%Y %k:%M')
+    @end_date = DateTime.strptime('8/29/2013 14:23', '%m/%d/%Y %k:%M')
+  end
+
   describe 'validations' do
     it 'is valid with a duration, start_date, end_date, subscription_type, bike_id, start_station_id, end_station_id and weather_id' do
 
-      trip = Trip.create(duration: 22, start_date: DateTime.strptime('8/29/2013 14:01', '%m/%d/%Y %k:%M'), end_date:DateTime.strptime('8/29/2013 14:23', '%m/%d/%Y %k:%M'), subscription_type: 'subscriber', bike_id: 1, start_station_id: 1, end_station_id: 2, weather_id: 1)
+      trip = Trip.create(duration: 22, start_date: start_date, end_date: end_date, subscription_type: 'subscriber', bike_id: 1, start_station_id: 1, end_station_id: 2, weather_id: 1)
 
       expect(trip).to be_valid
     end
@@ -17,7 +24,13 @@ RSpec.describe Trip do
     end
 
     it 'is invalid if its missing start date' do
-      trip = Trip.create(duration: 22, end_date: '8/29/2013 14:23', subscription_type: 'subscriber', bike_id: 1, start_station_id: 1, end_station_id: 2, weather_id: 1)
+      trip = Trip.create(duration: 22, end_date: end_date, subscription_type: 'subscriber', bike_id: 1, start_station_id: 1, end_station_id: 2, weather_id: 1)
+
+      expect(trip).to_not be_valid
+    end
+
+    it 'is invalid if its missing end date' do
+      trip = Trip.create(duration: 22, start_date: start_date, subscription_type: 'subscriber', bike_id: 1, start_station_id: 1, end_station_id: 2, weather_id: 1)
 
       expect(trip).to_not be_valid
     end

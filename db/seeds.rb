0dd33f69a964263454f4c5300825b8b1e1617a1e
dannyradden <dradden@gmail.com>
2017-03-20 15:04:59 -0600

    Finished cleaning zip codes

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,7 @@ DatabaseCleaner.strategy = :truncation
 DatabaseCleaner.clean
 
 @count = 0
+
 def clean_station(name)
   if Station.find_by(name: name) != nil
     Station.find_by(name: name).id
@@ -27,6 +28,13 @@ def clean_station(name)
   end
 end
 
+def clean_zipcode(zip_code)
+  return nil if zip_code.nil? || zip_code.length < 3
+  zip_code = zip_code[0..4]
+  ZipCode.find_or_create_by(zip_code: zip_code).id
+end
+
+
 CSV.foreach "db/csv/station.csv", headers: true, header_converters: :symbol do |row|
     city = City.find_or_create_by(name: row[:city])
 
@@ -38,11 +46,11 @@ CSV.foreach "db/csv/station.csv", headers: true, header_converters: :symbol do |
     )
 end
 
-CSV.foreach "db/csv/trip_fixture.csv", headers: true, header_converters: :symbol do |row|
+CSV.foreach "db/fixture_csv/trip_fixture.csv", headers: true, header_converters: :symbol do |row|
     @count += 1
     bike = Bike.find_or_create_by(bike_number: row[:bike_id])
     subscription_type = SubscriptionType.find_or_create_by(subscription_type: row[:subscription_type])
-    zip_code = ZipCode.find_or_create_by(zip_code: row[:zip_code])
+    zip_code = clean_zipcode(row[:zip_code])
     start_station = clean_station(row[:start_station_name])
     end_station = clean_station(row[:end_station_name])
 
@@ -53,7 +61,7 @@ CSV.foreach "db/csv/trip_fixture.csv", headers: true, header_converters: :symbol
       end_date:             row[:end_date],
       end_station_id:       end_station,
       bike_id:              bike.id,
-      zip_code_id:          zip_code.id,
+      zip_code_id:          zip_code,
       subscription_type_id: subscription_type.id
     )
     p "Creating Trip Number #{@count} with start station #{row[:start_station_name]} "

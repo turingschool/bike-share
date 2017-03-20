b20ec764721f8f6d8e404b6396300140a6d59aff
Christopher Dale <christopher.john.dale3@gmail.com>
2017-03-20 17:07:39 -0600

    tested start date presence

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Trip do
   describe 'validations' do
     it 'is valid with a duration, start_date, end_date, subscription_type, bike_id, start_station_id, end_station_id and weather_id' do
 
-      trip = Trip.create(duration: 22, start_date: '8/29/2013 14:01', end_date: '8/29/2013 14:23', subscription_type: 'subscriber', bike_id: 1, start_station_id: 1, end_station_id: 2, weather_id: 1)
+      trip = Trip.create(duration: 22, start_date: DateTime.strptime('8/29/2013 14:01', '%m/%d/%Y %k:%M'), end_date:DateTime.strptime('8/29/2013 14:23', '%m/%d/%Y %k:%M'), subscription_type: 'subscriber', bike_id: 1, start_station_id: 1, end_station_id: 2, weather_id: 1)
 
       expect(trip).to be_valid
     end

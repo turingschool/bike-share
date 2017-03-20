78ea205c93ac1227028eb095596c193d36cd3274
NatBee <nataliesbarron@gmail.com>
2017-03-19 21:48:27 -0600

    started code for seeding the trip database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 require 'csv'
 require_relative '../app/models/city'
 require_relative '../app/models/station'
+require_relative '../app/models/trip'
+require_relative '../app/models/zipcode'
 require 'database_cleaner'
 require 'date'
 
@@ -17,6 +19,22 @@ contents.each do |row|
                  dock_count: row[:dock_count],
                  installation_date: Date.strptime(row[:installation_date], "%m/%d/%y").to_s,
                  city_id: city.id
+                 )
+
+end
+
+contents.each do |row|
+  zipcode = Zipcode.find_or_create_by(zipcode: row[:zipcode])
+
+  # binding.pry
+  Trip.create(duration: row[:duration],
+              start_date: row[:start_date],
+              start_station_id: row[:start_station_id],
+              end_date: row[:end_date],
+              end_station_id: row[:end_station_id],
+              bike_id: row[:bike_id],
+              subscription_type: row[:subscription_type],
+              zipcode_id: zipcode.id
                  )
 
 end

cb9dc22670e7ccea696bc71c6e31b60d1ebd2e1d
dannyradden <dradden@gmail.com>
2017-03-20 10:01:07 -0600

    Working on updating seed with trip data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,24 @@
 require 'csv'
 require './app/models/station'
 require './app/models/city'
+require './app/models/bike'
+require './app/models/subscription_type'
+require './app/models/trip'
+require './app/models/zip_code'
 require 'database_cleaner'
 
 DatabaseCleaner.strategy = :truncation
 DatabaseCleaner.clean
+  @bad_array = []
+
+def clean_station(name)
+  if Station.find_by(name: name) != nil
+    Station.find_by(name: name).id
+  else
+    @bad_array << name
+    1
+  end
+end
 
 CSV.foreach "db/csv/station.csv", headers: true, header_converters: :symbol do |row|
     city = City.find_or_create_by(name: row[:city])
@@ -16,4 +30,25 @@ CSV.foreach "db/csv/station.csv", headers: true, header_converters: :symbol do |
         installation_date:  row[:installation_date]
     )
 end
-#our city_id is coming in as a string in our migraton
+@count = 0
+CSV.foreach "db/csv/trip.csv", headers: true, header_converters: :symbol do |row|
+    @count += 1
+    bike = Bike.find_or_create_by(bike_number: row[:bike_id])
+    subscription_type = SubscriptionType.find_or_create_by(subscription_type: row[:subscription_type])
+    zip_code = ZipCode.find_or_create_by(zip_code: row[:zip_code])
+    start_station = clean_station(row[:start_station_name])
+    end_station = clean_station(row[:end_station_name])
+
+    Trip.create(
+      duration:             row[:duration],
+      start_date:           row[:start_date],
+      start_station_id:     start_station,
+      end_date:             row[:end_date],
+      end_station_id:       end_station,
+      bike_id:              bike.id,
+      zip_code_id:          zip_code.id,
+      subscription_type_id: subscription_type.id
+    )
+    p @count
+    p @bad_array
+end

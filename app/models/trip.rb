c1053c729dcacea09abe554dda5716e38bc9efcd
somedayrainbows <ebassity@gmail.com>
2017-03-21 15:54:18 -0600

    updating trip and station models to reflect non-join-table connections to each other

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,8 @@
 class Trip < ActiveRecord::Base
     has_many :zipcodes
-    has_many :stations
+
+    belongs_to :start_station, class_name: 'Station', foreign_key: :start_station_id
+    belongs_to :end_station, class_name: 'Station', foreign_key: :end_station_id
 
     # validates :duration, :start_date, :start_station_id, :end_date, :end_station_id, :bike_id, :subscription_type, presence: true
 

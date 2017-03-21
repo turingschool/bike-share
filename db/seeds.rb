c4abf36aee51051e2f6d0056b9882f39f000d3de
NatBee <nataliesbarron@gmail.com>
2017-03-21 09:11:28 -0600

    seeded trips with fixture file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,8 +15,8 @@ contents = CSV.open("db/csv/station.csv", headers: true, header_converters: :sym
 contents.each do |row|
   city = City.find_or_create_by(city: row[:city])
 
-  # binding.pry
-  Station.create(name: row[:name],
+
+  Station.create(name: row[:name] ,
                  dock_count: row[:dock_count],
                  installation_date: Date.strptime(row[:installation_date], "%m/%d/%y").to_s,
                  city_id: city.id
@@ -31,11 +31,10 @@ trips.each do |row|
 
   subscription = Subscription.find_or_create_by(subscription: row[:subscription])
 
-
   Trip.create(duration: row[:duration],
-              start_date: row[:start_date],
+              start_date: DateTime.strptime(row[:start_date], "%m/%d/%Y %H:%M").to_s,
               start_station_id: row[:start_station_id],
-              end_date: row[:end_date],
+              end_date: DateTime.strptime(row[:end_date], "%m/%d/%Y %H:%M").to_s,
               end_station_id: row[:end_station_id],
               bike_id: row[:bike_id],
               subscription_type: row[:subscription_type],

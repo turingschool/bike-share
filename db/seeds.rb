f1d49ded210ee858e82d149444cefb43b1e5fcb9
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-03-21 11:45:49 -0600

    removed comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,8 +13,6 @@ DatabaseCleaner.clean
 stations = (CSV.open'db/csv/station.csv', headers: true, header_converters: :symbol)
 
 stations.each do |station|
-  # city = City.find_or_create_by(name: station[:city])
-
   Station.create(name:              station[:name],
                  dock_count:        station[:dock_count].to_i,
                  installation_date: Date.strptime(station[:installation_date], "%m/%d/%Y"),

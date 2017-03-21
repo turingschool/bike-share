5c507e629f6f59d0f84ea9dec4e2d9c00c8f3fc0
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-03-21 11:45:00 -0600

    refactored even more

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,12 +13,12 @@ DatabaseCleaner.clean
 stations = (CSV.open'db/csv/station.csv', headers: true, header_converters: :symbol)
 
 stations.each do |station|
-  city = City.find_or_create_by(name: station[:city])
+  # city = City.find_or_create_by(name: station[:city])
 
   Station.create(name:              station[:name],
                  dock_count:        station[:dock_count].to_i,
                  installation_date: Date.strptime(station[:installation_date], "%m/%d/%Y"),
-                 city_id:           city.id)
+                 city:              City.find_or_create_by(name: station[:city]))
 end
 
 trips = (CSV.open'db/fixtures/trips_fixture.csv', headers: true, header_converters: :symbol)

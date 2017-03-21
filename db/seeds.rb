b778f3b7b0656a15bf0ee7ff9b180f6124a032e7
Sam Landfried <samlandfried@gmail.com>
2017-03-21 16:29:06 -0600

    Merge iteration_4

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,8 +23,8 @@ require 'Date'
 start_date = DateTime.strptime('8/29/2013 14:01', '%m/%d/%Y %k:%M')
 end_date = DateTime.strptime('8/29/2013 14:23', '%m/%d/%Y %k:%M')
 
-  31.times do |i|
-    Station.create(lat: 37.329732, long: -121.90178200000001, name: "Start Dummy \##{i + 1}", dock_count: 12, city_id: 1, installation_date: Date.parse('8/6/2013'))
-    Station.create(lat: 37.329732, long: -121.90178200000001, name: "End Dummy \##{i + 1}", dock_count: 12, city_id: 1, installation_date: Date.parse('8/6/2013'))
-    Trip.create(duration: 22, start_date: start_date, end_date: end_date, subscription_type: 'subscriber', bike_id: 1, start_station_id: (i * 2) + 1, end_station_id: (i + 1) * 2, weather_id: 1)
-  end
+31.times do |i|
+  Station.create(lat: 37.329732, long: -121.90178200000001, name: "Start Dummy \##{i + 1}", dock_count: 12, city_id: 1, installation_date: Date.parse('8/6/2013'))
+  Station.create(lat: 37.329732, long: -121.90178200000001, name: "End Dummy \##{i + 1}", dock_count: 12, city_id: 1, installation_date: Date.parse('8/6/2013'))
+  Trip.create(duration: 22, start_date: start_date, end_date: end_date, subscription_type: 'subscriber', bike_id: 1, start_station_id: (i * 2) + 1, end_station_id: (i + 1) * 2, weather_id: 1)
+end

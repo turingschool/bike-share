b9180971a8793e1d3e2b2fca52b4221ab88d9cbf
stovermc <stovermc@gmail.com>
2017-03-20 22:46:40 -0600

    test not quiet passing for trips index.

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -1,5 +1,9 @@
+require_relative 'pagination'
+
 class BikeShareApp < Sinatra::Base
   set :method_override, true
+
+  include Pagination
 
   get "/" do
     redirect "/station_dashboard"
@@ -70,31 +74,33 @@ class BikeShareApp < Sinatra::Base
   end
 
   get "/trips/page/:num" do |page_num|
-!    trips     = Trip.where.not(start_date: nil)
-    @trips    = on_page(trips, page_num)
-    @next     = next_page(page_num, trips.count)
-    @previous = previous_page(page_num)
+    trips     = Trip.where.not(start_date: nil)
+    @trips    = on_page(trips, page_num.to_i)
+    @next     = next_page(page_num.to_i, trips.count)
+    @previous = previous_page(page_num.to_i)
+
+    erb :"trips/index"
   end
 
   private
 
-  ITEMS_PER_PAGE = 30
-
-  def on_page(items, page_num)
-    items.limit(ITEMS_PER_PAGE).offset((page_num - 1) * ITEMS_PER_PAGE)
-  end
-
-  def total_pages(item_count)
-    (item_count / ITEMS_PER_PAGE).ceil
-  end
-
-  def next_page(page_num, item_count)
-    page_num + 1 unless page_num <= total_pages(item_count)
-  end
-
-  def previous_page(page_num)
-    page_num - 1 unless page_num <= 1
-  end
+  # ITEMS_PER_PAGE = 30
+  #
+  # def on_page(items, page_num)
+  #   items.limit(ITEMS_PER_PAGE).offset((page_num - 1) * ITEMS_PER_PAGE)
+  # end
+  #
+  # def total_pages(item_count)
+  #   (item_count / ITEMS_PER_PAGE).ceil
+  # end
+  #
+  # def next_page(page_num, item_count)
+  #   page_num + 1 unless page_num <= total_pages(item_count)
+  # end
+  #
+  # def previous_page(page_num)
+  #   page_num - 1 unless page_num <= 1
+  # end
 
 
 end
--- spec/features/trip/user_can_see_all_trips_spec.rb
+++ spec/features/trip/user_can_see_all_trips_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "when a user visits '/trips' " do
   it "they see welcome message" do
     visit "/trips"
 
-    expect(page).to eq("/trips")
+    expect(page).to eq("/trips/page/1")
     expect(page).to have_content("All Trips")
   end
 
@@ -18,25 +18,51 @@ RSpec.describe "when a user visits '/trips' " do
     expect(current_path).to eq("/trips/page/1")
 
 
-    expect(page).to have_content("All Trips:")
-    expect(page).to have_content("Duration:")
-    expect(page).to have_content("Start Date:")
-    expect(page).to have_content("Start Station:")
-    expect(page).to have_content("End Date:")
-    expect(page).to have_content("End Station:")
-    expect(page).to have_content("Bike ID:")
-    expect(page).to have_content("Subscription ID:")
-    expect(page).to have_content("Zipcode ID:")
+    expect(page).to have_content("All Trips")
+    expect(page).to have_content("Duration")
+    expect(page).to have_content("Start Date")
+    expect(page).to have_content("Start Station")
+    expect(page).to have_content("End Date")
+    expect(page).to have_content("End Station")
+    expect(page).to have_content("Bike ID")
+    expect(page).to have_content("Subscription ID")
+    expect(page).to have_content("Zipcode ID")
   end
 
-  it "user sees first 30" do
+  it "and sees 30 trips on each page" do
 
     start_station = Station.create(name: "Denver", dock_count: 15, city_id: 1, installation_date: "12/05/1987")
     end_station = Station.create(name: "Boulder", dock_count: 14, city_id: 2, installation_date: "12/05/1987")
 
-    trip = start_station.trips.create(duration: 123, start_date: "12/05/1987",
-    start_station_id: 1, end_date: "12/05/1987",
-    end_station_id: end_station.id, bike_id: 1,
-    subscription_id: 1, zipcode_id: 1)
+    (30..91).each do |n|
+      trip = start_station.trips.create(duration: 123, start_date: "12/05/1987",
+                                       start_station_id: 1, end_date: "12/05/19#{n}",
+                                       end_station_id: 2, bike_id: 1,
+                                       subscription_id: 1, zipcode_id: 1)
+    end
+trip = Trip.create(duration: 123, start_date: "12/05/1987", start_station_id: 1, end_date: "12/05/1987", end_station_id: 2, bike_id: 1, subscription_id: 1, zipcode_id: 1)
+    visit('/trips/page/1')
+
+    expect(page).to have_content("1930")
+    expect(page).to have_content("1959")
+    expect(page).to_not have_content("1960")
+    expect(page).to_not have_content("1989")
+
+    click ("Next")
+
+    expect(page).to have_content("1960")
+    expect(page).to have_content("1989")
+    expect(page).to_not have_content("1930")
+    expect(page).to_not have_content("1959")
+    expect(current_path).to eq('/trips/page/2')
+
+    click ("Previous")
+
+    expect(page).to have_content("1930")
+    expect(page).to have_content("1959")
+    expect(page).to_not have_content("1960")
+    expect(page).to_not have_content("1989")
+    expect(current_path).to eq('/trips/page/1')
+
   end
 end

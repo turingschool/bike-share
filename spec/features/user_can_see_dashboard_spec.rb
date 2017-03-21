468728051e6eb64de49826f16ad45f26ed6f9d41
lukyans <sukyans@gmail.com>
2017-03-20 19:35:40 -0600

    resolving model test error

--- spec/features/user_can_see_dashboard_spec.rb
+++ spec/features/user_can_see_dashboard_spec.rb
@@ -1,12 +1,13 @@
 require_relative '../spec_helper'
+require 'pry'
 
 RSpec.describe "As a user visits station dashboard" do
 
   before(:each) do
     Station.create(name:'San Jose Diridon Caltrain Station', city: "San Jose", dock_count: 27, installation_date: "8/1/2013")
-    Station.create(name: "Denver Station", city: "Denver", dock_count: 25, installation_date: "8/6/2013")
-    Station.create(name: "Test Station", city: "Denver", dock_count: 55, installation_date: "8/6/2013")
-    Station.create(name: "Golden Station", city: "Denver", dock_count: 35, installation_date: "8/9/2013")
+    Station.create(name: "Denver Station", city: "Denver", dock_count: 25, installation_date: "8/6/2012")
+    Station.create(name: "Test Station", city: "Denver", dock_count: 55, installation_date: "8/6/2015")
+    Station.create(name: "Golden Station", city: "Denver", dock_count: 35, installation_date: "8/9/2016")
   end
 
   it "they can see content" do
@@ -16,60 +17,61 @@ RSpec.describe "As a user visits station dashboard" do
         expect(page).to have_content('Dashboard')
       end
 
-      within("body h4:nth-child(1)") do
-          expect(page).to have_content('Total Count: 2')
+      within("body") do
+          expect(page).to have_content('Total Count: 4')
       end
 
-      within("body h4:nth-child(2)") do
-          expect(page).to have_content('Average Bikes Available per Station: 2')
+      within("body") do
+          expect(page).to have_content('Average Bikes Available per Station: 35')
       end
 
-      within("body h4:nth-child(3)") do
-          expect(page).to have_content('Stations With Most Bikes: San Jose Diridon Caltrain Station (27)')
+      within("body") do
+          expect(page).to have_content('Stations With Most Bikes: Test Station: 55')
       end
 
-      within("body h4:nth-child(4)") do
+      within("body") do
           expect(page).to have_content('Stations With Most Bikes:')
 
       end
 
-      within("ul li:nth-child(1)") do
+      within(".most_bikes li:nth-child(1)") do
         expect(page).to have_content('Test Station: 55')
       end
 
-      within("ul li:nth-child(2)") do
+      within(".most_bikes li:nth-child(2)") do
         expect(page).to have_content('Golden Station: 35')
       end
 
-      within("ul li:nth-child(3)") do
+      within(".most_bikes li:nth-child(3)") do
         expect(page).to have_content('San Jose Diridon Caltrain Station: 27')
       end
 
-      within("body h4:nth-child(5)") do
+      within("body") do
           expect(page).to have_content('Station With Fewest Bikes: Denver Station (25)')
       end
 
-      within("body h4:nth-child(6)") do
-          expect(page).to have_content('Stations With Fewest Bikes')
+      within("body") do
+          expect(page).to have_content('Stations With Fewest Bikes:')
       end
 
-      within("ul li:nth-child(4)") do
+      within(".fewest_bikes li:nth-child(1)") do
         expect(page).to have_content('Denver Station: 25')
       end
 
-      within("ul li:nth-child(5)") do
+      within(".fewest_bikes li:nth-child(2)") do
         expect(page).to have_content('San Jose Diridon Caltrain Station: 27')
       end
 
-      within("ul li:nth-child(6)") do
+      within(".fewest_bikes li:nth-child(3)") do
         expect(page).to have_content('Golden Station: 35')
       end
 #------------------------------------------------------------------------------
-      within("body h4:nth-child(7)") do
+      within("body") do
+        binding.pry
           expect(page).to have_content('Most Recently Installed Station: Golden Station')
       end
 
-      within("body h4:nth-child(8)") do
+      within("body") do
           expect(page).to have_content('Oldest Station: San Jose Diridon Caltrain Station')
       end
 

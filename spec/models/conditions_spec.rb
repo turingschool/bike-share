ff97741a1a731e7db47a07a8973787dc136a1599
VictoriaVasys <vvasys@gmail.com>
2017-03-22 13:48:42 -0600

    add condition and station relationship tests

--- spec/models/conditions_spec.rb
+++ spec/models/conditions_spec.rb
@@ -50,6 +50,22 @@ RSpec.describe Condition do
       expect(conditions_for_date).to be_valid
     end
   end
+  
+  describe "relationships" do
+    it "should return multiple trips" do
+      conditions_for_date = Condition.create(date: "14/3/2017", max_temp: 30.0, min_temp: 20.0, mean_temp: 26.0, mean_humidity: 30.0, mean_visibility: 3.0, mean_wind_speed: 12.0, precipitation: 0.03)
+      city = City.create(name: "Denver")
+      station = city.stations.create(name: "Turing", dock_count: 100, installation_date: "14/3/2017")
+      bike = Bike.create(bike_number: 33)
+      subscription_type = SubscriptionType.create(subscription_type: "Subscriber")
+      zip_code = ZipCode.create(zip_code: 80602)
+      conditions_for_date.trips.create(duration: 100, start_date: "14/3/2017 14:14", start_station: station, end_date: "29/8/2013 20:14", end_station: station, bike: bike, subscription_type: subscription_type, zip_code: zip_code)
+      conditions_for_date.trips.create(duration: 200, start_date: "14/3/2017 14:14", start_station: station, end_date: "29/8/2013 20:14", end_station: station, bike: bike, subscription_type: subscription_type, zip_code: zip_code)
+      
+      expect(conditions_for_date.trips.count).to eq(2)
+      expect(conditions_for_date.trips.first.duration).to eq(100)
+    end
+  end
 
   describe "attributes" do
     it "should have name, city, dock count, and an installation date" do
--- spec/models/stations_spec.rb
+++ spec/models/stations_spec.rb
@@ -36,6 +36,22 @@ RSpec.describe Station do
       Station.create(name: "Memorial Union", dock_count: 100, installation_date: "14/3/2017", city_id: 1)
       expect(@city.stations.first.name).to eq("Memorial Union") 
     end
+
+    it "a station should have a start and end trips attached to it" do
+      @station1 = @city.stations.create(name: "Turing", dock_count: 100, installation_date: "14/3/2017")
+      @station2 = @city.stations.create(name: "Galvanize", dock_count: 1, installation_date: "1/4/1972")
+      @bike = Bike.create(bike_number: 33)
+      @subscription_type = SubscriptionType.create(subscription_type: "Subscriber")
+      @zip_code = ZipCode.create(zip_code: 80602)
+      @trip1 = Trip.create(duration: 100, start_date: "29/8/2013 14:14", start_station_id: @station1.id, end_date: "29/8/2013 20:14", end_station_id: @station2.id, bike_id: @bike.id, subscription_type_id: @subscription_type.id, zip_code_id: @zip_code.id)
+      @trip2 = Trip.create(duration: 200, start_date: "29/8/2013 14:14", start_station_id: @station1.id, end_date: "29/8/2013 20:14", end_station_id: @station2.id, bike_id: @bike.id, subscription_type_id: @subscription_type.id, zip_code_id: @zip_code.id)
+      @trip3 = Trip.create(duration: 300, start_date: "29/8/2013 14:14", start_station_id: @station2.id, end_date: "29/8/2013 20:14", end_station_id: @station2.id, bike_id: @bike.id, subscription_type_id: @subscription_type.id, zip_code_id: @zip_code.id)
+
+      expect(@station1.start_trips).to eq([@trip1, @trip2])
+      expect(@station2.end_trips).to eq([@trip1, @trip2, @trip3])
+
+      # expect(station.end_station.name).to eq('Galvanize')
+    end
   end
 
   describe "attributes" do
@@ -45,24 +61,6 @@ RSpec.describe Station do
       expect(station).to respond_to(:city)
       expect(station).to respond_to(:dock_count)
       expect(station).to respond_to(:installation_date)
-    end
-
-    describe "relationships" do
-      it "a station should have a start and end trips attached to it" do
-        @station1 = @city.stations.create(name: "Turing", dock_count: 100, installation_date: "14/3/2017")
-        @station2 = @city.stations.create(name: "Galvanize", dock_count: 1, installation_date: "1/4/1972")
-        @bike = Bike.create(bike_number: 33)
-        @subscription_type = SubscriptionType.create(subscription_type: "Subscriber")
-        @zip_code = ZipCode.create(zip_code: 80602)
-        @trip1 = Trip.create(duration: 100, start_date: "29/8/2013 14:14", start_station_id: @station1.id, end_date: "29/8/2013 20:14", end_station_id: @station2.id, bike_id: @bike.id, subscription_type_id: @subscription_type.id, zip_code_id: @zip_code.id)
-        @trip2 = Trip.create(duration: 200, start_date: "29/8/2013 14:14", start_station_id: @station1.id, end_date: "29/8/2013 20:14", end_station_id: @station2.id, bike_id: @bike.id, subscription_type_id: @subscription_type.id, zip_code_id: @zip_code.id)
-        @trip3 = Trip.create(duration: 300, start_date: "29/8/2013 14:14", start_station_id: @station2.id, end_date: "29/8/2013 20:14", end_station_id: @station2.id, bike_id: @bike.id, subscription_type_id: @subscription_type.id, zip_code_id: @zip_code.id)
-
-        expect(@station1.start_trips).to eq([@trip1, @trip2])
-        expect(@station2.end_trips).to eq([@trip1, @trip2, @trip3])
-
-        # expect(station.end_station.name).to eq('Galvanize')
-      end
     end
   end
 end

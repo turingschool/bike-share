9f2689c0db9cba478c7eb71f380eeaf6522edd77
lukyans <sukyans@gmail.com>
2017-03-21 23:18:32 -0600

    deleted start and end station id from all test and files

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,10 +2,8 @@ class Trip < ActiveRecord::Base
   validates :duration, presence: true
   validates :start_date, presence: true
   validates :start_station_name, presence: true
-  validates :start_station_id, presence: true
   validates :end_date, presence: true
   validates :end_station_name, presence: true
-  validates :end_station_id, presence: true
   validates :bike_id, presence: true
   validates :subscription_type, presence: true
   validates :zip_code, presence: true
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -3,7 +3,7 @@ require_relative "../spec_helper"
 RSpec.describe Trip do
   describe "knows attributes" do
 
-    let (:trip) {Trip.create(duration: 63, start_date: "2013/8/28", start_station_name: "South Van Ness at Market", start_station_id: 66, end_date: "2013/08/29", end_station_name: "South Van Ness at Market", end_station_id: 66, bike_id: 520, subscription_type: "Subscriber", zip_code: 94127)}
+    let (:trip) {Trip.create(duration: 63, start_date: "2013/8/28", start_station_name: "South Van Ness at Market", end_date: "2013/08/29", end_station_name: "South Van Ness at Market", bike_id: 520, subscription_type: "Subscriber", zip_code: 94127)}
 
     it "returns trip duration" do
       result = trip.duration
@@ -20,11 +20,6 @@ RSpec.describe Trip do
       expect(result).to eq("South Van Ness at Market")
     end
 
-    it "returns start station id" do
-      result = trip.start_station_id
-      expect(result).to eq(66)
-    end
-
     it "returns end date" do
       
       result = trip.end_date
@@ -34,11 +29,6 @@ RSpec.describe Trip do
     it "returns end station name" do
       result = trip.end_station_name
       expect(result).to eq("South Van Ness at Market")
-    end
-
-    it "returns end station id" do
-      result = trip.end_station_id
-      expect(result).to eq(66)
     end
 
     it "returns bike id" do
@@ -68,11 +58,6 @@ RSpec.describe Trip do
       expect(trip).to_not be_valid
     end
 
-    it "is invalid without a start station name" do
-      trip = Trip.create(duration: 63, start_date: "2013/8/28", start_station_id: 66, end_date: "2013/08/29", end_station_name: "South Van Ness at Market", end_station_id: 66, bike_id: 520, subscription_type: "Subscriber", zip_code: 94127)
-      expect(trip).to_not be_valid
-    end
-
     it "is invalid without a start station id" do
       trip = Trip.create(duration: 63, start_date: "2013/8/28", start_station_name: "South Van Ness at Market", end_date: "2013/08/29", end_station_name: "South Van Ness at Market", end_station_id: 66, bike_id: 520, subscription_type: "Subscriber", zip_code: 94127)
       expect(trip).to_not be_valid
@@ -85,11 +70,6 @@ RSpec.describe Trip do
 
     it "is invalid without end station name" do
       trip = Trip.create(duration: 63, start_date: "2013/8/28", start_station_name: "South Van Ness at Market", start_station_id: 66, end_date: "2013/08/29", end_station_id: 66, bike_id: 520, subscription_type: "Subscriber", zip_code: 94127)
-      expect(trip).to_not be_valid
-    end
-
-    it "is invalid without end station id" do
-      trip = Trip.create(duration: 63, start_date: "2013/8/28", start_station_name: "South Van Ness at Market", start_station_id: 66, end_date: "2013/08/29", end_station_name: "South Van Ness at Market", bike_id: 520, subscription_type: "Subscriber", zip_code: 94127)
       expect(trip).to_not be_valid
     end
 

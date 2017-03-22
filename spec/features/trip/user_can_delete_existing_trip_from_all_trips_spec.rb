a449dd44d7ce8f2f6b9fd56076748776becd1cc4
stovermc <stovermc@gmail.com>
2017-03-21 21:15:05 -0600

    finished funtionality for add a new trip

--- spec/features/trip/user_can_delete_existing_trip_from_all_trips_spec.rb
+++ spec/features/trip/user_can_delete_existing_trip_from_all_trips_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "as a user deletes an existing trip from all trips page" do
   it "they can see existing trip data and delete the trip record" do
     city = City.create(city: "Denver")
     zipcode = Zipcode.create(zip_code: 94062)
-    trip = Trip.create(duration: 550, start_date: "6/18/14 17:08", start_station_id: 51, end_date: "6/18/14 17:17", end_station_id: 70, bike_id: 551, subscription_type: "Subscriber", zipcode_id: 1)
+    trip = Trip.create(duration: 550, start_date: "6/18/14 17:08", start_station_id: 51, end_date: "6/18/14 17:17", end_station_id: 70, bike_id: 551, subscription_id: 1, zipcode_id: 1)
 
     # as a user
     # when i visit /trips

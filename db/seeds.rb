8da7425951a259a9fd5163936c03bdc8739ee9a6
caward12 <ward.colleen.a@gmail.com>
2017-03-22 16:01:19 -0600

    conditions table seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@ require_relative '../app/models/bike'
 require_relative '../app/models/subscription_type'
 require_relative '../app/models/zip_code'
 require_relative '../app/models/trip'
+require_relative '../app/models/condition'
 require 'database_cleaner'
 
 DatabaseCleaner.strategy = :truncation
@@ -32,4 +33,19 @@ trips.each do |trip|
               bike:                 Bike.find_or_create_by(bike_number: trip[:bike_id]),
               subscription_type:    SubscriptionType.find_or_create_by(subscription_type: trip[:subscription_type]),
               zip_code:             ZipCode.find_or_create_by(zip_code: trip[:zip_code]))
+end
+
+conditions = (CSV.open'db/csv/weather.csv', headers: true, header_converters: :symbol)
+
+conditions.each do |condition|
+  next unless condition[:zip_code] == "94107"
+
+  Condition.create!(date:             Date.strptime(condition[:date], "%m/%d/%Y"),
+                    max_temp:         condition[:max_temperature_f],
+                    mean_temp:        condition[:mean_temperature_f],
+                    min_temp:         condition[:min_temperature_f],
+                    mean_humidity:    condition[:mean_humidity],
+                    mean_visibility:  condition[:mean_visibility_miles],
+                    mean_wind:        condition[:mean_wind_speed_mph],
+                    precipitation:    condition[:precipitation_inches])
 end

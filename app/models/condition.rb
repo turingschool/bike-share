9e2e24a03d22751a09c1bd116ccb52fb68966cdf
jk1dd <portajonk@gmail.com>
2017-03-22 18:34:56 -0600

    add WIP i9 methods

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -20,4 +20,37 @@ class Condition < ActiveRecord::Base
     where(date: Trip.fewest_number_trips_date.first.to_s)
   end
 
+  def self.temperature_ranges
+    min_temp = minimum(:max_temperature_f)
+    max_temp = maximum(:max_temperature_f)
+    total_range = (min_temp..max_temp)
+    total_range.each_slice(10)
+  end
+
+  def self.date_in_temperature_ranges
+    temperature_ranges.map do |range|
+      where(max_temperature_f: range).map(&:date)
+    end
+
+  end
+
+  def self.rides_per_day
+    date_in_temperature_ranges.map do |range|
+      range.map do |date|
+        Trip.where(start_date: date)
+      end
+    end
+  end
+  # rides_per_day = For each date range, determine the number of rides
+  # on those days
+  # rides_per_day.max
+  # rides_per_day.average
+  # rides_per_day.min
+
+
+  # [13] pry(Condition)> x.map do |x|
+  # [13] pry(Condition)*   x.count
+  # [13] pry(Condition)* end
+  # Condition.where(max_temperature_f: range)
+
 end

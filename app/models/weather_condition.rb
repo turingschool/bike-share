42496bd69cf8ef03c68b4c5c1602dcef8010ab3d
Mark Van Akkeren <MarkVanAkkeren@gmail.com>
2017-03-23 01:24:22 -0600

    pushing last attempt at iteration9

--- app/models/weather_condition.rb
+++ app/models/weather_condition.rb
@@ -10,14 +10,14 @@ class WeatherCondition < ActiveRecord::Base
   validates :mean_wind_speed_mph, presence: true
   validates :precipitation_inches, presence: true
   validates :date, presence: true
-  
+
     def self.trips_by_temperature
-      temp_range = [[40,49],[50,59],[60,69],[70,79],[80,89]]
+      temp_range = [[40,49],[50,59],[60,69],[70,79],[80,89],[90,99]]
       trips_by_weather_condition(:max_temperature_f, temp_range)
     end
 
     def self.trips_by_precipitation
-      precip_range = [[0.0,0.49],[0.5,0.99],[100,1.49],[1.50,1.99]]
+      precip_range = [[0.0,0.49],[0.5,0.99],[1.00,1.49],[1.50,1.99],[2.00,2.49],[2.50,2.99],[3.00,3.49],[3.50,3.99]]
       trips_by_weather_condition(:precipitation_inches, precip_range)
     end
 
@@ -32,13 +32,12 @@ class WeatherCondition < ActiveRecord::Base
     end
 
     def self.trips_by_weather_condition(weather_variable, weather_condition_range)
-      weather_condition_range.reduce({}) do |trips_by_weather, stat_range|
-        query = self.where("#{weather_variable} >= ? AND #{weather_variable} <= ?", stat_range[0], stat_range[1])
-
+      weather_condition_range.reduce({}) do |trips_by_weather, condition_range|
+        query = self.where("#{weather_variable} >= ? AND #{weather_variable} <= ?", condition_range[0], condition_range[1])
         trip_date = query.reduce({}) do |trips_per_date, condition|
           trips_per_date.merge!({condition.date => condition.trips.count})
         end
-        key = stat_range.join(" - ")
+        key = weather_condition_range.join(" - ")
         trips_by_weather.merge!({key => trip_date})
       end
     end

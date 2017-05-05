9e2808acdf6a81ff4ddf64e198655f93e14468c5
Chris Smith <42@chrisryansmith.com>
2017-05-05 17:14:18 -0600

    update spec_helper with DatabaseCleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,18 @@ ENV["RACK_ENV"] ||= "test"
 
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec'
+require 'database_cleaner'
 require 'capybara/dsl'
 
+DatabaseCleaner.strategy = :truncation
 Capybara.app = BikeShareApp
 
 RSpec.configure do |c|
   c.include Capybara::DSL
+  c.before(:all) do
+    DatabaseCleaner.clean
+  end
+  c.after(:each) do
+    DatabaseCleaner.clean
+  end
 end

5e46443dca1579e255ecfa7a6b512e9fec56a909
Sam Lim <limsammy1@gmail.com>
2017-05-05 17:38:34 -0600

    wrote #find_records method that returns a row with a given query (array of csv row objects)

--- app/models/csv_loader.rb
+++ app/models/csv_loader.rb
@@ -6,7 +6,12 @@ class CSVLoader
     CSV.open file_name, headers:true, header_converters: :symbol
   end
 
-  def populate
-    
+  def find_records(contents, query)
+    search_in = query[:search_in].to_sym
+    search_for = query[:find]
+    result = contents.select do |row|
+      row[search_in] == search_for
+    end
+    result
   end
 end
--- spec/models/csv_loader_spec.rb
+++ spec/models/csv_loader_spec.rb
@@ -3,7 +3,7 @@ require_relative '../spec_helper'
 RSpec.describe CSVLoader do
   describe "parsing a csv file" do
     before(:each) do
-      # dummy = CSVLoader.new
+      @dummy = CSVLoader.new
       @file = CSVLoader.parse_file('./db/csv/station.csv')
     end
 
@@ -13,6 +13,16 @@ RSpec.describe CSVLoader do
 
     it 'has a count of 70' do
       expect(@file.readlines.size).to eq(70)
+    end
+
+    it 'returns array of csv rows' do
+      query = {search_in: 'name', find: 'San Jose Diridon Caltrain Station'}
+      data = @dummy.find_records(@file, query)
+      expect(data).to be_instance_of(Array)
+
+      data.each do |row|
+        expect(row).to be_instance_of(CSV::Row)
+      end
     end
   end
 end

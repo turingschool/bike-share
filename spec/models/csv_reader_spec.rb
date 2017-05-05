ca38a676b57c322712e228ea0dca15af1b269e77
Sam Lim <limsammy1@gmail.com>
2017-05-05 16:50:41 -0600

    set up parse_file test for csvreader module and file

---
+++ spec/models/csv_reader_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../spec_helper'
+
+RSpec.describe CSVReader do
+  describe "parsing a csv file" do
+    before(:each) do
+      @dummy = Object.new
+      @dummy.extend(CSVReader)
+    end
+
+    it 'returns csv object' do
+      @dummy.parse_file('./db/csv/station.csv')
+      expect(file).to be_instance_of(CSV)
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,8 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec'
 require 'capybara/dsl'
 
+require_relative '../../app/models/csv_reader'
+
 Capybara.app = BikeShareApp
 
 RSpec.configure do |c|

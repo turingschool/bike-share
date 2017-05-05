7c171ef47c7544ef97f35df059fe36740c5c9832
MasonHolland <masontholland@gmail.com>
2017-05-04 18:35:02 -0600

    initial commit. added database cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,17 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec'
 require 'capybara/dsl'
 
+DatabaseCleaner.strategy = :truncation
+
 Capybara.app = BikeShareApp
 
 RSpec.configure do |c|
   c.include Capybara::DSL
+  c.before(:all) do
+    DatabaseCleaner.clean
+  end
+
+  c.after(:each) do
+    DatabaseCleaner.clean
+  end
 end

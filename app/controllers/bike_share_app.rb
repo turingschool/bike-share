0a90c8821ea923322bd1b5a32eab89a2e75efc6e
MasonHolland <masontholland@gmail.com>
2017-05-06 11:30:49 -0600

    created feature test files for station, created routes and erb files for stations CRUD

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -1,3 +1,16 @@
 class BikeShareApp < Sinatra::Base
 
+  get '/stations' do
+    erb :"stations/index"
+  end
+
+  get '/stations/view_all' do
+    @stations = Station.all
+    erb :"stations/view_all"
+  end
+  # 
+  # get '/stations/:id' do
+  #
+  # end
+
 end

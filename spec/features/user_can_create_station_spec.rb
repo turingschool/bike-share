90231feeacc1c1142c8c9de64cf04d7883ad49da
Chris Smith <42@chrisryansmith.com>
2017-05-06 10:43:37 -0600

    Add feature specs for user creating station and view station details

---
+++ spec/features/user_can_create_station_spec.rb
@@ -0,0 +1,20 @@
+require './spec/spec_helper'
+
+RSpec.describe "when a user visits the new station page" do
+  it "they can create a new station" do
+    visit('/stations/new')
+    expect(page).to have_content("Add a new station")
+    fill_in("#station[name]", with: "Something")
+    select "City 1", :from => station[city_id]
+    fill_in("#station[dock_count]", with: 1)
+    # installation date (should eventually be a datepicker)
+    fill_in("#station[installation_date]", with: "08/30/2013")
+    fill_in("#station[latitude]", with: -1.1)
+    fill_in("#station[longitude]", with: 1.1)
+
+    click_button("Create Station")
+
+    expect(page).to have_content("Name: Something")
+    expect(page).to have_current_path("/stations/1/")
+  end
+end

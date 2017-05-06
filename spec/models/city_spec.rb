04e608419c373537a9182a3abe7961326e40e940
Chris Smith <42@chrisryansmith.com>
2017-05-06 16:07:17 -0600

    added city access to stations

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -7,4 +7,18 @@ RSpec.describe City do
     expect(city.id).to eq 1
     expect(city.name).to eq "squeevillia"
   end
+
+  it "cannot be made without a name" do
+    city = City.create
+
+    expect(city).to be_invalid
+  end
+
+  it "city has stations" do
+    city = City.create(name: "squeevillia")
+    station = city.stations.create(dock_count: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
+
+    expect(station.city.name).to eq "squeevillia"
+
+  end
 end

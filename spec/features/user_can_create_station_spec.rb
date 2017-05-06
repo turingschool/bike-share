6f2365f0166f66436a890918deb5b349461927f7
squeemishly <squeemishly@gmail.com>
2017-05-06 13:05:38 -0600

    checking how the page loads in capybara

--- spec/features/user_can_create_station_spec.rb
+++ spec/features/user_can_create_station_spec.rb
@@ -11,8 +11,10 @@ RSpec.describe "when a user visits the new station page" do
     fill_in("station[installation_date]", with: "08/30/2013")
     fill_in("station[latitude]", with: -1.1)
     fill_in("station[longitude]", with: 1.1)
+    save_and_open_page
 
     click_button("Create Station")
+    save_and_open_page
 
     #expect(Station.all.count).to eq(1)
     expect(page).to have_current_path("/stations/1/")
--- spec/features/user_can_see_one_station_spec.rb
+++ spec/features/user_can_see_one_station_spec.rb
@@ -14,6 +14,7 @@ RSpec.describe "when a user visits the view station page" do
 
   it "they can view information about a station" do
     visit('/stations/1')
+    save_and_open_page
     expect(page).to have_content("Name: Something")
     expect(page).to have_content("Edit Station")
     expect(page).to have_selector(".btn-edit")

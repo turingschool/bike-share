c233fbe824b76b8b52452456b51f7c3a06075938
Yohanan Assefa <nanahoy@gmail.com>
2017-05-06 12:31:31 -0600

    created feature test user_can_view_edit_page_spec

---
+++ spec/features/user_can_view_edit_page_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../spec_helper'
+
+#As a user when I am on the additional info page
+# and click on the edit link
+# I expect to be redirected to an edit page
+# and see a form to edit station info
+
+RSpec.describe "when a user visits the edit page" do
+  it "displays an edit form" do
+    City.create(name: "Denver")
+    city = City.find(1)
+    station = city.stations.create(name: "MLK", dock_count: 22, installation_date: "9/19/1983")
+
+    visit("/stations/1")
+
+    click_on "Edit"
+    expect(current_path).to eq("/stations/1/edit")
+    expect(page).to have_content("Submit")
+    expect(page).to have_content("MLK")
+    save_and_open_page
+  end
+end 

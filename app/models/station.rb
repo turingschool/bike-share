841dad8200ca56262f0032ba06b5fed9853e4741
NataliaColome <nmcolome@gmail.com>
2017-05-05 18:05:39 -0600

    full station validation

--- app/models/station.rb
+++ app/models/station.rb
@@ -3,4 +3,6 @@ class Station < ActiveRecord::Base
   belongs_to :the_date
   validates :name, presence: true
   validates :dock_count, presence: true
+  validates :city_id, presence: true
+  validates :the_date_id, presence: true
 end

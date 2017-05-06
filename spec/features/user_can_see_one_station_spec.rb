647c855ee35e41c3ec475c7d87989e21df56b52e
Chris Smith <42@chrisryansmith.com>
2017-05-06 12:07:53 -0600

    add before block to the user_can_see_one_station so the page exists

--- spec/features/user_can_see_one_station_spec.rb
+++ spec/features/user_can_see_one_station_spec.rb
@@ -1,6 +1,17 @@
 require './spec/spec_helper'
 
 RSpec.describe "when a user visits the view station page" do
+  before do
+    Station.create(
+                  name: "something",
+                  dock_count: 1,
+                  city_id: 1,
+                  installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'),
+                  longitude: -121.9,
+                  latitude: 30.7
+    )
+  end
+
   it "they can view information about a station" do
     visit('/stations/1')
     expect(page).to have_content("Name: Something")

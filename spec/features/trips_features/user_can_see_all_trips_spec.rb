ae998f593ae2dadd916ed491292591945595a453
Rhonda <rwilhel@gmail.com>
2017-05-07 16:02:40 -0600

    Progress made on user feature testing for Trips

--- spec/features/trips_features/user_can_see_all_trips_spec.rb
+++ spec/features/trips_features/user_can_see_all_trips_spec.rb
@@ -9,22 +9,63 @@ RSpec.describe "When the user visits the Trips page" do
     expect(page).to have_content("All Trips")
     expect(page).to have_content("Add New Trip")
     expect(page).to have_content("a.btn-new")
+    click_button "Add New Trip"
+    expect(page).to have_current_path('/trips/new')
   end
 
   context "when trips have been added" do
     before do
       zipcode1 = Zipcode.create(name: "99999")
       Trip.create!(
-                    duration: 1000,
-                    start_date: Date.strptime("08/30/2013",'%m/%d/%Y'),
+                    duration: 60,
+                    start_date: Date.strftime("08/30/2013 11:11", "%m/%d/%Y %H:%M"),
                     start_station_id: 1,
-                    end_date: Date.strptime("08/31/2013",'%m/%d/%Y'),
+                    end_date: Date.strftime("08/30/2013 11:12", "%m/%d/%Y %H:%M"),
                     end_station_id: 1,
                     bike_id: 1,
                     subscription_type: "Subscriber"
-                    zipcode_id: 
       )
-      visit('/stations')
+      Station.create!(
+                    name: "something",
+                    dock_count: 1,
+                    city_id: 1,
+                    installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'),
+                    longitude: -121.9,
+                    latitude: 30.7
+      )
+      visit('/trips')
+    end
+    it "the page lists the added trip's duration" do
+      expect(page).to have_content(60)
+    end
+    it "the page lists the added trip's start date and time" do
+      expect(page).to have_content("08/30/2013 11:11")
+    end
+    it "the page lists the added trip's start station name" do
+      expect(page).to have_content("something")
+    end
+    it "the page lists the added trip's end date and time" do
+      expect(page).to have_content("08/30/2013 11:12")
+    end
+    it "the page lists the added trip's end station name" do
+      expect(page).to have_content("something")
+    end
+    it "the page lists the added trip's bike ID" do
+      expect(page).to have_content(1)
+    end
+    it "the page lists the added trip's subscription type" do
+      expect(page).to have_content("Subscriber")
+    end
+    it "the page lists the added trip's view link" do
+      expect(page).to have_content("View Trip")
+      expect(page).to have_selector(".btn-view")
+    end
+    it "the page lists the added trip's edit link" do
+      expect(page).to have_content("Edit Trip")
+      expect(page).to have_selector(".btn-edit")
+    end
+    it "the page lists the added trip's delete link" do
+      expect(page).to have_selector("form .btn-delete")
     end
   end
 end

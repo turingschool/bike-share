fd5ab2e1724c09fdf23692998444405e4a5df0d9
NataliaColome <nmcolome@gmail.com>
2017-05-07 13:40:54 -0600

    Edit feature tests are working

--- spec/features/user_can_edit_an_existing_station_spec.rb
+++ spec/features/user_can_edit_an_existing_station_spec.rb
@@ -1,23 +1,28 @@
 require_relative '../spec_helper'
+require 'pry'
 
-RSpec.describe "When a user visits '/stations/new'" do
-  it "they see a 'new' form to create a new station" do
+RSpec.describe "When a user visits '/stations/{id}/edit'" do
+  it "sees an 'edit' form to edit an existing station" do
     date = DateRef.create!(date: "2010-01-01")
     city = City.create!(name: "Denver")
     Station.create!(name: "Broadway", dock_count: 10, city_id: city.id, date_ref_id: date.id)
 
-    visit('/stations/new')
+    visit('/stations/1/edit')
     fill_in("station[name]", with: "15th st")
     fill_in("station[dock_count]", with: 13)
     fill_in("station[installation_date]", with: "2017-10-01")
-    select("Denver", from: "station[city_id]")
-    click_on "Create New Station"
+    click_on "Edit Station"
 
-    expect(current_path).to eq("/stations/2")
+    expect(current_path).to eq("/stations/1")
+    expect(page).to have_content("15th st")
+    expect(Station.first.name).to eq("15th st")
+    expect(Station.first.dock_count).to eq(13)
+    expect(page).to have_content("2017-10-01")
+    expect(page).to have_content("Denver")
   end
 
-  it "they see the form labels" do
-    visit('/stations/new')
+  it "sees the form labels" do
+    visit('/stations/1/edit')
     expect(page).to have_content("Name:")
     expect(page).to have_content("Dock count:")
     expect(page).to have_content("Installation Date:")

4d6efd55c08b18c33137d02930a2f8b9a6aeb548
adamgunther1 <adamgunther1@gmail.com>
2017-05-07 13:36:34 -0600

    adds validations to models for zipcode and trips

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,3 +1,10 @@
 class Trip < ActiveRecord::Base
-  
+  belongs_to :zipcode
+  validates :duration, presence: true
+  validates :start_date, presence: true
+  validates :start_station_id, presence: true
+  validates :end_date, presence: true
+  validates :end_station_id, presence: true
+  validates :bike_id, presence: true
+  validates :subscription_type, presence: true
 end

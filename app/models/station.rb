1785eeb2b1156b907f4f59676319995a6f5aad77
Sam Lim <limsammy1@gmail.com>
2017-05-07 07:32:38 -0600

    #total returns total count of entries

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,4 +1,9 @@
 class Station < ActiveRecord::Base
   validates :name, :dock_count, :city, :date, presence: true
 
+  # total count of stations
+  def self.total
+    Station.all.count
+  end
+
 end

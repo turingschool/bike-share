c7338e88bbe7cb33cf72fb1e8563a3143e98d25a
Thomas Soucie <tsoucie1858@gmail.com>
2017-05-07 17:10:50 -0600

    Resolved migration errors

--- db/migrate/20170507214534_add_cities_to_stations.rb
+++
@@ -1,5 +0,0 @@
-class AddCitiesToStations < ActiveRecord::Migration[5.0]
-  def change
-    add_column :stations, :city_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170507215548) do
+ActiveRecord::Schema.define(version: 20170507202436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "cities", force: :cascade do |t|
     t.text     "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "dates", force: :cascade do |t|
-    t.integer  "day"
-    t.integer  "month"
-    t.integer  "year"
-    t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

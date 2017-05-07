47a7c403b602915460e54a90f633388921ee901a
Rhonda <rwilhel@gmail.com>
2017-05-07 16:08:26 -0600

    Progress made on features testing for Trips

--- spec/features/trips_features/user_can_edit_a_trip_spec.rb
+++ spec/features/trips_features/user_can_edit_a_trip_spec.rb
@@ -2,5 +2,30 @@ require './spec/spec_helper'
 
 RSpec.describe "When a user edits a trip" do
   before do
-    
+    zipcode1 = Zipcode.create(name: "99999")
+    Trip.create!(
+                  duration: 60,
+                  start_date: Date.strftime("08/30/2013 11:11", "%m/%d/%Y %H:%M"),
+                  start_station_id: 1,
+                  end_date: Date.strftime("08/30/2013 11:12", "%m/%d/%Y %H:%M"),
+                  end_station_id: 1,
+                  bike_id: 1,
+                  subscription_type: "Subscriber"
+    )
+    Station.create!(
+                  name: "something",
+                  dock_count: 1,
+                  city_id: 1,
+                  installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'),
+                  longitude: -121.9,
+                  latitude: 30.7
+    )
+  end
+
+  it "can edit the duration" do
+    visit('/trips/1/edit')
+    expect(page)
+
+  end
+
 end

d046f2441386944f9987b8d4cacf54285b2af585
MasonHolland <masontholland@gmail.com>
2017-05-07 15:37:21 -0600

    subscription type validations complete, model test passing

--- app/models/subscription_type.rb
+++ app/models/subscription_type.rb
@@ -1,3 +1,5 @@
 class SubscriptionType < ActiveRecord::Base
   has_many :trips
+
+  validates :name, presence: true
 end
--- spec/models/subscription_type_spec.rb
+++ spec/models/subscription_type_spec.rb
@@ -8,4 +8,25 @@ RSpec.describe SubscriptionType  do
       expect(subscription.name).to eq("Customer")
     end
   end
+
+  describe "validations" do
+    it "is invalid without a name" do
+      subscription = SubscriptionType.create(name: nil)
+      expect(subscription).to be_invalid
+    end
+  end
+
+  describe "access to trips" do
+    it "can access trip duration" do
+      subscription = SubscriptionType.create(name: "Customer")
+      subscription.trips.create(duration: 400,
+                                start_station: 1,
+                                end_station: 2,
+                                start_date: "2012-8-12",
+                                end_date: "2012-8-12",
+                                bike_id: 122,
+                                zip_code: 80218)
+      expect(subscription.trips.first.duration).to eq(400)
+    end
+  end
 end

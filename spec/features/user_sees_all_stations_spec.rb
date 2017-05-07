460cf679c697c1014142543b6c7a4c1ce5810178
Thomas Soucie <tsoucie1858@gmail.com>
2017-05-07 13:22:01 -0600

    Commits first feature test to load stations/index page

--- spec/features/user_sees_all_stations_spec.rb
+++ spec/features/user_sees_all_stations_spec.rb
@@ -1,14 +1,23 @@
 require './spec/spec_helper'
 
 RSpec.describe "test bike share app" do
-  Station.create(name: 'Aaron', city: 'Denver', dock_count: 56, install_date: '2010/08/13')
 
   it 'routs to station index' do
+    Station.create(name: 'Lodo', city: 'Denver', dock_count: 56, install_date: '2010/08/13')
 
     visit('/stations')
 
-    expect(page).to have_content("Stations")
+    save_and_open_page
 
+    expect(page).to have_content("Stations")
+    expect(page).to have_content("Lodo")
+    expect(page).to have_content("56")
+    expect(page).to have_content("2010-08-13")
   end
+
+
+
+  # describe "validations" do 
+  #   it "is invalid without name"
 
 end

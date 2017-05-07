6724f4bb2b628ec8f152deda14ddc0ae1a407473
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-05-06 19:06:32 -0600

    Create new station functionality

--- spec/features/user_can_view_edited_station_spec.rb
+++ spec/features/user_can_view_edited_station_spec.rb
@@ -7,14 +7,18 @@ require_relative '../spec_helper'
 RSpec.describe "user can fill in form" do
   it "it updates page with correct information" do
       city = City.create(name: "Washington DC")
+      City.create(name: "Frankfurt")
+      City.create(name: "Tokyo")
       station = city.stations.create!(name: "MLK", dock_count: 32, installation_date: "1984-05-24")
 
       visit('/stations/1/edit')
       fill_in "station[name]", with: "Malcolm X"
-      save_and_open_page
+      select("Tokyo", :from => "city[name]")
 
       click_button "Submit changes"
+      save_and_open_page
       expect(current_path).to eq('/stations/1')
       expect(page).to have_content("Malcolm X")
+      expect(page).to have_content("Tokyo")
   end
 end

6aff3c97b0a744232cebf21556dd8e1ba80c288c
Sam Lim <limsammy1@gmail.com>
2017-05-06 22:13:29 -0600

    implemented sanitize method into seed file, rake db:seed succesfully seeds test database

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,16 @@ require 'csv'
 require_relative '../app/models/csv_loader'
 require_relative '../app/models/station'
 
-CSV.foreach("db/csv/station.csv", { encoding: "UTF-8", headers: ['name', 'dock_count', 'city', 'installation_date'], header_converters: :symbol, converters: :all}) do |row|
-  row = CSVLoader.row
-  Station.create(CSVLoader.sanitize(row))
+loader = CSVLoader.new
+data = loader.sanitize_station('./db/csv/station.csv')
+
+data.each do |station|
+  puts "Seeding db_stations with: #{station[[:name]]}"
+
+  Station.create!(
+                    name: station[[:name]],
+                    dock_count: station[[:dock_count]],
+                    city: station[[:city]],
+                    date: station[[:date]]
+                  )
 end

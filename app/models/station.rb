31b5b64f85c34d9bed24e4b7ce525700a163f0a4
Alex Koch <alex549us3@gmail.com>
2017-05-08 10:07:45 -0600

    some cleanup

--- app/models/station.rb
+++ app/models/station.rb
@@ -2,11 +2,11 @@ class Station < ActiveRecord::Base
   belongs_to :city
 
   def self.average_available_bikes
-    self.average(:dock_count).to_i
+    average(:dock_count).to_i
   end
 
   def self.max_available_bikes
-    self.all.max_by(3) do |station|
+    all.max_by(3) do |station|
       station.dock_count
     end
   end

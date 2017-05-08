5da64ff0daaf02181beb604219f1b5c8bdabd805
William Thomas <wthoma390@gmail.com>
2017-05-08 14:20:28 -0600

    makes create trips migration

---
+++ db/migrate/20170508201547_create_trips.rb
@@ -0,0 +1,16 @@
+class CreateTrips < ActiveRecord::Migration[5.0]
+  def change
+    create_table :trips do |t|
+      t.integer :duration
+      t.string :start_date
+      t.integer :start_station_id
+      t.string :end_date
+      t.integer :end_station_id
+      t.integer :bike_id
+      t.string :subscription_type
+      t.integer :zip_code
+
+      t.timestamps null: false
+    end
+  end
+end

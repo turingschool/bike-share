a516481288d71233cbdf9a73b6f520182b79b3b2
Sam Lim <limsammy1@gmail.com>
2017-05-08 15:19:01 -0600

    wrote sanitize_trips method

--- app/models/csv_loader.rb
+++ app/models/csv_loader.rb
@@ -36,4 +36,20 @@ class CSVLoader
       }
     end
   end
+
+  def sanitize_trips(path)
+    data = CSV.open(path, headers: true, header_converters: :symbol)
+
+    data.map do |row|
+      {
+        [:duration] => row[:duration].to_i,
+        [:start_date] => row[:start_date],
+        [:start_station_id] => row[:start_station_id].to_i,
+        [:end_date] => row[:end_date],
+        [:end_station_id] => row[:end_station_id].to_i,
+        [:bike_id] => row[:bike_id].to_i,
+        [:subscription_type] => row[:subscription_type],
+        [:zip_code] => row[:zip_code].to_i
+      }
+  end
 end

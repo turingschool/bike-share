62392f4d390295ea6e2f6853e495b6bf020e4a46
squeemishly <squeemishly@gmail.com>
2017-05-08 14:14:43 -0600

    edit test and form created

--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -1,7 +1,13 @@
 class ConditionsController < BikeShareApp
   set :views, File.join(APP_ROOT, "app", "views", "conditions")
 
-  get '/conditions/new' do
-    erb :"new"
+  get '/conditions/:id/edit' do |id|
+    @condition = Condition.find(id)
+    erb :"edit"
+  end
+
+  put '/conditions/:id' do |id|
+    @condition = Condition.update(id)
+    redirect "/conditions/#{id}"
   end
 end
---
+++ spec/features/conditions/user_can_edit_an_existing_item_spec.rb
@@ -0,0 +1,40 @@
+require './spec/spec_helper'
+require './spec/features/conditions/controller_helper'
+
+RSpec.describe "When a user edits a condition" do
+  it "can edit all feilds" do
+    condition = Condition.create(
+                                date: Date.strptime("08/30/2013",'%m/%d/%Y'),
+                                max_temp: 87.0,
+                                mean_temp: 76.0,
+                                min_temp: 54.0,
+                                mean_humidity: 90.0,
+                                mean_visibility: 10.0,
+                                mean_wind_speed: 11.0,
+                                precipitation: 0,
+                                )
+
+    visit('/conditions/1/edit')
+
+    fill_in("condition[date]", with: "07/20/2013")
+    fill_in("condition[max_temp]", with: 83.0)
+    fill_in("condition[mean_temp]", with: 72.0)
+    fill_in("condition[min_temp]", with: 62.0)
+    fill_in("condition[mean_humidity]", with: 87.0)
+    fill_in("condition[mean_visibility]", with: 9.0)
+    fill_in("condition[mean_wind_speed]", with: 12.0)
+    fill_in("condition[precipitation]", with: 0.01)
+
+    click_button("Update Condition")
+
+    expect(page).to have_content("Date: 07/20/2013")
+    expect(page).to have_content("Highest Temp: 83.0°F")
+    expect(page).to have_content("Mean Temp: 72.0°F ")
+    expect(page).to have_content("Lowest Temp: 62.0°F")
+    expect(page).to have_content("Mean Humidity: 87.0")
+    expect(page).to have_content("Mean Visibility: 9.0 miles")
+    expect(page).to have_content("Mean Wind Speed: 12.0 mph")
+    expect(page).to have_content("Precipitation: 0.01 inches")
+
+  end
+end

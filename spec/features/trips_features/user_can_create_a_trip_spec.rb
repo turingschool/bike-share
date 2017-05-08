b6adaccb804d8ce352bd27293f03adede7a6651d
adamgunther1 <adamgunther1@gmail.com>
2017-05-08 11:50:14 -0600

    adds city and stations for test setup

--- spec/features/trips_features/user_can_create_a_trip_spec.rb
+++ spec/features/trips_features/user_can_create_a_trip_spec.rb
@@ -2,13 +2,24 @@ require './spec/spec_helper'
 
 RSpec.describe "when a user visits the new trip page" do
   it "they can create a new trip" do
+    city_1 = City.create(name: "squeevillia")
+    city_2 = City.create(name: "rhondarhiffic")
+    city_3 = City.create(name: "chrissable")
+    city_4 = City.create(name: "adamtastic")
+    Station.create(
+                  name: "something",
+                  dock_count: 1,
+                  city_id: 1,
+                  installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'),
+                  longitude: -121.9,
+                  latitude: 30.7
+    )
     zipcode = Zipcode.create(zipcode: 94127)
     expect(Trip.all.count).to eq(0)
 
     visit('/trips/new')
     expect(page).to have_content("Add a new trip")
-    #This should be calculated by end_date - start_date
-    # fill_in("trip[duration]", with: 840)
+    fill_in("trip[duration]", with: 840)
     fill_in("trip[start_date]", with: "08/30/2013 12:45")
     select "squeevillia", :from => "trip[start_station_id]"
     fill_in("trip[end_date]", with: "08/30/2013 12:59")

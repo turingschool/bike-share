f31abac061d3ef9b2fabcc0fbe9558e3b4d74852
Chris Smith <42@chrisryansmith.com>
2017-05-08 09:49:29 -0600

    update station model spec to have describe and before for model dashboard methods)

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -50,66 +50,40 @@ RSpec.describe Station do
     expect(station).to be_invalid
   end
 
-  it "can calculate the average number of bikes for all stations" do
-    Station.create(name: "something", dock_count: 1, city_id: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-    Station.create(name: "else", dock_count: 2, city_id: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-    Station.create(name: "other", dock_count: 3, city_id: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-    Station.create(name: "and again", dock_count: 4, city_id: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
+  describe "station can calculate dashboard information" do
+    before {
+      Station.create(name: "something", dock_count: 1, city_id: 1, installation_date: Date.strptime("08/30/2015",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
+      Station.create(name: "else", dock_count: 2, city_id: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
+      Station.create(name: "other", dock_count: 3, city_id: 1, installation_date: Date.strptime("08/30/2014",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
+      Station.create(name: "and again", dock_count: 4, city_id: 1, installation_date: Date.strptime("08/30/2014",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
+    }
+    it "can calculate the average number of bikes for all stations" do
+      expect(Station.average_bikes).to eq(2.5)
+    end
 
-    expect(Station.average_bikes).to eq(2.5)
+    it "can calculate the most bikes at any stations" do
+      expect(Station.most_bikes_count).to eq(4)
+    end
+
+    it "can calculate which stations have the most bikes" do
+      expect(Station.most_bikes_avail_at).to eq('and again')
+    end
+
+    it "can calculate the fewest bikes at any stations" do
+      expect(Station.fewest_bikes_count).to eq(1)
+    end
+
+    it "can calculate which stations have the fewest bikes" do
+      expect(Station.fewest_bikes_avail_at).to eq('something')
+    end
+
+    it "can find the oldest station created" do
+      expect(Station.oldest_station).to eq('else')
+    end
+
+    it "can find the newewst station created" do
+      expect(Station.newest_station).to eq('something')
+    end
   end
 
-  it "can calculate the most bikes at any stations" do
-    Station.create(name: "something", dock_count: 1, city_id: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-    Station.create(name: "else", dock_count: 2, city_id: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-    Station.create(name: "other", dock_count: 3, city_id: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-    Station.create(name: "and again", dock_count: 4, city_id: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-
-    expect(Station.most_bikes_count).to eq(4)
-  end
-
-  it "can calculate which stations have the most bikes" do
-    Station.create(name: "something", dock_count: 1, city_id: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-    Station.create(name: "else", dock_count: 2, city_id: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-    Station.create(name: "other", dock_count: 3, city_id: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-    Station.create(name: "and again", dock_count: 4, city_id: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-
-    expect(Station.most_bikes_avail_at).to eq('and again')
-  end
-
-  it "can calculate the fewest bikes at any stations" do
-    Station.create(name: "something", dock_count: 1, city_id: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-    Station.create(name: "else", dock_count: 2, city_id: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-    Station.create(name: "other", dock_count: 3, city_id: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-    Station.create(name: "and again", dock_count: 4, city_id: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-
-    expect(Station.fewest_bikes_count).to eq(1)
-  end
-
-  it "can calculate which stations have the fewest bikes" do
-    Station.create(name: "something", dock_count: 1, city_id: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-    Station.create(name: "else", dock_count: 2, city_id: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-    Station.create(name: "other", dock_count: 3, city_id: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-    Station.create(name: "and again", dock_count: 4, city_id: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-
-    expect(Station.fewest_bikes_avail_at).to eq('something')
-  end
-
-  it "can find the oldest station created" do
-    Station.create(name: "something", dock_count: 1, city_id: 1, installation_date: Date.strptime("08/20/2014",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-    Station.create(name: "else", dock_count: 2, city_id: 1, installation_date: Date.strptime("07/19/2012",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-    Station.create(name: "other", dock_count: 3, city_id: 1, installation_date: Date.strptime("01/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-    Station.create(name: "and again", dock_count: 4, city_id: 1, installation_date: Date.strptime("08/29/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-
-    expect(Station.oldest_station).to eq('else')
-  end
-
-  it "can find the newewst station created" do
-    Station.create(name: "something", dock_count: 1, city_id: 1, installation_date: Date.strptime("08/20/2014",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-    Station.create(name: "else", dock_count: 2, city_id: 1, installation_date: Date.strptime("07/19/2012",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-    Station.create(name: "other", dock_count: 3, city_id: 1, installation_date: Date.strptime("01/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-    Station.create(name: "and again", dock_count: 4, city_id: 1, installation_date: Date.strptime("08/29/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-
-    expect(Station.newest_station).to eq('something')
-  end
 end

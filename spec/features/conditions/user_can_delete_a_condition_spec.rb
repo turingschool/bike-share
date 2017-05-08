679a0faba50c3b69775e8b84923ecb4d0c1b2382
squeemishly <squeemishly@gmail.com>
2017-05-08 14:25:05 -0600

    created delete a condition test

---
+++ spec/features/conditions/user_can_delete_a_condition_spec.rb
@@ -0,0 +1,70 @@
+require './spec/spec_helper'
+require './spec/features/conditions/controller_helper'
+
+RSpec.describe "When a user clicks delete" do
+  it "on the index" do
+    condition = Condition.create(
+                                date: Date.strptime("08/30/2013",'%m/%d/%Y'),
+                                max_temp: 87.0,
+                                mean_temp: 76.0,
+                                min_temp: 54.0,
+                                mean_humidity: 90.0,
+                                mean_visibility: 10.0,
+                                mean_wind_speed: 11.0,
+                                precipitation: 0,
+                                )
+
+    visit('/conditions')
+
+    expect(page).to have_button("Delete")
+    expect(Condition.all.count).to eq(1)
+    click_button("Delete")
+
+    expect(Condition.all.count).to eq(0)
+    expect(page).to have_current_path("/conditions")
+  end
+
+  it "on the edit page" do
+    condition = Condition.create(
+                                date: Date.strptime("08/30/2013",'%m/%d/%Y'),
+                                max_temp: 87.0,
+                                mean_temp: 76.0,
+                                min_temp: 54.0,
+                                mean_humidity: 90.0,
+                                mean_visibility: 10.0,
+                                mean_wind_speed: 11.0,
+                                precipitation: 0,
+                                )
+
+    visit('/conditions/:id/edit')
+
+    expect(page).to have_button("Delete")
+    expect(Condition.all.count).to eq(1)
+    click_button("Delete")
+
+    expect(Condition.all.count).to eq(0)
+    expect(page).to have_current_path("/conditions")
+  end
+
+  it "on the show page" do
+    condition = Condition.create(
+                                date: Date.strptime("08/30/2013",'%m/%d/%Y'),
+                                max_temp: 87.0,
+                                mean_temp: 76.0,
+                                min_temp: 54.0,
+                                mean_humidity: 90.0,
+                                mean_visibility: 10.0,
+                                mean_wind_speed: 11.0,
+                                precipitation: 0,
+                                )
+
+    visit('/conditions/:id')
+
+    expect(page).to have_button("Delete")
+    expect(Condition.all.count).to eq(1)
+    click_button("Delete")
+
+    expect(Condition.all.count).to eq(0)
+    expect(page).to have_current_path("/conditions")
+  end
+end

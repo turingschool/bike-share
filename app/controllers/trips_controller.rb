7feef7e00c916902cae1fbc4ad8a3dadc2894188
Rhonda <rwilhel@gmail.com>
2017-05-08 13:17:20 -0600

    Completes Trips controller routes

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -13,9 +13,37 @@ class TripsController < BikeShareApp
 
   put '/trips/:id' do |id|
     trip_details = params[:trip]
-    trip_details[:start_date] = Date.strptime("08/30/2013 11:11",'%m/%d/%Y %H:%M')
+    trip_details[:start_date] = Date.strptime(trip_details[:start_date],'%m/%d/%Y %H:%M')
+    trip_details[:end_date] = Date.strptime(trip_details[:end_date],'%m/%d/%Y %H:%M')
+    @trip = Trip.update(id, trip_details)
+    redirect "/trips/#{id}"
   end
 
+  delete '/trips/:id' do |id|
+    Trip.destroy(id)
+    redirect "/trips"
+  end
 
+  get 'trips/:id/edit' do |id|
+    @trip = Trip.find(id)
+    erb :"edit"
+  end
 
+  get '/trips' do
+    @trips = Trip.all
+    erb :"index"
+  end
+
+  post '/trips' do
+    trip_details = params[:trip]
+    trip_details[:start_date] = Date.strptime(trip_details[:start_date],'%m/%d/%Y %H:%M')
+    trip_details[:end_date] = Date.strptime(trip_details[:end_date],'%m/%d/%Y %H:%M')
+    @trip = Trip.create!(trip_details)
+    redirect "/trips/#{@trip.id}"
+  end
+
+  get '/trips-dashboard' do
+    @trips = Trip.all
+    erb :"dashboard"
+  end
 end

1b0a658bbee164b070a35f481e8beedd78fb90dc
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-05-08 17:19:47 -0600

    trip model testing

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -5,9 +5,6 @@ class Trip<ActiveRecord::Base
   belongs_to :date_ref
   belongs_to :end_date, :class_name => "DateRef"
 
-  # belongs_to :start_station, :class_name => "Station"
-  # belongs_to :end_station, :class_name => "Station"
-
   belongs_to :start_station, foreign_key: "start_station_id", class_name: "Station"
   belongs_to :end_station, foreign_key: "end_station_id", class_name: "Station"
 
@@ -21,39 +18,32 @@ class Trip<ActiveRecord::Base
   def self.sterilize(params)
     trip = params[:trip]
 
-    start_date = trip[:start_date].split('T')[0]
-    start_date = DateRef.find_or_create_by!(date: start_date)
-
-    end_date = trip[:end_date].split('T')[0]
-    end_date = DateRef.find_or_create_by!(date: end_date)
-
-    bike = Bike.find_or_create_by!(bike: trip[:bike])
-
-    zipcode =
-      if  trip[:zipcode].nil?
-        nil
-      else
-        Zipcode.find_or_create_by!(zipcode: (trip[:zipcode][0..4]))
-      end
-
-    subscription = SubscriptionType.find_or_create_by!(sub_type: trip[:subscription])
-
-    start_station = Station.find(trip[:start_station].to_i)
-    end_station = Station.find(trip[:end_station].to_i)
-
-    { start_date: start_date, 
-    end_date: end_date, 
-    bike: bike, 
-    zipcode: zipcode, 
-    subscription: subscription, 
-    start_station: start_station , 
-    end_station: end_station }
+    { 
+      start_date: Trip.validate_date(trip[:start_date]), 
+      end_date: Trip.validate_date(trip[:end_date]), 
+      bike: Bike.find_or_create_by!(bike: trip[:bike]), 
+      zipcode: Trip.zip_validate(trip[:zipcode]), 
+      subscription: SubscriptionType.find_or_create_by!(sub_type: trip[:subscription]), 
+      start_station: Station.find(trip[:start_station].to_i) , 
+      end_station: Station.find(trip[:end_station].to_i) 
+    }
   end
 
+  def self.validate_date(date)
+    date = date.split('T')[0]
+    DateRef.find_or_create_by!(date: date)
+  end
+
+  def self.zip_validate(zipcode)
+    if  zipcode.nil?
+      nil
+    else
+      Zipcode.find_or_create_by!(zipcode: zipcode[0..4])
+    end
+  end
 
   def self.create_new(params)
     trip_data = sterilize(params)
-    # binding.pry
     Trip.create!(
             date_ref_id: trip_data[:start_date].id,
             end_date_id: trip_data[:end_date].id,
@@ -66,22 +56,14 @@ class Trip<ActiveRecord::Base
 
   def self.update_record(params)
     trip_data = sterilize(params)
-        Trip.update(params[:id],
-            date_ref_id: trip_data[:start_date].id,
-            end_date_id: trip_data[:end_date].id,
-            start_station_id: trip_data[:start_station].id,
-            end_station_id: trip_data[:end_station].id,
-            bike_id: trip_data[:bike].id,
-            zipcode_id: trip_data[:zipcode].nil? ? trip_data[:zipcode] : trip_data[:zipcode].id,
-            subscription_type_id: trip_data[:subscription].id)
-  end
-  
-  def month
-    self.date_ref.date.month
-  end
-
-  def year
-    self.date_ref.date.year
+    Trip.update(params[:id],
+        date_ref_id: trip_data[:start_date].id,
+        end_date_id: trip_data[:end_date].id,
+        start_station_id: trip_data[:start_station].id,
+        end_station_id: trip_data[:end_station].id,
+        bike_id: trip_data[:bike].id,
+        zipcode_id: trip_data[:zipcode].nil? ? trip_data[:zipcode] : trip_data[:zipcode].id,
+        subscription_type_id: trip_data[:subscription].id)
   end
 
   def self.dashboard
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -53,6 +53,134 @@ RSpec.describe Trip do
     it "is invalid without subscription id" do
       trip = Trip.new(date_ref_id: date.id, end_date_id: date2.id, start_station_id: station.id, end_station_id: station.id, bike_id: bike.id, zipcode_id: zipcode.id)
     end
+  end
 
+  describe "sterilize" do
+    it "creates hash of params data" do
+      date = DateRef.create(date: "2010-01-01")
+      date2 = DateRef.create(date: "2011-02-02")
+      city = City.create(name: "Denver")
+      station = Station.create!(name: "Broadway", dock_count: 12, lat: 1.2325, long: 12.54683, date_ref_id: date.id, city_id: city.id)
+      zipcode = Zipcode.create(zipcode: 81625)
+      bike = Bike.create(bike: 670)
+      subscription_type = SubscriptionType.create(sub_type: "weekly")
+
+      params = {
+        :trip =>
+          {
+            start_date: "2013-05-05",
+            end_date: "2013-05-05",
+            bike: "1001",
+            zipcode: "10010",
+            subscription: "weekly",
+            start_station: station.id,
+            end_station: station.id
+          }
+      }
+
+      result = Trip.sterilize(params)  
+
+      expect(result[:start_date].date.to_s).to eq('2013-05-05')
+      expect(result[:end_date].date.to_s).to eq('2013-05-05')
+      expect(result[:bike].bike).to eq(1001)
+      expect(result[:zipcode].zipcode).to eq(10010)
+      expect(result[:subscription].sub_type).to eq("weekly")
+      expect(result[:start_station].name).to eq('Broadway')
+      expect(result[:end_station].name).to eq('Broadway')
+    end
+  end
+
+  describe "update_record" do
+    it "creates updated hash of params data" do
+      date = DateRef.create(date: "2010-01-01")
+      date2 = DateRef.create(date: "2011-02-02")
+      city = City.create(name: "Denver")
+      station = Station.create!(name: "Broadway", dock_count: 12, lat: 1.2325, long: 12.54683, date_ref_id: date.id, city_id: city.id)
+      station1 = Station.create!(name: "Lawrence", dock_count: 12, lat: 1.2325, long: 12.54683, date_ref_id: date.id, city_id: city.id)
+      zipcode = Zipcode.create(zipcode: 81625)
+      bike = Bike.create(bike: 670)
+      subscription_type = SubscriptionType.create(sub_type: "weekly")
+      subscription_type2 = SubscriptionType.create(sub_type: "Monthly")
+      trip = Trip.create!(date_ref_id: date.id, end_date_id: date2.id, start_station_id: station.id, end_station_id: station.id, bike_id: bike.id, zipcode_id: zipcode.id, subscription_type_id: subscription_type.id)
+
+      params = {
+        :id => trip.id,
+        :trip =>
+          {
+            start_date: "2013-08-05",
+            end_date: "2013-05-04",
+            bike: "10012",
+            zipcode: "10012",
+            subscription: "Monthly",
+            start_station: station1.id,
+            end_station: station1.id
+          }
+      }
+
+      trip = Trip.update_record(params)  
+
+      
+      expect(trip.date_ref.date.to_s).to eq('2013-08-05')
+      expect(trip.end_date.date.to_s).to eq('2013-05-04')
+      expect(trip.bike.bike).to eq(10012)
+      expect(trip.zipcode.zipcode).to eq(10012)
+      expect(trip.subscription_type.sub_type).to eq("Monthly")
+      expect(trip.start_station.name).to eq('Lawrence')
+      expect(trip.end_station.name).to eq('Lawrence')
+    end
+  end
+  
+  describe "dashboard" do
+    it "creates hash of dashboard data" do
+      date1 = DateRef.create(date: "2010-01-01")
+      date2 = DateRef.create(date: "2011-02-02")
+      date3 = DateRef.create(date: "2012-03-02")
+
+      city = City.create(name: "San Francisco")
+      
+      station1 = Station.create!(name: "Broadway", dock_count: 12, lat: 1.2325, long: 12.54683, date_ref_id: date1.id, city_id: city.id)
+      station2 = Station.create!(name: "Lawrence", dock_count: 52, lat: 1.2325, long: 12.54683, date_ref_id: date2.id, city_id: city.id)
+      station3 = Station.create!(name: "Blake", dock_count: 21, lat: 1.2325, long: 12.54683, date_ref_id: date2.id, city_id: city.id)
+      station4 = Station.create!(name: "Turing", dock_count: 10, lat: 1.2325, long: 12.54683, date_ref_id: date3.id, city_id: city.id)
+
+      zipcode = Zipcode.create(zipcode: 81625)
+
+      bike1 = Bike.create(bike: 670)
+      bike2 = Bike.create(bike: 571)
+      bike3 = Bike.create(bike: 272)
+      
+
+
+      subscription_type1 = SubscriptionType.create(sub_type: "Customer")
+      subscription_type2 = SubscriptionType.create(sub_type: "Subscriber")
+
+      trip1 = Trip.create!(duration: 10, date_ref_id: date1.id, end_date_id: date3.id, start_station_id: station3.id, end_station_id: station2.id, bike_id: bike1.id, zipcode_id: zipcode.id, subscription_type_id: subscription_type1.id)
+      trip2 = Trip.create!(duration: 15, date_ref_id: date1.id, end_date_id: date3.id, start_station_id: station3.id, end_station_id: station2.id, bike_id: bike1.id, zipcode_id: zipcode.id, subscription_type_id: subscription_type1.id)
+      trip3 = Trip.create!(duration: 20, date_ref_id: date2.id, end_date_id: date3.id, start_station_id: station3.id, end_station_id: station2.id, bike_id: bike1.id, zipcode_id: zipcode.id, subscription_type_id: subscription_type1.id)
+      trip4 = Trip.create!(duration: 20000, date_ref_id: date2.id, end_date_id: date3.id, start_station_id: station3.id, end_station_id: station2.id, bike_id: bike1.id, zipcode_id: zipcode.id, subscription_type_id: subscription_type1.id)
+      trip5 = Trip.create!(duration: 1000000, date_ref_id: date3.id, end_date_id: date3.id, start_station_id: station1.id, end_station_id: station1.id, bike_id: bike2.id, zipcode_id: zipcode.id, subscription_type_id: subscription_type1.id)
+      trip6 = Trip.create!(duration: 20000, date_ref_id: date3.id, end_date_id: date3.id, start_station_id: station2.id, end_station_id: station3.id, bike_id: bike2.id, zipcode_id: zipcode.id, subscription_type_id: subscription_type2.id)
+      trip7 = Trip.create!(duration: 15000, date_ref_id: date2.id, end_date_id: date3.id, start_station_id: station4.id, end_station_id: station4.id, bike_id: bike2.id, zipcode_id: zipcode.id, subscription_type_id: subscription_type2.id)
+      trip8 = Trip.create!(duration: 50000, date_ref_id: date2.id, end_date_id: date3.id, start_station_id: station1.id, end_station_id: station3.id, bike_id: bike3.id, zipcode_id: zipcode.id, subscription_type_id: subscription_type2.id)
+
+      dashboard = Trip.dashboard  
+
+      
+      expect(dashboard[:average_duration_ride]).to eq(138130)
+      expect(dashboard[:longest_ride].values).to eq([1000000])
+      expect(dashboard[:shortest_ride].values).to eq([10])
+      expect(dashboard[:popular_starting_station]).to eq('Blake')
+      expect(dashboard[:popular_ending_station]).to eq('Lawrence')
+      expect(dashboard[:month_breakdown]).to eq([{2012.0=>{3.0=>2}}, 
+                                                 {2013.0=>{8.0=>1}}, 
+                                                 {2011.0=>{2.0=>4}}, 
+                                                 {2010.0=>{1.0=>2}}])
+      expect(dashboard[:most_ridden_bike]).to eq(670)
+      expect(dashboard[:least_ridden_bike]).to eq(10012)
+      expect(dashboard[:subscription_breakout]).to eq({"Customer"=>[5, 0.56], "Subscriber"=>[3, 0.33], "Monthly"=>[1, 0.11]})
+      expect(dashboard[:top_trip_date][0].to_s).to eq("2011-02-02")
+      expect(dashboard[:lowest_trip_date][0].to_s).to eq("2013-08-05")
+ 
+    end
   end
 end

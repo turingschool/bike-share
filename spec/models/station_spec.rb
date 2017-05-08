07fd4c3d1220fb7cef34b3d8ed84950f3be197e1
Chris Smith <42@chrisryansmith.com>
2017-05-08 09:51:16 -0600

    wrap other model tests in describe blocks

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -2,52 +2,56 @@ require './spec/spec_helper'
 
 
 RSpec.describe Station do
-  it "has all attributes" do
-    station = Station.create(name: "something", dock_count: 1, city_id: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
+  describe "station can return all attributes" do
+    it "has all attributes" do
+      station = Station.create(name: "something", dock_count: 1, city_id: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
 
-    expect(station.id).to eq 1
-    expect(station.name).to eq "something"
-    expect(station.dock_count).to eq 1
-    expect(station.city_id).to eq 1
-    expect(station.installation_date).to eq Date.strptime("08/30/2013",'%m/%d/%Y')
-    expect(station.longitude).to eq -121.9
-    expect(station.latitude).to eq 30.7
+      expect(station.id).to eq 1
+      expect(station.name).to eq "something"
+      expect(station.dock_count).to eq 1
+      expect(station.city_id).to eq 1
+      expect(station.installation_date).to eq Date.strptime("08/30/2013",'%m/%d/%Y')
+      expect(station.longitude).to eq -121.9
+      expect(station.latitude).to eq 30.7
+    end
   end
 
-  it "is invalid without a name" do
-    station = Station.create(dock_count: 1, city_id: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
+  describe "station requires all necessary fields" do
+    it "is invalid without a name" do
+      station = Station.create(dock_count: 1, city_id: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
 
-    expect(station).to be_invalid
-  end
+      expect(station).to be_invalid
+    end
 
-  it "is invalid without a dock_count" do
-    station = Station.create(name: "something", city_id: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
+    it "is invalid without a dock_count" do
+      station = Station.create(name: "something", city_id: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
 
-    expect(station).to be_invalid
-  end
+      expect(station).to be_invalid
+    end
 
-  it "is invalid without a city_id" do
-    station = Station.create(name: "something", dock_count: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
+    it "is invalid without a city_id" do
+      station = Station.create(name: "something", dock_count: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
 
-    expect(station).to be_invalid
-  end
+      expect(station).to be_invalid
+    end
 
-  it "is invalid without a installation_date" do
-    station = Station.create(name: "something", city_id: 1, dock_count: 1, longitude: -121.9, latitude: 30.7)
+    it "is invalid without a installation_date" do
+      station = Station.create(name: "something", city_id: 1, dock_count: 1, longitude: -121.9, latitude: 30.7)
 
-    expect(station).to be_invalid
-  end
+      expect(station).to be_invalid
+    end
 
-  it "is invalid without a longitude" do
-    station = Station.create(name: "something", city_id: 1, dock_count: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), latitude: 30.7)
+    it "is invalid without a longitude" do
+      station = Station.create(name: "something", city_id: 1, dock_count: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), latitude: 30.7)
 
-    expect(station).to be_invalid
-  end
+      expect(station).to be_invalid
+    end
 
-  it "is invalid without a latitude" do
-    station = Station.create(name: "something", city_id: 1, dock_count: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9)
+    it "is invalid without a latitude" do
+      station = Station.create(name: "something", city_id: 1, dock_count: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9)
 
-    expect(station).to be_invalid
+      expect(station).to be_invalid
+    end
   end
 
   describe "station can calculate dashboard information" do

c237ca1d31f57f3bdd162787a8dd63825849b16f
Aaron Olsen <mraaronolsen@gmail.com>
2017-05-07 19:08:56 -0600

    Seed to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
 require 'CSV'
+require './app/models/city.rb'
 
 stations = CSV.open './db/csv/station.csv', headers: true, header_converters: :symbol
 
+binding.pry
 stations.each do |row|
   city = City.find_or_create_by(name: row[:city])
 

c04f0b975123fa37203a117756880152f20eab5d
Rhonda <rwilhel@gmail.com>
2017-05-08 12:12:57 -0600

    Progress on Trips controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,5 +1,21 @@
 class TripsController < BikeShareApp
   set :views, File.join(APP_ROOT, "app", "views", "trips")
 
-  
+  get '/trips/new' do
+    @trip = Trip.new
+    erb :'new'
+  end
+
+  get '/trips/:id' do |id|
+    @trip = Trip.find(id)
+    erb :'show'
+  end
+
+  put '/trips/:id' do |id|
+    trip_details = params[:trip]
+    trip_details[:start_date] = Date.strptime("08/30/2013 11:11",'%m/%d/%Y %H:%M')
+  end
+
+
+
 end
--- spec/features/trips_features/user_can_delete_a_trip_spec.rb
+++ spec/features/trips_features/user_can_delete_a_trip_spec.rb
@@ -18,9 +18,9 @@ RSpec.describe "When a user clicks delete" do
     zipcode = Zipcode.create(zipcode: 94127)
     trip = Trip.create(
                       duration: 240
-                      start_date: DATE.strptime("08/30/2013 11:11",'%m/%d/%Y %H:%M'),
+                      start_date: Date.strptime("08/30/2013 11:11",'%m/%d/%Y %H:%M'),
                       start_station_id: 1,
-                      end_date: DATE.strptime("08/30/2013 11:15",'%m/%d/%Y %H:%M'),
+                      end_date: Date.strptime("08/30/2013 11:15",'%m/%d/%Y %H:%M'),
                       end_station_id: 1,
                       bike_id: 5,
                       subscription_type: "Subscriber",
@@ -53,9 +53,9 @@ RSpec.describe "When a user clicks delete" do
     zipcode = Zipcode.create(zipcode: 94127)
     trip = Trip.create(
                       duration: 240
-                      start_date: DATE.strptime("08/30/2013 11:11",'%m/%d/%Y %H:%M'),
+                      start_date: Date.strptime("08/30/2013 11:11",'%m/%d/%Y %H:%M'),
                       start_station_id: 1,
-                      end_date: DATE.strptime("08/30/2013 11:15",'%m/%d/%Y %H:%M'),
+                      end_date: Date.strptime("08/30/2013 11:15",'%m/%d/%Y %H:%M'),
                       end_station_id: 1,
                       bike_id: 5,
                       subscription_type: "Subscriber",
@@ -87,9 +87,9 @@ RSpec.describe "When a user clicks delete" do
     zipcode = Zipcode.create(zipcode: 94127)
     trip = Trip.create(
                       duration: 240
-                      start_date: DATE.strptime("08/30/2013 11:11",'%m/%d/%Y %H:%M'),
+                      start_date: Date.strptime("08/30/2013 11:11",'%m/%d/%Y %H:%M'),
                       start_station_id: 1,
-                      end_date: DATE.strptime("08/30/2013 11:15",'%m/%d/%Y %H:%M'),
+                      end_date: Date.strptime("08/30/2013 11:15",'%m/%d/%Y %H:%M'),
                       end_station_id: 1,
                       bike_id: 5,
                       subscription_type: "Subscriber",

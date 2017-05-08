9e62b6533f8e4417e98a23ff790c13e6b75367e5
Alex Koch <alex549us3@gmail.com>
2017-05-08 09:35:00 -0600

    adds some of the methods, probably not the best way, but working for now.

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,4 +1,18 @@
 class Station < ActiveRecord::Base
   belongs_to :city
+
+  def self.average_available_bikes
+    sum = 0
+    self.all.each do |station|
+      sum += station.dock_count
+    end
+    (sum / self.count)
+  end
+
+  def self.max_available_bikes
+    self.all.max_by(3) do |station|
+      station.dock_count
+    end
+  end
 end
 

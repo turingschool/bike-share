3ecfea54e8195c2aeb08791dbe05ff8695072fde
Thomas Soucie <tsoucie1858@gmail.com>
2017-05-09 17:49:08 -0600

    updated user_wants_to_add_station feature testing to correct test expect formatting

--- spec/features/stations/user_wants_to_add_new_station_spec.rb
+++ spec/features/stations/user_wants_to_add_new_station_spec.rb
@@ -21,22 +21,23 @@ RSpec.describe "user wants to add station" do
       expect(page).to have_content('May 8 2013')
   end
 
-  it "adds a station without a name" do
+  it "and adds without a name" do
     visit('/stations/new')
 
-      fill_in "station[dock_count]", with: 1981
-      fill_in "city", with: 'Denver'
-      fill_in "station[install_date]", with: '2008/10/21'
+    fill_in "station[dock_count]", with: 19
+    fill_in "city", with: 'Denver'
+    fill_in "station[install_date]", with: '2008/10/21'
 
-      click_button "Add Station"
-      station = Station.last
+    click_button "Add Station"
+    save_and_open_page
+    station = Station.last
 
-      expect(current_path).to eq("/stations/new")
+    expect(current_path).to eq("/stations/new")
 
-      expect(page).to have_content('ENTER VALUE HERE!!!')
-      expect(page).to have_content('Denver')
-      expect(page).to have_content(1981)
-      expect(page).to have_content('2008-10-21') 
+    expect(page).to have_field("station[name]", with: "can't be blank")
+    expect(page).to have_field('city', with: "Denver")
+    expect(page).to have_field("station[dock_count]", with: "19")
+    expect(page).to have_field("station[install_date]", with: "2008-10-21 00:00:00 UTC") 
   end
 
 end

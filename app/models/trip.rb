6463bcd5bfffca0f5b0ac66d8ea551e96c0e1b1a
William Thomas <wthoma390@gmail.com>
2017-05-09 14:49:05 -0600

    add method names and calculations

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -12,4 +12,85 @@ class Trip < ActiveRecord::Base
     end
     first_30_trips
   end
+
+  #Trips Dashboard methods for Info
+
+  def self.average_duration_of_a_ride
+    average(:duration).to_i / 60
+  end
+
+  def self.longest_ride
+    maximum(:duration).to_i / 60
+  end
+
+  def self.shortest_ride
+    minimum(:duration).to_i / 60
+  end
+
+  def self.station_with_most_starting_place_rides
+    group(:start_station).count("id").max_by do |station, count|
+      count
+    end
+  end
+
+  def self.station_with_most_ending_place_rides
+    group(:end_station).count("id").max_by do |station, count|
+      count
+    end
+  end
+
+  def self.month_by_month_breakdown_with_subtotals_by_year
+
+  end
+
+  def self.most_ridden_bike
+
+  end
+
+  def self.least_ridden_bike
+
+  end
+
+  def self.user_subscription_type_with_count_and_percentage
+
+  end
+
+  def self.highest_number_of_trips_by_date
+
+  end
+
+  def self.lowest_number_of_trips_by_date
+
+  end
+
+  #Individual station methods for Info
+
+  def self.number_of_rides_started_at_station
+
+  end
+
+  def self.number_of_rides_ended_at_station
+
+  end
+
+  def self.most_frequest_starting_station
+
+  end
+
+  def self.least_frequest_starting_station
+
+  end
+
+  def self.highest_number_of_trips_of_stations_by_date
+
+  end
+
+  def self.most_frequesnt_zip_code_for_starting_station
+
+  end
+
+  def self.most_frequenst_bike_id_for_starting_station
+
+  end
+
 end

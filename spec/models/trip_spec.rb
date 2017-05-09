ddc3846a0ebcf22c3e2f3517dc62f7d0732ae709
Sam Lim <limsammy1@gmail.com>
2017-05-09 15:23:37 -0600

    Started writing tests for trip calcs, added fixture

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -106,4 +106,48 @@ RSpec.describe Trip, :type => :model do
     end
   end
 
+  describe "trip calculations" do
+    before(:each) do
+      station_data = CSVLoader.new.sanitize_station('./db/csv/station.csv')
+      station_data.each do |station|
+        if City.find_by(name: station[:city]).nil?
+          city = City.create(name: station[:city])
+        else
+          city = City.find_by(name: station[:city])
+        end
+
+        Station.create(id: station[:id],
+          name: station[:name],
+          dock_count: station[:dock_count],
+          city_id: city.id,
+          date: station[:date]
+          )
+      end
+
+      trip_data = CSVLoader.new.sanitize_trips('./spec/fixtures/trip_fixture.csv')
+      trip_data.each do |trip|
+        start_station = Station.find(trip[:start_station_id])
+        end_station = Station.find(trip[:start_station_id])
+
+        Trip.create(id: trip[:id],
+          duration: trip[:duration],
+          start_date: trip[:start_date],
+          start_station: start_station,
+          end_date: trip[:end_date],
+          end_station: end_station,
+          bike_id: trip[:bike_id],
+          subscription_type: trip[:subscription_type],
+          zip_code: trip[:zip_code]
+          )
+      end
+    end
+
+    it 'calculates average duration of a ride' do
+      expect(Trip.average_duration_of_a_ride).to eq(394)
+    end
+
+    it 'calculates the longest ride' do
+      expect(Trip.longest_ride).to eq(528)
+    end
+  end
 end

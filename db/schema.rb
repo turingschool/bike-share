7c021a0954e62f9c38cf79d7931152b6f5079011
Sam Lim <limsammy1@gmail.com>
2017-05-08 20:39:40 -0600

    Updated schema for migration20170509023655

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170508201547) do
+ActiveRecord::Schema.define(version: 20170509023655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 20170508201547) do
     t.integer  "end_station_id"
     t.integer  "bike_id"
     t.string   "subscription_type"
-    t.integer  "zip_code"
+    t.bigint   "zip_code"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
   end

cff52fb04f553ae7b09f0d714752fe48355a9a62
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-05-09 05:53:24 -0600

    schema and models in place for weather

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,6 @@
 class City < ActiveRecord::Base
   has_many :stations
+  has_many :weather_statistic
   validates :name, presence: true
 
 end
--- app/models/weather_statistic.rb
+++ app/models/weather_statistic.rb
@@ -1,4 +1,16 @@
 class WeatherStatistic < ActiveRecord::Base
   belongs_to :date_ref
-  belongs_to :zipcode
+  belongs_to :city
+
+  def self.key_zip_to_city
+    {
+      '94107'=> 'San Francisco',
+      '94063'=> 'Redwood City', 
+      '94301'=> 'Palo Alto', 
+      '94041'=> 'Mountain View', 
+      '95113'=> 'San Jose'
+    }
+  
+  end
+
 end

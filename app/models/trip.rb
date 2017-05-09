65d7e4c473b83d377f6474a5ecc3c1e188ce12bd
Sam Lim <limsammy1@gmail.com>
2017-05-09 15:53:04 -0600

    removed zip code validation

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class Trip < ActiveRecord::Base
-  validates :duration, :start_date, :start_station_id, :end_date, :end_station_id, :bike_id, :subscription_type, :zip_code, presence: true
+  validates :duration, :start_date, :start_station_id, :end_date, :end_station_id, :bike_id, :subscription_type, presence: true
 
   belongs_to :start_station, :class_name => "Station"
   belongs_to :end_station, :class_name => "Station"

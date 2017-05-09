9be7e6ae749fe869f6a1aa4f03aa218becaea323
Sam Lim <limsammy1@gmail.com>
2017-05-08 22:19:51 -0600

    Fixed seeding bug with table data. Run rake:db:reset to repopulate the db

--- app/models/csv_loader.rb
+++ app/models/csv_loader.rb
@@ -29,10 +29,11 @@ class CSVLoader
     data.map do |row|
       # binding.pry
       {
-        [:name] => row[:name],
-        [:dock_count] => row[:dock_count].to_i,
-        [:city] => row[:city],
-        [:date] => row[:installation_date] # Should we make this an integer for calcs?
+        :id => row[:id],
+        :name => row[:name],
+        :dock_count => row[:dock_count].to_i,
+        :city => row[:city],
+        :date => row[:installation_date] # Should we make this an integer for calcs?
       }
     end
   end
@@ -42,14 +43,15 @@ class CSVLoader
 
     data.map do |row|
       {
-        [:duration] => row[:duration].to_i,
-        [:start_date] => row[:start_date],
-        [:start_station_id] => row[:start_station_id].to_i,
-        [:end_date] => row[:end_date],
-        [:end_station_id] => row[:end_station_id].to_i,
-        [:bike_id] => row[:bike_id].to_i,
-        [:subscription_type] => row[:subscription_type],
-        [:zip_code] => row[:zip_code].to_i
+        :id => row[:id],
+        :duration => row[:duration],
+        :start_date => row[:start_date],
+        :start_station_id => row[:start_station_id].to_i,
+        :end_date => row[:end_date],
+        :end_station_id => row[:end_station_id].to_i,
+        :bike_id => row[:bike_id].to_i,
+        :subscription_type => row[:subscription_type].downcase,
+        :zip_code => row[:zip_code].to_i
       }
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,31 +8,39 @@ loader = CSVLoader.new
 
 station_data = loader.sanitize_station('./db/csv/station.csv')
 station_data.each do |station|
-  puts "Seeding db_stations with: #{station[[:name]]}"
+  puts "Seeding db_stations with: #{station[:name]}"
 
-  if City.find_by(name: station[[:city]]).nil?
-    city = City.create(name: station[[:city]])
+  if City.find_by(name: station[:city]).nil?
+    city = City.create(name: station[:city])
   else
-    city = City.find_by(name: station[[:city]])
+    city = City.find_by(name: station[:city])
   end
 
-  Station.create(name: station[[:name]],
-                  dock_count: station[[:dock_count]],
+  Station.create(id: station[:id],
+                  name: station[:name],
+                  dock_count: station[:dock_count],
                   city_id: city.id,
-                  date: station[[:date]]
+                  date: station[:date]
                   )
 end
 
-# trip_data = loader.sanitize_trips('./db/csv/trip.csv')
-# trip_data.each do |trip|
-#   puts "Seeding db_trips with trip_id #{trip[[:id]]}"
+trip_data = loader.sanitize_trips('./db/csv/trip.csv')
+count = 1
+trip_data.each do |trip|
+  count += 1
+  puts "Seeding db_trips count: #{count}"
 
-#   Trip.create(duration: trip[[:duration]],
-#                start_date: trip[[:start_date]],
-#                start_station: trip[[:start_station_id]],
-#                end_date: trip[[:end_date]],
-#                end_station: trip[[:end_station_id]],
-#                bike_id: trip[[:bike_id]],
-#                zip_code: trip[[:zip_code]]
-#                )
-# end
+  start_station = Station.find(trip[:start_station_id])
+  end_station = Station.find(trip[:start_station_id])
+
+  Trip.create(id: trip[:id],
+              duration: trip[:duration],
+              start_date: trip[:start_date],
+              start_station: start_station,
+              end_date: trip[:end_date],
+              end_station: end_station,
+              bike_id: trip[:bike_id],
+              subscription_type: trip[:subscription_type],
+              zip_code: trip[:zip_code]
+              )
+end

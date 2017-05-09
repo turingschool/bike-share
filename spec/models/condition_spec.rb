bdff41eb83ba5ad85322a76327049aa13499d1bf
squeemishly <squeemishly@gmail.com>
2017-05-09 16:41:38 -0600

    added testing back into condition spec file

--- spec/models/condition_spec.rb
+++ spec/models/condition_spec.rb
@@ -29,6 +29,99 @@ RSpec.describe Condition do
     expect(condition).to be_invalid
   end
 
+  it "doesn't work without a max_temp" do
+     condition = Condition.create(
+                                 date: Date.strptime("08/30/2013",'%m/%d/%Y'),
+                                 mean_temp: 76.0,
+                                 min_temp: 54.0,
+                                 mean_humidity: 90.0,
+                                 mean_visibility: 10.0,
+                                 mean_wind_speed: 11.0,
+                                 precipitation: 0,
+                                 )
+     expect(condition).to be_invalid
+   end
+
+   it "doesn't work without a mean_temp" do
+     condition = Condition.create(
+                                 date: Date.strptime("08/30/2013",'%m/%d/%Y'),
+                                 max_temp: 87.0,
+                                 min_temp: 54.0,
+                                 mean_humidity: 90.0,
+                                 mean_visibility: 10.0,
+                                 mean_wind_speed: 11.0,
+                                 precipitation: 0,
+                                 )
+     expect(condition).to be_invalid
+   end
+
+   it "doesn't work without a min_temp" do
+     condition = Condition.create(
+                                 date: Date.strptime("08/30/2013",'%m/%d/%Y'),
+                                 max_temp: 87.0,
+                                 mean_temp: 76.0,
+                                 mean_humidity: 90.0,
+                                 mean_visibility: 10.0,
+                                 mean_wind_speed: 11.0,
+                                 precipitation: 0,
+                                 )
+     expect(condition).to be_invalid
+   end
+
+   it "doesn't work without a mean_humidity" do
+     condition = Condition.create(
+                                 date: Date.strptime("08/30/2013",'%m/%d/%Y'),
+                                 max_temp: 87.0,
+                                 mean_temp: 76.0,
+                                 min_temp: 54.0,
+                                 mean_visibility: 10.0,
+                                 mean_wind_speed: 11.0,
+                                 precipitation: 0,
+                                 )
+     expect(condition).to be_invalid
+   end
+
+   it "doesn't work without a humidity" do
+     condition = Condition.create(
+                                 date: Date.strptime("08/30/2013",'%m/%d/%Y'),
+                                 max_temp: 87.0,
+                                 mean_temp: 76.0,
+                                 min_temp: 54.0,
+                                 mean_visibility: 10.0,
+                                 mean_wind_speed: 11.0,
+                                 precipitation: 0,
+                                 )
+
+     expect(condition).to be_invalid
+   end
+
+   it "doesn't work without a mean_wind_speed" do
+     condition = Condition.create(
+                                 date: Date.strptime("08/30/2013",'%m/%d/%Y'),
+                                 max_temp: 87.0,
+                                 mean_temp: 76.0,
+                                 min_temp: 54.0,
+                                 mean_humidity: 90.0,
+                                 mean_visibility: 10.0,
+                                 precipitation: 0,
+                                 )
+     expect(condition).to be_invalid
+   end
+
+   it "doesn't work without a precipitation" do
+     condition = Condition.create(
+                                 date: Date.strptime("08/30/2013",'%m/%d/%Y'),
+                                 max_temp: 87.0,
+                                 mean_temp: 76.0,
+                                 min_temp: 54.0,
+                                 mean_humidity: 90.0,
+                                 mean_visibility: 10.0,
+                                 mean_wind_speed: 11.0
+                                 )
+     expect(condition).to be_invalid
+   end
+ 
+
   it "returns 'selected' when passed its zipcode id" do
     condition = Condition.create(
                                 date: Date.strptime("08/30/2013",'%m/%d/%Y'),

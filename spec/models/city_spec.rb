ab62b0ece5c7b9d71f409992d982f50de964ff77
Chris Smith <42@chrisryansmith.com>
2017-05-10 08:33:18 -0600

    replace city with let in model

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,9 +1,8 @@
 require './spec/spec_helper'
 
 RSpec.describe City do
+  let(:city) { City.create(name:"squeevillia") }
   it "has a name attribute" do
-    city = City.create(name:"squeevillia")
-
     expect(city.id).to eq 1
     expect(city.name).to eq "squeevillia"
   end
@@ -15,10 +14,8 @@ RSpec.describe City do
   end
 
   it "city has stations" do
-    city = City.create(name: "squeevillia")
     station = city.stations.create(dock_count: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
 
     expect(station.city.name).to eq "squeevillia"
-
   end
 end

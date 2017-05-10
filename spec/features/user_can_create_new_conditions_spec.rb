529d330b4594e2793b61c86deb19916168dc5b74
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-05-10 16:11:22 -0600

    test errors fixed from sad paths

--- spec/features/user_can_create_new_conditions_spec.rb
+++ spec/features/user_can_create_new_conditions_spec.rb
@@ -3,6 +3,7 @@ require_relative '../spec_helper'
 RSpec.describe "When a user visits '/conditions/new'" do
   it "user sees a 'new' form to create a new condition" do
     date = DateRef.create!(date: "2010-01-01")
+    date2 = DateRef.create!(date: "2010-02-01")
     WeatherStatistic.create!(max_temperature: 1, mean_temperature: 1, min_temperature: 1, mean_humidity: 1, mean_visibility: 1, mean_wind_speed:1, precipitation:1, date_ref_id: 1)
 
     visit('/conditions/new')
@@ -14,11 +15,12 @@ RSpec.describe "When a user visits '/conditions/new'" do
     fill_in("weather[mean_visibility]", with: 60)
     fill_in("weather[mean_wind_speed]", with: 60)
     fill_in("weather[precipitation]", with: 60)
-    fill_in("weather[date_ref_id]", with: "2010-01-01")
-
+    fill_in("weather[date_ref_id]", with: '2010-02-01')
     click_on("Create New Weather")
 
-    expect(current_path).to eq('/conditions/2')
+    weather = WeatherStatistic.find_by(date_ref_id: 2)
+
+    expect(current_path).to eq("/conditions/#{weather.id}")
     expect(page).to have_content("Maximum Temperature")
     expect(page).to have_content("Average Temperature")
     expect(page).to have_content("Minimum Temperature")

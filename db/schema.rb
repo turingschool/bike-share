fd1f7496f63ecbfe1a362b0f3e9b77e5e456db32
Alex Koch <alex549us3@gmail.com>
2017-05-10 13:04:17 -0600

    migration update and models updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170510032858) do
+ActiveRecord::Schema.define(version: 20170510165002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,18 @@ ActiveRecord::Schema.define(version: 20170510032858) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.integer  "city_id"
+  end
+
+  create_table "trips", force: :cascade do |t|
+    t.integer  "duration"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.integer  "end_station_id"
+    t.integer  "start_station_id"
+    t.integer  "subscription_type_id"
+    t.integer  "zip_code"
+    t.integer  "city_id"
+    t.integer  "bike_id"
   end
 
 end

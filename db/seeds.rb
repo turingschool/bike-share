dd6167b63beed0c8498ab3fed02b5513ef6d3262
Sam Lim <limsammy1@gmail.com>
2017-05-10 06:26:45 -0600

    Condition seeding works, run rake db:seed to populate

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@ require_relative '../app/models/csv_loader'
 require_relative '../app/models/station'
 require_relative '../app/models/city'
 require_relative '../app/models/trip'
+require_relative '../app/models/condition'
 
 loader = CSVLoader.new
 
@@ -25,7 +26,7 @@ station_data.each do |station|
 end
 
 trip_data = loader.sanitize_trips('./db/csv/trip.csv')
-count = 1
+count = 0
 trip_data.each do |trip|
   count += 1
   puts "Seeding db_trips count: #{count}"
@@ -43,4 +44,19 @@ trip_data.each do |trip|
               subscription_type: trip[:subscription_type],
               zip_code: trip[:zip_code]
               )
+end
+
+weather_data = loader.sanitize_weather('./db/csv/weather.csv')
+count = 0
+weather_data.each do |condition|
+  count += 1
+  puts "Seeding db_conditions count: #{count}"
+  Condition.create(max_temperature: condition[:max_temperature],
+                   mean_temperature: condition[:mean_temperature],
+                   min_temperature: condition[:min_temperature],
+                   mean_humidity: condition[:mean_humidity],
+                   mean_visibility: condition[:mean_visibility],
+                   mean_wind_speed: condition[:mean_wind_speed],
+                   precipitation: condition[:precipitation]
+                   )
 end

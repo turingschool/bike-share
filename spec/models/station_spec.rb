62fe7ee3853a9fc255c8b6d290a2171762020ea2
Thomas Soucie <tsoucie1858@gmail.com>
2017-05-10 11:39:23 -0600

    Commits max_bike_station method model test for station

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -13,10 +13,16 @@ RSpec.describe Station do
     it "returns the maximum number of bikes at a station" do
       Loader.new('./spec/fixtures/station_fixtures.csv').load_stations
 
-      expect(Station.max_available_bikes).to eq(37)
-
+      expect(Station.max_available_bikes).to eq(27)
     end
   end
 
+  describe ".max_bike_station" do
+    it "returns the station with maximum number available bikes" do
+      Loader.new('./spec/fixtures/station_fixtures.csv').load_stations
 
+      expect(Station.max_bike_station).to eq("San Jose Diridon Caltrain Station")
+    end
+  end
+  
 end

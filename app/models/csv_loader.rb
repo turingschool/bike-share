b1393ecfeaa69b9122feb467bcdef874202dc338
Sam Lim <limsammy1@gmail.com>
2017-05-09 23:01:29 -0600

    sanitize_weather method passes spec

--- app/models/csv_loader.rb
+++ app/models/csv_loader.rb
@@ -55,4 +55,24 @@ class CSVLoader
       }
     end
   end
+
+  def sanitize_weather(path)
+    data = CSV.open(path, headers: true, header_converters: :symbol)
+
+    data.map do |row|
+      if row[:precipitation_inches] == 'T'
+        row[:precipitation_inches] = 0
+      end
+      {
+        :date => row[:date],
+        :max_temperature => row[:max_temperature],
+        :mean_temperature => row[:mean_temperature],
+        :min_temperature => row[:min_temperature],
+        :mean_humidity => row[:mean_humidity],
+        :mean_visibility => row[:mean_visibility],
+        :mean_wind_speed => row[:mean_wind_speed],
+        :precipitation => row[:precipitation_inches]
+      }
+    end
+  end
 end

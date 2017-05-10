c091f655fcb587cf27749c92c328cf0c75ae924a
adamgunther1 <adamgunther1@gmail.com>
2017-05-10 17:00:14 -0600

    advances model tests

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -141,40 +141,58 @@ RSpec.describe Trip do
     end
   end
 
-  # describe "station can calculate dashboard information" do
-  #   before {
-  #     Station.create(name: "something", dock_count: 1, city_id: 1, installation_date: Date.strptime("08/30/2015",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-  #     Station.create(name: "else", dock_count: 2, city_id: 1, installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-  #     Station.create(name: "other", dock_count: 3, city_id: 1, installation_date: Date.strptime("08/30/2014",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-  #     Station.create(name: "and again", dock_count: 4, city_id: 1, installation_date: Date.strptime("08/30/2014",'%m/%d/%Y'), longitude: -121.9, latitude: 30.7)
-  #   }
-  #   it "can calculate the average number of bikes for all stations" do
-  #     expect(Station.average_bikes).to eq(2.5)
-  #   end
+  describe "trip can calculate dashboard information" do
+    before {
+      zipcode = Zipcode.create(zipcode: 94127)
+      station_1 = Station.create(
+                      dock_count: 1, 
+                      city_id: 1, 
+                      installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), 
+                      longitude: -121.9, 
+                      latitude: 30.7)
+      station_2 = Station.create(
+                      dock_count: 2, 
+                      city_id: 1, 
+                      installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'), 
+                      longitude: -120.9, 
+                      latitude: 30.9)                      
+      trip_1 = Trip.create(
+                      duration: 240,
+                      start_date: DateTime.strptime("08/30/2013 11:11", "%m/%d/%Y %H:%M"),
+                      start_station_id: 1,
+                      end_date: DateTime.strptime("08/30/2013 11:15", "%m/%d/%Y %H:%M"),
+                      end_station_id: 1,
+                      bike_id: 5,
+                      subscription_type: "Subscriber",
+                      zipcode_id: 1
+    )
+      trip_2 = Trip.create(
+                      duration: 240,
+                      start_date: DateTime.strptime("08/30/2013 11:11", "%m/%d/%Y %H:%M"),
+                      start_station_id: 1,
+                      end_date: DateTime.strptime("08/30/2013 11:15", "%m/%d/%Y %H:%M"),
+                      end_station_id: 1,
+                      bike_id: 6,
+                      subscription_type: "Subscriber",
+                      zipcode_id: 1
+    )
+      trip_3 = Trip.create(
+                      duration: 240,
+                      start_date: DateTime.strptime("08/30/2013 11:11", "%m/%d/%Y %H:%M"),
+                      start_station_id: 2,
+                      end_date: DateTime.strptime("08/30/2013 11:15", "%m/%d/%Y %H:%M"),
+                      end_station_id: 3,
+                      bike_id: 6,
+                      subscription_type: "Subscriber",
+                      zipcode_id: 1
+    )
+    }
+    it "can calculate the average number of duration for all rides" do
+      expect(Trip.average_duration).to eq()
+    end
 
-  #   it "can calculate the most bikes at any stations" do
-  #     expect(Station.most_bikes_count).to eq(4)
-  #   end
 
-  #   it "can calculate which stations have the most bikes" do
-  #     expect(Station.most_bikes_avail_at).to eq('and again')
-  #   end
-
-  #   it "can calculate the fewest bikes at any stations" do
-  #     expect(Station.fewest_bikes_count).to eq(1)
-  #   end
-
-  #   it "can calculate which stations have the fewest bikes" do
-  #     expect(Station.fewest_bikes_avail_at).to eq('something')
-  #   end
-
-  #   it "can find the oldest station created" do
-  #     expect(Station.oldest_station).to eq('else')
-  #   end
-
-  #   it "can find the newewst station created" do
-  #     expect(Station.newest_station).to eq('something')
-  #   end
-  # end
+  end
+  
 
 end

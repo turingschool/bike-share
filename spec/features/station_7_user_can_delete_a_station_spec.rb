257132e5089bb7e62e7a404bd114ba635aa6b843
MasonHolland <masontholland@gmail.com>
2017-05-09 19:54:36 -0600

    passing tests. fixed bugs in station feature tests. added data analysis to individual station page

--- spec/features/station_7_user_can_delete_a_station_spec.rb
+++ spec/features/station_7_user_can_delete_a_station_spec.rb
@@ -8,6 +8,19 @@ require_relative '../spec_helper'
 RSpec.describe "user can delete a station" do
   it "takes you to view all page and entry is deleted" do
     city = City.create(name: "Vancover")
+    StartStation.create(station_id: 1)
+    EndStation.create(station_id: 1)
+    subscription_type = SubscriptionType.create(name: "Customer")
+    ride_date = RideDate.find_or_create_by(day: 12, month: 8, year: 2012)
+    StartDate.create(ride_date_id: 1)
+    EndDate.create(ride_date_id: 1)
+    trip = subscription_type.trips.create(duration: 400,
+                              start_station_id: 1,
+                              end_station_id: 1,
+                              start_date_id: 1,
+                              end_date_id: 1,
+                              bike_id: 122,
+                              zip_code: 80218)
     city.stations.create(name: "MLK", dock_count: 35, installation_date: "2010-11-12")
     city.stations.create(name: "Malcolm X", dock_count: 35, installation_date: "2011-12-11")
 

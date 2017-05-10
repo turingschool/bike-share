3261cba45168adf40c428d1225ed781281c55fd8
Thomas Soucie <tsoucie1858@gmail.com>
2017-05-10 11:36:05 -0600

    Commits max_available station model test

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -3,9 +3,20 @@ require './spec/spec_helper'
 RSpec.describe Station do
   describe ".average_available_bikes" do
     it "returns the average available bikes per station" do
-      #Load data
+      Loader.new('./spec/fixtures/station_fixtures.csv').load_stations
 
-      expect(Station.average_available_bikes).to eq(average_of_loaded)
+      expect(Station.average_available_bikes).to eq(16)
     end
-  end  
+  end
+
+  describe ".max_available_bikes" do
+    it "returns the maximum number of bikes at a station" do
+      Loader.new('./spec/fixtures/station_fixtures.csv').load_stations
+
+      expect(Station.max_available_bikes).to eq(37)
+
+    end
+  end
+
+
 end

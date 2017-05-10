8fce7c5614d6e84412ed6d82ee64a1ece75ed7f6
Rhonda <rwilhel@gmail.com>
2017-05-10 16:58:44 -0600

    Progress made on Trip-Dashboard feature test

--- spec/features/trips/user_can_access_trips_dashboard_spec.rb
+++ spec/features/trips/user_can_access_trips_dashboard_spec.rb
@@ -1,57 +1,65 @@
 require './spec/spec_helper'
 
-RSpec describe "When a user visits the trips dashboard (/trips-dashboard)" do
+RSpec.describe "When a user visits the trips dashboard (/trips-dashboard)" do
   it "loads the appropriate descriptions and calculations" do
     city_1 = City.create(name: "squeevillia")
-    Station.create(name: "something",
+    Station.create(
+                  name: "something",
                   dock_count: 1,
                   city_id: 1,
                   installation_date: Date.strptime("08/30/2013",'%m/%d/%Y'),
                   longitude: 1.1,
                   latitude: -1.1
     )
-    Station.create(name: "something else",
+    Station.create(
+                  name: "something else",
                   dock_count: 3,
                   city_id: 1,
                   installation_date: Date.strptime("08/30/2015",'%m/%d/%Y'),
                   longitude: 1,
                   latitude: -2
     )
-    Station.create(name: "awesome",
+    Station.create(
+                  name: "awesome",
                   dock_count: 3,
                   city_id: 1,
                   installation_date: Date.strptime("08/30/2015",'%m/%d/%Y'),
-                  longitude: 1,
-                  latitude: -2
+                  longitude: 2,
+                  latitude: -3
     )
-    Trip.create(duration: 63
-                start_date: "2013-08-29 14:13:00",
+    Trip.create(
+                duration: 63,
+                start_date: DateTime.strptime("08/30/2013 11:11", "%m/%d/%Y %H:%M"),
                 start_station_id: 1,
-                end_date: "2013-08-29 14:14:00",
+                end_date: DateTime.strptime("08/30/2013 11:12", "%m/%d/%Y %H:%M"),
                 end_station_id: 1,
                 bike_id: 1,
                 subscription_type: "Subscriber",
                 zipcode_id: 1
     )
-    Trip.create(duration: 70
-                start_date: "2014-05-29 14:42:00",
+    Trip.create(
+                duration: 70,
+                start_date: DateTime.strptime("05/29/2014 12:42", "%m/%d/%Y %H:%M"),
                 start_station_id: 2,
-                end_date: "2014-05-29 14:43:00",
+                end_date: DateTime.strptime("05/29/2014 12:43", "%m/%d/%Y %H:%M"),
                 end_station_id: 3,
                 bike_id: 2,
                 subscription_type: "Customer",
                 zipcode_id: 2
     )
-    Trip.create(duration: 71
-                start_date: "2015-01-01 10:16:00",
+    Trip.create(
+                duration: 71,
+                start_date: DateTime.strptime("01/01/2015 10:16", "%m/%d/%Y %H:%M"),
                 start_station_id: 1,
-                end_date: "2015-01-01 10:17:00",
+                end_date: DateTime.strptime("01/01/2015 10:17", "%m/%d/%Y %H:%M"),
                 end_station_id: 3,
                 bike_id: 1,
                 subscription_type: "Subscriber",
                 zipcode_id: 3
     )
+
     visit('/trips-dashboard')
+    save_and_open_page
 
     expect(page).to have_content("Average Duration of a Ride: 68 seconds")
     expect(page).to have_content("Longest Ride: 71 seconds")
@@ -67,8 +75,9 @@ RSpec describe "When a user visits the trips dashboard (/trips-dashboard)" do
     expect(page).to have_content("Total Number of Rides: 1")
     expect(page).to have_content("Number of Subscribers: 2")
     expect(page).to have_content("Percent of Subscribers: 66.67%")
-
+    expect(page).to have_content("Number of Customers: 1")
     expect(page).to have_content("Percent of Customers: 33.33%")
-
+    expect(page).to have_content("Date With Highest Number of Trips:")
+    expect(page).to have_content("Date With Lowest Number of Trips:")
   end
 end

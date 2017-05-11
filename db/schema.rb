4978adc56c965a69701463d990b62867cd756393
Sam Lim <limsammy1@gmail.com>
2017-05-11 00:17:19 -0600

    added index in trips for start_date for faster querying

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511050518) do
+ActiveRecord::Schema.define(version: 20170511055446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20170511050518) do
     t.bigint   "zip_code"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.index ["start_date"], name: "index_trips_on_start_date", using: :btree
   end
 
 end

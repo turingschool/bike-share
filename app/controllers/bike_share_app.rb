c345a80b7d59e178a19b3bad09facc2bab54132f
jtruong2 <jtruong2@comcast.net>
2017-06-29 18:00:28 -0600

    create stations

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -1,3 +1,14 @@
 class BikeShareApp < Sinatra::Base
 
+  
+  get '/stations/new' do
+    erb :new
+  end
+
+  post '/stations' do
+    Station.create(params[:station])
+    redirect '/stations'
+  end
+
+
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -1,2 +1,3 @@
 class Station < ActiveRecord::Base
+  validates_presence_of :name, :dock_count, :installation_date, :city
 end

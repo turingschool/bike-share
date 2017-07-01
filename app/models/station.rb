f432a6d0e308101004bea87aa5524a085d01d1aa
erose357 <erose357@gmail.com>
2017-07-01 13:57:48 -0600

    Passes validations tests

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,2 +1,5 @@
-class Station < ActiveRecord::Base 
+class Station < ActiveRecord::Base
+
+  validates_presence_of :name, :city, :dock_count, :installation_date
+
 end
--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -1,7 +1,24 @@
 RSpec.describe Station do
   describe "Validations" do
     it "is invalid without a name" do
+      station = Station.new(dock_count: 1, city: "Boston", installation_date: Time.now)
 
+      expect(station).to_not be_valid
+    end
+    it "is invalid without a dock_count" do
+      station = Station.new(name: "California", city: "Boston", installation_date: Time.now)
+
+      expect(station).to_not be_valid
+    end
+    it "is invalid without a city" do
+      station = Station.new(dock_count: 1, name: "Boston", installation_date: Time.now)
+
+      expect(station).to_not be_valid
+    end
+    it "is invalid without a installation_date" do
+      station = Station.new(dock_count: 1, city: "Boston", name: "California")
+
+      expect(station).to_not be_valid
     end
   end
 end

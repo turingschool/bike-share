515f5422cd97ddfd524393e30c4a71f4930c6371
BFunk <bret.e.funk@gmail.com>
2017-06-30 19:59:15 -0600

    basic bootstrap and views

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -1,3 +1,16 @@
 class BikeShareApp < Sinatra::Base
 
+  get '/' do
+    erb :dashboard
+  end
+
+  get '/stations/new' do
+    erb :new
+  end
+
+  get '/stations/all' do
+    @stations = Station.all
+    erb :index
+  end
+
 end

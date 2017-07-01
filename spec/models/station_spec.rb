968aa7f53d32ad08de48785e1f62237dce27a152
BFunk <bret.e.funk@gmail.com>
2017-07-01 16:33:38 -0600

    removed error page, added validation tests to station

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -9,4 +9,30 @@ RSpec.describe Station do
       expect(station.installation_date).to eq(20160808)
     end
   end
+  describe "Validations" do
+  it "is invalid without a name" do
+    station = Station.new(dock_count: 22, city: "Milan", installation_date: 20160808)
+
+    expect(station).to_not be_valid
+    end
+  end
+  describe "Validations" do
+  it "is invalid without a dock count" do
+    station = Station.new(name: "Bret", city: "Milan", installation_date: 20160808)
+
+    expect(station).to_not be_valid
+    end
+  end
+  describe "Validations" do
+  it "is invalid without a city" do
+    station = Station.new(name: "Bret", dock_count: 22, installation_date: 20160808)
+
+    expect(station).to_not be_valid
+    end
+  it "is invalid without a date" do
+    station = Station.new(name: "Bret", dock_count: 22, city: "Milan")
+
+    expect(station).to_not be_valid
+    end
+  end
 end

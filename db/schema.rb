a7078279d122e65e2d12744974c38e604844844c
Matt DeVoe <matthew.devoe@gmail.com>
2017-07-01 12:54:21 -0600

    Created Database Tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,96 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170629224602) do
+ActiveRecord::Schema.define(version: 20170701185240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "stations", force: :cascade do |t|
+  create_table "cities", force: :cascade do |t|
     t.text     "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "dates", force: :cascade do |t|
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "station_names", force: :cascade do |t|
+    t.text     "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "stations", force: :cascade do |t|
+    t.integer  "station_id"
+    t.integer  "station_name_id"
+    t.decimal  "lat"
+    t.decimal  "long"
     t.integer  "dock_count"
-    t.text     "city"
-    t.date     "installation_date"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
+    t.integer  "city_id"
+    t.integer  "installation_date_id"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  create_table "subscription_types", force: :cascade do |t|
+    t.text     "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "trips", force: :cascade do |t|
+    t.integer  "trip_id"
+    t.integer  "duration"
+    t.integer  "start_date_id"
+    t.integer  "start_station_name_id"
+    t.integer  "start_station_id"
+    t.integer  "end_date_id"
+    t.integer  "end_station_name_id"
+    t.integer  "end_station_id"
+    t.integer  "bike_id"
+    t.integer  "subscription_type_id"
+    t.integer  "zip_code_id"
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+  end
+
+  create_table "weather", force: :cascade do |t|
+    t.integer  "date_id"
+    t.decimal  "max_temperature_f"
+    t.decimal  "mean_temperature_f"
+    t.decimal  "min_temperature_f"
+    t.decimal  "max_dew_point_f"
+    t.decimal  "mean_dew_point_f"
+    t.decimal  "min_dew_point_f"
+    t.decimal  "max_humidity"
+    t.decimal  "mean_humidity"
+    t.decimal  "min_humidity"
+    t.decimal  "max_sea_level_pressure_inches"
+    t.decimal  "mean_sea_level_pressure_inches"
+    t.decimal  "min_sea_level_pressure_inches"
+    t.decimal  "max_visibility_miles"
+    t.decimal  "mean_visibility_miles"
+    t.decimal  "min_visibility_miles"
+    t.decimal  "max_wind_speed_mph"
+    t.decimal  "mean_wind_speed_mph"
+    t.decimal  "max_gust_speed_mph"
+    t.decimal  "precipitation_inches"
+    t.decimal  "cloud_cover"
+    t.decimal  "events"
+    t.decimal  "wind_dir_degrees"
+    t.integer  "zip_code_id"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+  end
+
+  create_table "zip_codes", force: :cascade do |t|
+    t.integer  "zip_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

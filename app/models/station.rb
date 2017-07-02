756c16880d4684ae6f87a526d508ce641fd9d4f2
jtruong2 <jtruong2@comcast.net>
2017-07-02 12:32:25 -0600

    fix installation_date from string to date

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,5 +1,5 @@
 class Station < ActiveRecord::Base
-  validates_presence_of :name, :dock_count, :city_id
+  validates_presence_of :name, :dock_count, :city_id, #:installation_date uncomment this after seeding
   belongs_to :cities
 
 end

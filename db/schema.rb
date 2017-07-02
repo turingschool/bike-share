2be1b2fe01e3a308b4475d0b1932d274b8ed658c
jtruong2 <jtruong2@comcast.net>
2017-07-02 12:22:00 -0600

    fix installation_date to be date data type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170701234016) do
+ActiveRecord::Schema.define(version: 20170702145148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,8 +22,8 @@ ActiveRecord::Schema.define(version: 20170701234016) do
   create_table "stations", force: :cascade do |t|
     t.text    "name"
     t.integer "dock_count"
-    t.string  "installation_date"
     t.integer "city_id"
+    t.date    "installation_date"
   end
 
   create_table "subscriptions", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'pry'
+require 'date'
 require './app/models/station'
 require './app/models/city'
 require './app/models/subscription'
@@ -14,40 +15,43 @@ end
 
 def seed_city_database(file_path)
   csv = from_csv(file_path)
-  cities = csv.each do |city|
-    city.keep_if {|k,v| k == :city}
+  cities = csv.each do |row|
+    row.keep_if {|k,v| k == :city}
   end
-  cities.uniq.each {|city| City.create!(city)}
+  row.uniq.each {|city| City.create!(city)}
 end
 
 def seed_station_database(file_path)
   stations = from_csv(file_path)
-  stations.each do |station|
+  stations.each do |row|
     city = City.find_by(city: station[:city])
-    station.delete_if { |k,v| k == :lat || k == :long || k == :city}
-    city.stations.create!(station)
+    row.delete_if { |k,v| k == :lat || k == :long || k == :city}
+    city.stations.create!(row)
   end
 end
 
+
+
 def seed_subscriptions_database(file_path)
   subscriptions = from_csv(file_path)
-  subscriptions.each do |subscription_type|
-    subscription_type.keep_if {|k,v| k == :subscription_type}
+  subscriptions.each do |row|
+    row.keep_if {|k,v| k == :subscription_type}
   end
   subscriptions.uniq.each {|sub_type| Subscription.create!(sub_type)}
 end
 
 def seed_trips_database(file_path)
   trips = from_csv(file_path)
-  trips.each do |trip|
+  trips.each do |row|
     subscription = Subscription.find_by(subscription_type: trip[:subscription_type])
-    trip.delete_if {|k,v| k == :subscription_type || k == :start_station_name || k == :end_station_name}
-    next if trip[:zip_code] == '' || trip[:zip_code] == nil
-    subscription.trips.create!(trip)
+    row.delete_if {|k,v| k == :subscription_type || k == :start_station_name || k == :end_station_name}
+    next if row[:zip_code] == '' || row[:zip_code] == nil
+    subscription.trips.create!(row)
   end
 end
 
+
 # seed_city_database("db/csv/station.csv")
-# seed_station_database("db/csv/station.csv")
+seed_station_database("db/csv/station.csv")
 # seed_subscriptions_database("db/csv/trip.csv")
-seed_trips_database("db/csv/trip.csv")
+# seed_trips_database("db/csv/trip.csv")

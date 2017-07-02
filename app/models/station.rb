a985b36e1d5e8488de7d9a37032af1fe04bffa65
MikelSage <michael.sagapolutele@yahoo.com>
2017-07-01 22:56:41 -0600

    add .most_availble_bikes method to station

--- app/models/station.rb
+++ app/models/station.rb
@@ -2,4 +2,7 @@ class Station < ActiveRecord::Base
 
   validates_presence_of :name, :city, :dock_count, :installation_date
 
+  def self.most_available_bikes
+    Station.pluck(:dock_count).max
+  end
 end
--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -1,4 +1,21 @@
 RSpec.describe Station do
+   describe "Class Methods" do
+     describe '.most_available_bikes' do
+       it "returns the most bikes available at a station" do
+         station_1 = Station.create(name: "First", dock_count: 4, city: "Detroit", installation_date: Time.now)
+         station_2 = Station.create(name: "Second", dock_count: 2, city: "Detroit", installation_date: Time.now)
+         station_3 = Station.create(name: "Third", dock_count: 23, city: "Detroit", installation_date: Time.now)
+         station_4 = Station.create(name: "Fourth", dock_count: 6, city: "Detroit", installation_date: Time.now)
+         station_5 = Station.create(name: "Fifth", dock_count: 5, city: "Detroit", installation_date: Time.now)
+
+         result = Station.most_available_bikes
+
+         expect(station_1.class).to eq(Station)
+         expect(result).to eq(23)
+       end
+     end
+   end
+
   describe "Validations" do
     it "is invalid without a name" do
       station = Station.new(dock_count: 1, city: "Boston", installation_date: Time.now)
@@ -30,4 +47,5 @@ RSpec.describe Station do
      expect(station).to be_valid
     end
   end
+
 end

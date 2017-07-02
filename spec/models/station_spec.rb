ff24a97f481356df3b3437dc627a61390a0d1485
erose357 <erose357@gmail.com>
2017-07-01 23:01:03 -0600

    Add method for stations with fewest bikes available by dock count

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -30,4 +30,28 @@ RSpec.describe Station do
      expect(station).to be_valid
     end
   end
+
+    describe "Class Methods" do
+      describe ".fewest_bikes_available" do
+      it "returns the station with the fewest bikes available based on docks" do
+        Station.create(name: "Mod2BEE", dock_count: 1, installation_date: "11/4/13", city: "Chicago")
+        Station.create(name: "Mod1BEE", dock_count: 5, installation_date: "12/7/14", city: "Chicago")
+        Station.create(name: "Mod3BEE", dock_count: 3, installation_date: "1/23/12", city: "Chicago")
+
+        result = Station.fewest_bikes_available
+
+        expect(result[0].name).to eq("Mod2BEE")
+      end
+      it "returns the stations with the fewest bikes available based on docks" do
+        Station.create(name: "Mod2BEE", dock_count: 1, installation_date: "11/4/13", city: "Chicago")
+        Station.create(name: "Mod1BEE", dock_count: 1, installation_date: "12/7/14", city: "Chicago")
+        Station.create(name: "Mod3BEE", dock_count: 5, installation_date: "1/23/12", city: "Chicago")
+
+        result = Station.fewest_bikes_available
+
+        expect(result.count).to eq(2)
+        expect(result[0].class).to eq(Station)
+      end
+    end
+  end
 end

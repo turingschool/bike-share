b9fd1c8b1462b340742588054893743f3260874a
MikelSage <michael.sagapolutele@yahoo.com>
2017-07-01 23:45:59 -0600

    add least_available_bikes method

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,5 +1,7 @@
 class Station < ActiveRecord::Base
 
   validates_presence_of :name, :city, :dock_count, :installation_date
-
+  def self.least_available_bikes
+    Station.pluck(:dock_count).min
+  end
 end

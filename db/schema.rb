5869bfda6d985f404c6b96f1fdb834298529631e
MikelSage <michael.sagapolutele@yahoo.com>
2017-07-02 16:35:27 -0600

    create migration to add installation_date_id to stations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170702222345) do
+ActiveRecord::Schema.define(version: 20170702223326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170702222345) do
     t.text    "name"
     t.integer "dock_count"
     t.text    "city"
+    t.integer "installation_date_id"
   end
 
 end

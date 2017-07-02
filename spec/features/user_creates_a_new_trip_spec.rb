1af0461cafc0b1afdcd33277e37295849e23f947
Jim Szalewski <jim.szalewski@gmail.com>
2017-07-01 20:50:45 -0600

    fixed test

--- spec/features/user_creates_a_new_trip_spec.rb
+++ spec/features/user_creates_a_new_trip_spec.rb
@@ -17,5 +17,12 @@ RSpec.describe "User creates a trip" do
     trip = Trip.last
     expect(current_path).to eq("/trips")
     expect(page).to have_content("Duration: 30")
+    expect(page).to have_content("Start Date: 6/12/16")
+    expect(page).to have_content("Start Station ID: 34")
+    expect(page).to have_content("End Date: 6/12/16")
+    expect(page).to have_content("End Station ID: 5")
+    expect(page).to have_content("Bike ID: 12")
+    expect(page).to have_content("Subscription Type ID: 1")
+    expect(page).to have_content("Zip Code: 90210")
   end
 end

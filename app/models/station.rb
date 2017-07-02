399dbf334a5af564c86bc821c65c815a29483fb7
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-07-01 20:37:06 -0600

    creates most recently installed and average_bikes_available class methods

--- app/models/station.rb
+++ app/models/station.rb
@@ -2,4 +2,12 @@ class Station < ActiveRecord::Base
 
   validates_presence_of :name, :city, :dock_count, :installation_date
 
+
+  def self.average_bikes_available_per_station
+    average(:dock_count).round
+  end
+
+  def self.most_recently_installed
+    self.order(:installation_date).last
+  end
 end

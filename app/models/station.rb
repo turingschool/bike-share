6fb02153fa9ab482e21c3b625b8b2b411d3d5e7d
Desislava Todorova <dslavatodorova@gmail.com>
2017-07-02 08:58:52 -0600

    add oldest_station method and test for station

--- app/models/station.rb
+++ app/models/station.rb
@@ -2,6 +2,10 @@ class Station < ActiveRecord::Base
   validates_presence_of :name, :city, :dock_count, :installation_date
 
   def self.total_count
-    Station.all.count
+    Station.count
+  end
+
+  def self.oldest_station
+    Station.order(:installation_date).first
   end
 end
--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -4,43 +4,56 @@ RSpec.describe Station do
       it "returns total count for stations" do
         station_1 = Station.create(name: "A", city: "Boston", dock_count: 1, installation_date: Time.now)
         station_2 = Station.create(name: "B", city: "Denver", dock_count: 1, installation_date: Time.now)
+        station_3 = Station.create(name: "C", city: "Denver", dock_count: 3, installation_date: Time.now)
 
         count = Station.total_count
 
-        expect(count).to eq(2)
+        expect(count).to eq(3)
       end
     end
-  end
 
-  describe "Validations" do
-    it "is invalid without a name" do
-      station = Station.new(dock_count: 1, city: "Boston", installation_date: Time.now)
+    describe ".oldest_station" do
+      it "returns the station with the earliest installation date" do
+        station_1 = Station.create(name: "A", city: "Boston", dock_count: 1, installation_date: "08/07/2017")
+        station_2 = Station.create(name: "B", city: "Denver", dock_count: 2, installation_date: "08/08/2016")
+        station_3 = Station.create(name: "C", city: "Denver", dock_count: 3, installation_date: "08/09/2015")
 
-      expect(station).to_not be_valid
+        station = Station.oldest_station
+
+        expect(station).to eq(station_3)
+      end
     end
 
-    it "is invalid without a dock_count" do
-      station = Station.new(name: "California", city: "Boston", installation_date: Time.now)
+    describe "Validations" do
+      it "is invalid without a name" do
+        station = Station.new(dock_count: 1, city: "Boston", installation_date: Time.now)
 
-      expect(station).to_not be_valid
-    end
+        expect(station).to_not be_valid
+      end
 
-    it "is invalid without a city" do
-      station = Station.new(dock_count: 1, name: "Boston", installation_date: Time.now)
+      it "is invalid without a dock_count" do
+        station = Station.new(name: "California", city: "Boston", installation_date: Time.now)
 
-      expect(station).to_not be_valid
-    end
+        expect(station).to_not be_valid
+      end
 
-    it "is invalid without a installation_date" do
-      station = Station.new(dock_count: 1, city: "Boston", name: "California")
+      it "is invalid without a city" do
+        station = Station.new(dock_count: 1, name: "Boston", installation_date: Time.now)
 
-      expect(station).to_not be_valid
-    end
+        expect(station).to_not be_valid
+      end
 
-    it "is valid with all attributes" do
-     station = Station.new(name: "Gary", city:"Denver", dock_count: 2, installation_date: Time.now)
+      it "is invalid without a installation_date" do
+        station = Station.new(dock_count: 1, city: "Boston", name: "California")
 
-     expect(station).to be_valid
+        expect(station).to_not be_valid
+      end
+
+      it "is valid with all attributes" do
+       station = Station.new(name: "Gary", city:"Denver", dock_count: 2, installation_date: Time.now)
+
+       expect(station).to be_valid
+      end
     end
   end
 end

eabc4c3547f42a6625fa6bcf04c9d92a3f37c8bd
Jim Szalewski <jim.szalewski@gmail.com>
2017-07-03 12:15:29 -0600

    change method names to match new migrations

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,43 +1,43 @@
 RSpec.describe Trip do
   describe "Validations" do
     it "is invalid without a duration" do
-      trip = Trip.new(start_date: 12, start_station_id: 12, end_date: 12, end_station_id: 12, bike_id: 12, subscription_type_id: 1)
+      trip = Trip.new(start_date: 8/29/2013, start_station_id: 66, end_date: 8/29/2013, end_station_id: 66, bike_id: 520, subscription_id: 1)
 
       expect(trip).to_not be_valid
     end
 
     it "is invalid without a start date" do
-      trip = Trip.new(duration: 12, start_station_id: 12, end_date: 12, end_station_id: 12, bike_id: 12, subscription_type_id: 1)
+      trip = Trip.new(duration: 63, start_station_id: 66, end_date: 8/29/2013, end_station_id: 66, bike_id: 520, subscription_id: 1)
 
       expect(trip).to_not be_valid
     end
 
-    it "is invalid without a end date" do
-      trip = Trip.new(duration: 12, start_date: 12, start_station_id: 12, end_station_id: 12, bike_id: 12, subscription_type_id: 1)
+    it "is invalid without an end date" do
+      trip = Trip.new(duration: 63, start_date: 8/29/2013, start_station_id: 66, end_station_id: 66, bike_id: 520, subscription_id: 1)
 
       expect(trip).to_not be_valid
     end
 
     it "is invalid without a start station id" do
-      trip = Trip.new(duration: 12, start_date: 12, end_date: 12, end_station_id: 12, bike_id: 12, subscription_type_id: 1)
+      trip = Trip.new(duration: 63, start_date: 8/29/2013, end_date: 8/29/2013, end_station_id: 66, bike_id: 520, subscription_id: 1)
 
       expect(trip).to_not be_valid
     end
 
-    it "is invalid without a end station id" do
-      trip = Trip.new(duration: 12, start_date: 12, start_station_id: 12, end_date: 12, bike_id: 12, subscription_type_id: 1)
+    it "is invalid without an end station id" do
+      trip = Trip.new(duration: 63, start_date: 8/29/2013, start_station_id: 66, end_date: 8/29/2013, bike_id: 520, subscription_id: 1)
 
       expect(trip).to_not be_valid
     end
 
     it "is invalid without a bike id" do
-      trip = Trip.new(duration: 12, start_date: 12, start_station_id: 12, end_date: 12, end_station_id: 12, subscription_type_id: 1)
+      trip = Trip.new(duration: 63, start_date: 8/29/2013, start_station_id: 66, end_date: 8/29/2013, end_station_id: 66, subscription_id: 1)
 
       expect(trip).to_not be_valid
     end
 
     it "is invalid without a subscription type id" do
-      trip = Trip.new(duration: 12, start_date: 12, start_station_id: 12, end_date: 12, end_station_id: 12, bike_id: 12)
+      trip = Trip.new(duration: 63, start_date: 8/29/2013, start_station_id: 66, end_date: 8/29/2013, end_station_id: 66, bike_id: 520)
 
       expect(trip).to_not be_valid
     end

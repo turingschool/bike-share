28e9f197c319743e06632c30cfc3add91ee2e6ef
erose357 <erose357@gmail.com>
2017-07-03 19:08:42 -0600

    Add zipcode cleaning methods

--- app/models/zipcode.rb
+++ app/models/zipcode.rb
@@ -1,8 +1,25 @@
 class Zipcode < ActiveRecord::Base
   validates_presence_of :zipcode
 
-  def self.create_zipcode(instance)
-    find_or_create_by(zipcode: instance.zipcode)
+  #use this method for seed only?
+  # def self.create_zipcode(instance)
+  #   find_or_create_by(zipcode: instance.zipcode)
+  # end
+
+  def clean_zipcode(string)
+    if string.length < 5
+      return add_zeros(string)
+    elsif string.length > 5
+      return string[0..4]
+    else
+      return string
+    end
   end
-  
+
+  def add_zeros(string)
+    until string.length == 5
+      string += "0"
+    end
+    return string
+  end
 end

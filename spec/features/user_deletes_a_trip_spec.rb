7b859967106c397ac3033d9d3f600dc5e7e53305
Jim Szalewski <jim.szalewski@gmail.com>
2017-07-04 15:00:28 -0600

    modify datetime formatting

--- spec/features/user_deletes_a_trip_spec.rb
+++ spec/features/user_deletes_a_trip_spec.rb
@@ -1,7 +1,13 @@
 RSpec.describe "User deletes a trip" do
   it "from all trips page" do
-    Trip.create(duration: 63, start_date: 8/29/2013, start_station_id: 66, end_date: 8/29/2013, end_station_id: 66, bike_id: 520, subscription_id: 1)
-    Trip.create(duration: 70, start_date: 8/29/2013, start_station_id: 10, end_date: 8/29/2013, end_station_id: 10, bike_id: 661, subscription_id: 1)
+
+    start_date_1 = DateTime.new(2013, 8, 29, 7, 13)
+    end_date_1 = DateTime.new(2013, 8, 29, 7, 14)
+    start_date_2 = DateTime.new(2013, 8, 29, 7, 43)
+    end_date_2 = DateTime.new(2013, 8, 29, 7, 44)
+
+    Trip.create(duration: 63, start_date: start_date_1, start_station_id: 66, end_date: end_date_1, end_station_id: 66, bike_id: 520, subscription_id: 1)
+    Trip.create(duration: 70, start_date: start_date_2, start_station_id: 10, end_date: end_date_2, end_station_id: 10, bike_id: 661, subscription_id: 1)
 
     visit('/trips')
 
--- spec/features/user_edits_a_trip_spec.rb
+++ spec/features/user_edits_a_trip_spec.rb
@@ -1,19 +1,26 @@
 RSpec.describe "User edits a trip" do
   it "with valid attributes" do
-    Trip.create(duration: 63, start_date: 8/29/2013, start_station_id: 66, end_date: 8/29/2013, end_station_id: 66, bike_id: 520, subscription_id: 1)
+
+    start_date_1 = DateTime.new(2013, 8, 29, 7, 13)
+    end_date_1 = DateTime.new(2013, 8, 29, 7, 14)
+
+    Trip.create(duration: 63, start_date: start_date_1, start_station_id: 66, end_date: end_date_1, end_station_id: 66, bike_id: 520, subscription_id: 1)
 
     visit('/trips/1/edit')
 
     save_and_open_page
 
+    start_date_2 = DateTime.new(2013, 8, 29, 7, 43)
+    end_date_2 = DateTime.new(2013, 8, 29, 7, 44)
+
     fill_in("trip[duration]", with: 30)
-    fill_in("trip[start_date]", with: 6/12/16)
+    fill_in("trip[start_date]", with: start_date_2)
     fill_in("trip[start_station_id]", with: 34)
-    fill_in("trip[end_date]", with: 6/12/16)
+    fill_in("trip[end_date]", with: end_date_2)
     fill_in("trip[end_station_id]", with: 5)
     fill_in("trip[bike_id]", with: 26)
     fill_in("trip[subscription_id]", with: 1)
-    fill_in("trip[zipcode]", with: 95138)
+    fill_in("trip[zip_code]", with: 95138)
 
     duration      = find_field("trip[duration]").value.to_i
     start_date    = find_field("trip[start_date]").value
@@ -22,12 +29,12 @@ RSpec.describe "User edits a trip" do
     end_station   = find_field("trip[end_station_id]").value.to_i
     bike          = find_field("trip[bike_id]").value.to_i
     subscription  = find_field("trip[subscription_id]").value.to_i
-    zip_code      = find_field("trip[zipcode]").value.to_i
+    zip_code      = find_field("trip[zip_code]").value.to_i
 
     expect(duration).to eq(30)
-    expect(start_date).to eq(6/12/16)
+    expect(start_date).to eq("2013-08-29T07:43:00+00:00")
     expect(start_station).to eq(34)
-    expect(end_date).to eq(6/12/16)
+    expect(end_date).to eq("2013-08-29T07:44:00+00:00")
     expect(end_station).to eq(5)
     expect(bike).to eq(26)
     expect(subscription).to eq(1)
@@ -35,12 +42,14 @@ RSpec.describe "User edits a trip" do
 
     click_button("Submit Changes")
 
+    save_and_open_page
+
     trip = Trip.last
     expect(current_path).to eq("/trips")
     expect(page).to have_content("Duration: 30")
-    expect(page).to have_content("Start Date: 6/12/16")
+    expect(page).to have_content("Start Date: 2013-08-29 07:43:00 UTC")
     expect(page).to have_content("Start Station ID: 34")
-    expect(page).to have_content("End Date: 6/12/16")
+    expect(page).to have_content("End Date: 2013-08-29 07:44:00 UTC")
     expect(page).to have_content("End Station ID: 5")
     expect(page).to have_content("Bike ID: 26")
     expect(page).to have_content("Subscription Type ID: 1")

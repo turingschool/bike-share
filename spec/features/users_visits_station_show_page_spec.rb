928fc1807f9b1b5a7be2723cd2edb552dad64faa
erose357 <erose357@gmail.com>
2017-07-04 11:17:51 -0600

    Commented out save_and_open_page in station show page test

--- spec/features/users_visits_station_show_page_spec.rb
+++ spec/features/users_visits_station_show_page_spec.rb
@@ -1,12 +1,8 @@
 RSpec.describe "user visits station show page" do
   it "and they see all information for that station" do
     date = BikeShareDate.create(date: Date.strptime('1/1/2012', '%m/%d/%Y'))
-    # date_2 = BikeShareDate.create(date: Date.strptime('2/5/2017', '%m/%d/%Y'))
-    # date_3 = BikeShareDate.create(date: Date.strptime('4/12/2014', '%m/%d/%Y'))
 
     station = Station.create(name: "A", dock_count: 1, installation_date_id: 1, city: "Chicago")
-    # station_2 = Station.create(name: "B", dock_count: 2, installation_date_id: 3, city: "Denver")
-    # station_c = Station.create(name: "C", dock_count: 3, installation_date_id: 3, city: "Littleton")
 
     trip_1 = Trip.create(duration: 75, start_date_id: 2, start_station_id: 1, end_date_id: 2, end_station_id: 1, bike_id: 6, subscription_type: "customer", zipcode_id: 7)
     trip_2 = Trip.create(duration: 75, start_date_id: 2, start_station_id: 1, end_date_id: 2, end_station_id: 1, bike_id: 6, subscription_type: "customer", zipcode_id: 7)
@@ -15,11 +11,11 @@ RSpec.describe "user visits station show page" do
 
 
     visit("/stations/#{station.id}")
-    save_and_open_page
+    # save_and_open_page
     expect(page).to have_content(station.name)
     expect(page).to have_content(station.dock_count)
     expect(page).to have_content(station.city)
-    expect(page).to have_content(station.installation_date.date)
+    expect(page).to have_content(station.installation_date)
     expect(page).to have_content(4)
     expect(page).to have_content(3)
   end

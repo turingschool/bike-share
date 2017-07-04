a254c61e92da5a84634f766d79b585de6b91ada4
jtruong2 <jtruong2@comcast.net>
2017-07-04 09:38:00 -0600

    finish iteration 6

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -28,7 +28,7 @@ class Trip < ActiveRecord::Base
   end
 
   def self.month_summary
-    dates = Trip.group('(EXTRACT(YEAR FROM end_date))::integer').group('(EXTRACT(MONTH FROM end_date))::integer').order('2 DESC, 3 DESC').count
+    dates = Trip.group('(EXTRACT(YEAR FROM end_date))::integer').group('(EXTRACT(MONTH FROM end_date))::integer').order('count_all DESC').count
   end
 
   def self.most_ridden_bike
@@ -49,7 +49,43 @@ class Trip < ActiveRecord::Base
   end
 
   def self.date_with_highest_trip_count
-    dates = Trip.group('(EXTRACT(MONTH FROM start_date))::integer').group('(EXTRACT(DAY FROM start_date))::integer').group('(EXTRACT(YEAR FROM start_date))::integer').order('2 DESC, 3 DESC').count
+    Trip.group('(EXTRACT(MONTH FROM start_date))::integer').group('(EXTRACT(DAY FROM start_date))::integer').group('(EXTRACT(YEAR FROM start_date))::integer').order('count_all DESC').count.first
   end
 
+  def self.date_with_lowest_trip_count
+    Trip.group('(EXTRACT(MONTH FROM start_date))::integer').group('(EXTRACT(DAY FROM start_date))::integer').group('(EXTRACT(YEAR FROM start_date))::integer').order('count_all').count.first
+  end
+
+  def self.number_of_starting_rides_at_station(station_id)
+    stations = Trip.group(:start_station_id).count(:id)
+    stations[station_id]
+  end
+
+  def self.number_of_ending_rides_at_station(station_id)
+    stations = Trip.group(:end_station_id).count(:id)
+    stations[station_id]
+  end
+
+  def self.most_frequent_destination(station_id)
+    station = Trip.where(start_station_id: station_id).group(:end_station_id).order('count_id DESC').count(:id).first
+    Station.find(station[0])[:name]
+  end
+
+  def self.most_frequent_origination(station_id)
+    station = Trip.where(end_station_id: station_id).group(:start_station_id).order('count_id DESC').count(:id).first
+    Station.find(station[0])[:name]
+  end
+
+  def self.date_with_highest_trip_from_this_station(station_id)
+    date = Trip.where(start_station_id: station_id).group('(EXTRACT(MONTH FROM start_date))::integer').group('(EXTRACT(DAY FROM start_date))::integer').group('(EXTRACT(YEAR FROM start_date))::integer').order('count_all').count.first
+    "#{date[0][0]}/#{date[0][1]}/#{date[0][2]}"
+  end
+
+  def self.most_frequent_zip_code_users_starting(station_id)
+    Trip.where(start_station_id: station_id).group(:zip_code).order('count_id DESC').count(:id).first[0]
+  end
+
+  def self.most_frequent_bike_starting(station_id)
+    Trip.where(start_station_id: station_id).group(:bike_id).order('count_id DESC').count(:id).first[0]
+  end
 end

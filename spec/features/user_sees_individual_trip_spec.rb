ca27399ec4e3b236809568a2c746d6b7756f1439
Desislava Todorova <dslavatodorova@gmail.com>
2017-07-04 19:55:46 -0600

    add zipcode object in user sees individual trip test

--- spec/features/user_sees_individual_trip_spec.rb
+++ spec/features/user_sees_individual_trip_spec.rb
@@ -2,8 +2,9 @@ RSpec.describe "user selects a trip" do
   it "can see information about selected trip" do
     trip = Trip.create(duration: 53, start_date_id: 2, end_date_id: 2, start_station_id: 3, end_station_id: 3, bike_id: 3, subscription_type: 'User', zipcode_id: 3)
     trip_2 = Trip.create(duration: 174, start_date_id: 1, end_date_id: 3, start_station_id: 2, end_station_id: 2, bike_id: 1, subscription_type: 'User', zipcode_id: 2)
-    trip_3 = Trip.create(duration: 13, start_date_id: 3, end_date_id: 1, start_station_id: 1, end_station_id: 3, bike_id: 2, subscription_type: 'User', zipcode_id: 4)
+    trip_3 = Trip.create(duration: 13, start_date_id: 3, end_date_id: 1, start_station_id: 1, end_station_id: 3, bike_id: 2, subscription_type: 'User', zipcode_id: 1)
 
+    zipcode = Zipcode.create(zipcode: 80202)
     date = BikeShareDate.create(date: Date.strptime('1/1/2012', '%m/%d/%Y'))
     date_2 = BikeShareDate.create(date: Date.strptime('2/5/2017', '%m/%d/%Y'))
     date_3 = BikeShareDate.create(date: Date.strptime('2/21/2017', '%m/%d/%Y'))
@@ -21,7 +22,6 @@ RSpec.describe "user selects a trip" do
     expect(page).to have_content("Colfax")
     expect(page).to have_content(2)
     expect(page).to have_content("User")
-    expect(page).to have_content(4)
-    #need to create relationship to zipcode.zipcode
+    expect(page).to have_content(80202)
   end
 end

e3f938a4452d2cd6f089eeb5ee068b8444e5396b
Ellen Cooper <ellencooper74@gmail.com>
2017-07-04 18:24:12 -0600

    creates weather migration and migration to add weather_id to trips

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,5 @@
 class Trip < ActiveRecord::Base
   validates_presence_of :duration, :start_date, :end_date, :start_station_id, :end_station_id, :bike_id, :subscription_id
   belongs_to :subscription
+  belongs_to :weather
 end

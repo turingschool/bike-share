24cc25703b8d0538afcbcd95af2813bb1076c22e
Matt DeVoe <matthew.devoe@gmail.com>
2017-07-05 20:15:10 -0600

    Iteration 1 Model Testing Complete

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -39,6 +39,112 @@ RSpec.describe Station do
 
       expect(result).to eq(station.station_name.name)
     end
+
+    it "returns stations with most bikes" do
+      station_name = StationName.create(name: "Test Station")
+      station_name_1 = StationName.create(name: "Super Duper Doritos")
+      station_name_2 = StationName.create(name: "Meow Wolf")
+      station_name_3 = StationName.create(name: "Lizzy")
+      station_name_4 = StationName.create(name: "Jack Skeleton")
+      station_name_5 = StationName.create(name: "Barney")
+      install_date = BikeShareDate.create(bike_share_date: "2013-08-06 00:00:00")
+      install_date_2 = BikeShareDate.create(bike_share_date: "2014-09-15 00:00:00")
+      city = City.create(name: "Denver")
+      station = Station.create(station_name_id: station_name.id, dock_count: 20, city_id: city.id,
+                installation_date_id: install_date.id)
+      station1 = Station.create(station_name_id: station_name_1.id, dock_count: 20, city_id: city.id,
+                installation_date_id: install_date.id)
+      station2 = Station.create(station_name_id: station_name_2.id, dock_count: 20, city_id: city.id,
+                installation_date_id: install_date.id)
+      station3 = Station.create(station_name_id: station_name_3.id, dock_count: 20, city_id: city.id,
+                installation_date_id: install_date.id)
+      station4 = Station.create(station_name_id: station_name_4.id, dock_count: 20, city_id: city.id,
+                installation_date_id: install_date.id)
+      station5 = Station.create(station_name_id: station_name_5.id, dock_count: 5, city_id: city.id,
+                installation_date_id: install_date_2.id)
+
+
+      result = Station.stations_with_most_bikes
+      expected = Station.where(installation_date_id: install_date.id)
+
+      expect(result[0]).to eq(expected[0])
+    end
+
+    it "returns stations with fewest bikes available" do
+      station_name = StationName.create(name: "Test Station")
+      install_date = BikeShareDate.create(bike_share_date: "2013-08-06 00:00:00")
+      city = City.create(name: "Denver")
+      station = Station.create(station_name_id: station_name.id, dock_count: 20, city_id: city.id,
+                installation_date_id: install_date.id)
+      station1 = Station.create(station_name_id: station_name.id, dock_count: 10, city_id: city.id,
+                installation_date_id: install_date.id)
+
+      result = Station.fewest_bikes_avaiable
+
+      expect(result).to eq(station1.station_name.name)
+    end
+
+    it "returns stations with fewest bikes" do
+      station_name = StationName.create(name: "Test Station")
+      station_name_1 = StationName.create(name: "Super Duper Doritos")
+      station_name_2 = StationName.create(name: "Meow Wolf")
+      station_name_3 = StationName.create(name: "Lizzy")
+      station_name_4 = StationName.create(name: "Jack Skeleton")
+      station_name_5 = StationName.create(name: "Barney")
+      install_date = BikeShareDate.create(bike_share_date: "2013-08-06 00:00:00")
+      install_date_2 = BikeShareDate.create(bike_share_date: "2014-09-15 00:00:00")
+      city = City.create(name: "Denver")
+      station = Station.create(station_name_id: station_name.id, dock_count: 3, city_id: city.id,
+                installation_date_id: install_date.id)
+      station1 = Station.create(station_name_id: station_name_1.id, dock_count: 3, city_id: city.id,
+                installation_date_id: install_date.id)
+      station2 = Station.create(station_name_id: station_name_2.id, dock_count: 3, city_id: city.id,
+                installation_date_id: install_date.id)
+      station3 = Station.create(station_name_id: station_name_3.id, dock_count: 3, city_id: city.id,
+                installation_date_id: install_date.id)
+      station4 = Station.create(station_name_id: station_name_4.id, dock_count: 3, city_id: city.id,
+                installation_date_id: install_date.id)
+      station5 = Station.create(station_name_id: station_name_5.id, dock_count: 5, city_id: city.id,
+                installation_date_id: install_date_2.id)
+
+
+      result = Station.stations_with_fewest_bikes
+      expected = Station.where(installation_date_id: install_date.id)
+
+      expect(result[0]).to eq(expected[0])
+    end
+
+      it "returns most recently installed station" do
+        station_name_1 = StationName.create(name: "Super Duper Doritos")
+        station_name_2 = StationName.create(name: "Meow Wolf")
+        install_date_1 = BikeShareDate.create(bike_share_date: "2013-08-06 00:00:00")
+        install_date_2 = BikeShareDate.create(bike_share_date: "2014-09-15 00:00:00")
+        city = City.create(name: "Denver")
+        station1 = Station.create(station_name_id: station_name_1.id, dock_count: 20, city_id: city.id,
+                  installation_date_id: install_date_1.id)
+        station2 = Station.create(station_name_id: station_name_2.id, dock_count: 20, city_id: city.id,
+                  installation_date_id: install_date_2.id)
+
+        result = Station.most_recent_station
+
+        expect(result).to eq(station2.station_name.name)
+      end
+
+      it "returns oldest station" do
+        station_name_1 = StationName.create(name: "Super Duper Doritos")
+        station_name_2 = StationName.create(name: "Meow Wolf")
+        install_date_1 = BikeShareDate.create(bike_share_date: "2013-08-06 00:00:00")
+        install_date_2 = BikeShareDate.create(bike_share_date: "2014-09-15 00:00:00")
+        city = City.create(name: "Denver")
+        station1 = Station.create(station_name_id: station_name_1.id, dock_count: 20, city_id: city.id,
+                  installation_date_id: install_date_1.id)
+        station2 = Station.create(station_name_id: station_name_2.id, dock_count: 20, city_id: city.id,
+                  installation_date_id: install_date_2.id)
+
+        result = Station.oldest_station
+
+        expect(result).to eq(station1.station_name.name)
+      end
   end
 
   describe "instance methods" do

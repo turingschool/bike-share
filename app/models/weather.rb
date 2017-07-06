4c04873cf450a2eb64ffca9d6c16608fd87646ba
Ellen Cooper <ellencooper74@gmail.com>
2017-07-05 22:39:01 -0600

    refactors average into separate method (working)

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -3,13 +3,18 @@ class Weather < ActiveRecord::Base
   has_many :trips
 
   def self.rides_by_max_temp
-    lower_range_limit = Weather.minimum("max_temperature")/10 * 10
-    upper_range_limit = (Weather.maximum("max_temperature")/10 + 1) * 10
-    # Trip.joins(:weather).where("max_temperature > ?", 40).where("max_temperature < ?", 60).pluck("max_temperature")
-    # Trip.joins(:weather).where("max_temperature > ?", 40).where("max_temperature < ?", 60).group(:weather).count(:id)
-    # day_count = Trip.joins(:weather).where("max_temperature > ?", 40).where("max_temperature < ?", 60).pluck("date").uniq.count
-    trip_count = Trip.joins(:weather).where('max_temperature BETWEEN ? AND ?',40, 50).count
-    day_count = Weather.where('max_temperature BETWEEN ? AND ?',40, 50).count
+    lower_limit = (Weather.minimum("max_temperature")/10).to_i * 10
+    upper_limit = (Weather.maximum("max_temperature")/10 + 1).to_i * 10
+    Weather.average_rides("max_temperature", upper_limit, lower_limit, 10)
+    # trip_count = Trip.joins(:weather).where('max_temperature BETWEEN ? AND ?',40, 50).count
+    # day_count = Trip.joins(:weather).where('max_temperature BETWEEN ? AND ?',40, 50).pluck("date").uniq.count
+    # trip_count/day_count
+  end
+
+  def self.average_rides(condition, upper_limit, lower_limit, increment)
+    join_table = Trip.joins(:weather).where("#{condition} BETWEEN ? AND ?", lower_limit, lower_limit + increment)
+    trip_count = join_table.count
+    day_count = join_table.pluck("date").uniq.count
     trip_count/day_count
   end
 end

15d7b6edf8aa996a670d7bf97192ab7e50d5032c
Jim Szalewski <jim.szalewski@gmail.com>
2017-07-05 20:17:15 -0600

    test user creates a station

---
+++ spec/features/user_creates_a_new_station_spec.rb
@@ -0,0 +1,17 @@
+RSpec.describe "User creates a station" do
+  it "with valid attributes" do
+
+    visit('/stations/new')
+    fill_in("station[name]", with: "Santa Clara at Almaden")
+    fill_in("station[dock_count]", with: 11)
+    fill_in("station[installation_date]", with: "2013-06-08")
+    fill_in("station[city_id]", with: 1)
+
+    click_button("Create Station")
+
+    save_and_open_page
+
+    expect(current_path).to eq("/stations")
+    expect(page).to have_content("Santa Clara at Almaden")
+  end
+end

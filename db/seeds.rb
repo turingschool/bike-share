3f4431d1abe1eeab4e549e55838cb315f388be70
JoelLindow <joellindow@gmail.com>
2017-07-06 10:33:06 -0600

    Plugging in final day styling. Reconfiguring views

--- db/seeds.rb
+++ db/seeds.rb
@@ -85,8 +85,8 @@ end
 # seed_city_database("db/csv/station.csv")
 # seed_weather_database("db/csv/weather.csv")
 # seed_station_database("db/csv/station.csv")
-# seed_subscriptions_database("db/csv/trip.csv")
-# seed_trips_database("db/csv/trip.csv")
+seed_subscriptions_database("db/csv/trip.csv")
+seed_trips_database("db/csv/trip.csv")
 
 # #fixtures:
 # seed_city_database("db/fixtures/stations.csv")

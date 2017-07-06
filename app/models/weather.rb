f40fb42b18a03b7a1aafd0add095c041ef48a5ea
Jim Szalewski <jim.szalewski@gmail.com>
2017-07-06 10:03:34 -0600

    add paginate gem and 30 per page method

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -1,4 +1,11 @@
+require 'will_paginate'
+require 'will_paginate/active_record'
+
 class Weather < ActiveRecord::Base
   validates_presence_of :date
   has_many :trips
+
+  def self.per_page
+    30
+  end
 end

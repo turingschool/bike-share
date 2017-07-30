145fb3dc1c5220d9d455e4f091cc3ee86be114f8
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-07-30 15:13:12 -0600

    changes database path

--- config/database.rb
+++ config/database.rb
@@ -1,14 +1,15 @@
 # set the database based on the current environment
 database_name = "bike-share-#{BikeShareApp.environment}"
-db = URI.parse(ENV['DATABASE_URL'] || "postgres://localhost/#{database_name}")
+
 
 # connect ActiveRecord with the current database
+db = URI.parse(ENV['DATABASE_URL'] || "postgres://localhost/#{database_name}")
 ActiveRecord::Base.establish_connection(
   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
   :host     => db.host,
   :port     => db.port,
   :username => db.user,
   :password => db.password,
-  :database => "#{database_name}",
+  :database => db.path[1..-1],
   :encoding => 'utf8'
 )

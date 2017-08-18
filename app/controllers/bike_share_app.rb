575c953c85b48a9cdb7acd55c0740934ffef0a95
sidewinder2020 <sdwinder1@gmail.com>
2017-08-18 16:53:37 -0600

    create index, create erbs for station, flesh out controller

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -1,3 +1,14 @@
 class BikeShareApp < Sinatra::Base
+  get '/' do
+    erb :dashboard
+  end
 
+  get '/stations' do
+    @stations = Station.all
+    erb :'/station/index'
+  end
+
+  get '/stations/new' do
+    erb :'station/new'
+  end
 end

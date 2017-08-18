6a29e2b8874cbda10ee314269949598396c6efe6
Brandon Stewart <boveus@gmail.com>
2017-08-17 21:47:57 -0600

    Fix the thing.

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -6,7 +6,7 @@ class Trip < ActiveRecord::Base
   validates :duration, :start_date, :end_date, :start_station, :end_station, :bike_id, :subscription_type_id, presence: true
 
   def self.find_thirty_trips(start_index)
-    find_range = (start_index..(start_index + 29)).to_a
+    find_range = (start_index...(start_index + 30)).to_a
     find(find_range)
   end
 end

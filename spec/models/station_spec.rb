2f2d38b01285cc224380f06ca31dac0af0721012
snayrouz <samuel.nayrouz@gmail.com>
2017-08-18 12:37:52 -0600

    Pass station spec validations

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -5,5 +5,24 @@ RSpec.describe Station do
 
       expect(station1).to_not be_valid
     end
+
+    it 'is invalid without a dock count' do
+      station1 = Station.create(name: "San Jose Civic Center", city: "San Jose", installation_date: "8/15/13")
+
+      expect(station1).to_not be_valid
+    end
+
+    it 'is invalid without a city' do
+      station1 = Station.create(name: "San Jose Civic Center", dock_count: 27, installation_date: "8/15/13")
+
+      expect(station1).to_not be_valid
+    end
+
+    it 'is invalid without a installation date' do
+      station1 = Station.create(name: "San Jose Civic Center", dock_count: 27, city: "San Jose")
+
+      expect(station1).to_not be_valid
+    end
+
   end
 end

23f444ba36adfdbd13fea4ba02a8837681813196
Boots2291 <buteramv@gmail.com>
2017-08-17 19:30:39 -0600

    Add DatabaseCleaner gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,16 @@ require 'capybara/dsl'
 
 Capybara.app = BikeShareApp
 
+DatabaseCleaner.strategy = :truncation
+
 RSpec.configure do |c|
   c.include Capybara::DSL
+
+  c.before(:all) do
+    DatabaseCleaner.clean
+  end
+
+  c.after(:each) do
+    DatabaseCleaner.clean
+  end
 end

b89e73cd6436c065adae14ea147411bd38ca3125
jayphodges <jayphodges@gmail.com>
2017-08-18 16:56:05 -0600

    Initial rspec setup

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -2,6 +2,11 @@ require_relative ""
 
 RSpec.describe Station do
   describe "Validations" do
+    it "validations city, name, dock_count, and installation date" do
+      date = DateTime.now
+      valid_station = Station.create(name: "StationOne", city_id: 1, dock_count: 10, installation_date: date)
 
+      expect(valid_station).to be_valid
+    end
   end
 end

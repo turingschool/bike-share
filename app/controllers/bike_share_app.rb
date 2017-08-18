8dc544229fc5d07b963e71486b79acd2703f414b
mimilettd <mimilettd@gmail.com>
2017-08-18 16:47:18 -0600

    user visits stations and can see all stations

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -1,5 +1,16 @@
 class BikeShareApp < Sinatra::Base
+  get '/' do
+    erb :index
+  end
 
+  get '/stations' do
+    @stations = Station.all
+    erb :'stations/index'
+  end
 
+  get '/stations/new' do
+    @station = Station.new
+    erb :'stations/new'
+  end
 
 end

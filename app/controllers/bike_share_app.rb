f7b19be373108783630da60625d8ca7cd33e7c31
sidewinder2020 <sdwinder1@gmail.com>
2017-08-18 17:09:13 -0600

    create station routes in controller

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -11,4 +11,29 @@ class BikeShareApp < Sinatra::Base
   get '/stations/new' do
     erb :'station/new'
   end
+
+  post '/stations' do
+    station = Station.new(params)
+    station.save
+    redirect '/stations'
+  end
+
+  get '/stations/:id' do |id|
+    @station = Station.find(id)
+    erb :'/station/show'
+  end
+
+  get '/stations/:id/edit' do |id|
+    @station = Station.find(id)
+    erb :'/station/edit'
+  end
+
+  put '/stations/:id/edit' do |id|
+    @station = Station.find(id)
+    redirect "/stations/#{id}"
+  end
+
+  delete '/stations/:id' do
+    Station.destroy[:id].to_i
+  end
 end

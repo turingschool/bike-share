8f10c2df4d71d58dd4a0d22cb589ac2cf43d3633
Sam Lim <limsammy1@gmail.com>
2017-08-18 17:21:24 -0600

    more explicit validation tests

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -8,9 +8,29 @@ RSpec.describe Station do
 
     end
 
-    it "invalid station cannot be created" do
-      date = DateTime.now
+    it "invalid station cannot be created with no date" do
       invalid_station = Station.create(name: "StationOne", city_id: 1, dock_count: 10)
+
+      expect(invalid_station).to_not be_valid
+    end
+
+    it 'invalid station cannot be created without name' do
+      date = DateTime.now
+      invalid_station = Station.create(city_id: 1, dock_count: 10, installation_date: date)
+
+      expect(invalid_station).to_not be_valid
+    end
+
+    it 'invalid station cannot be created without city_id' do
+      date = DateTime.now
+      invalid_station = Station.create(name: "StationOne", dock_count: 10, installation_date: date)
+
+      expect(invalid_station).to_not be_valid
+    end
+
+    it 'invalid station cannot be created without dock_count' do
+      date = DateTime.now
+      invalid_station = Station.create(name: "StationOne", city_id: 1, installation_date: date)
 
       expect(invalid_station).to_not be_valid
     end

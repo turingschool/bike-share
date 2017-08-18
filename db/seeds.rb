f426bb7a4733d2db15d8ace2231a8d928dda6c3a
Brandon Stewart <boveus@gmail.com>
2017-08-17 23:12:47 -0600

    Add seed file that might work.

---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+require 'csv'
+require './app/models/trip.rb'
+
+csv_file = File.read('db/csv/trip.csv')
+csv = CSV.parse(csv_file, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Trip.new
+  t.duration = row['duration']
+  t.start_date = row['start_date']
+  t.end_date = row['end_date']
+  t.start_station = row['start_station']
+  t.end_station = row['end_station']
+  t.bike_id = row['bike_id']
+  t.subscription_type_id = row['subscription_type_id']
+  t.zip_code = ['zip_code']
+  t.save
+end
+
+puts "There are now #{Trip.count} rows in the trips table"

b7ab1905ba532c9a8616dd7e971e3c4e483446eb
sidewinder2020 <sdwinder1@gmail.com>
2017-08-19 13:56:24 -0600

    Create CRUD, Add view folders, Begin Index and Dashboard

---
+++ app/models/trip.rb
@@ -0,0 +1,10 @@
+class Trip < ActiveRecord::Base
+  validates :duration, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :start_station, presence: true
+  validates :end_station, presence: true
+  validates :bike_id, presence: true
+  validates :subscription_type, presence: true
+  validates :zip_code, presence: true
+end

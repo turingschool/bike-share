489d9f38dde15d70a14a0e40b2ad8c2545037ad3
JunePaloma <kborkoski@gmail.com>
2017-08-19 17:23:08 -0600

    Add dope erb code

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -6,15 +6,17 @@ class BikeShareApp < Sinatra::Base
   end
 
   get '/trips/:id' do
+    erb :trip_show
 
   end
 
   get '/trips/new' do
+    erb :trip_new
 
   end
 
   get '/trips/:id/edit' do
-
+    erb :trip_edit
   end
 
 end

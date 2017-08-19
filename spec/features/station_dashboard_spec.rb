922e3031c24dfcfdfa9ecbc67f3c330a33a48506
Becki Srofe <blf_23@yahoo.com>
2017-08-19 14:37:41 -0600

    Finish feature tests for iteration 3

--- spec/features/station_dashboard_spec.rb
+++ spec/features/station_dashboard_spec.rb
@@ -28,4 +28,49 @@ RSpec.describe "User visits '/station_dashboard'" do
 
     expect(page).to have_content("8")
   end
+
+  it "returns station with the most bikes" do
+    Station.create(name: "Station_1", dock_count: 8, city: "San Fran", installation_date: "8/6/2013")
+    Station.create(name: "Station_2", dock_count: 6, city: "San Diego", installation_date: "8/7/2013")
+
+    visit '/station_dashboard'
+
+    expect(page).to have_content("Station_1")
+  end
+
+  it "returns least bikes available at a station" do
+    Station.create(name: "Station_1", dock_count: 8, city: "San Fran", installation_date: "8/6/2013")
+    Station.create(name: "Station_2", dock_count: 6, city: "San Fran", installation_date: "8/7/2013")
+
+    visit '/station_dashboard'
+
+    expect(page).to have_content("6")
+  end
+
+  it "returns station with the fewest bikes" do
+    Station.create(name: "Station_1", dock_count: 8, city: "San Fran", installation_date: "8/6/2013")
+    Station.create(name: "Station_2", dock_count: 6, city: "San Diego", installation_date: "8/7/2013")
+
+    visit '/station_dashboard'
+
+    expect(page).to have_content("Station_2")
+  end
+
+  it "returns most recently installed station" do
+    Station.create(name: "Station_1", dock_count: 8, city: "San Fran", installation_date: "8/6/2013")
+    Station.create(name: "Station_2", dock_count: 6, city: "San Diego", installation_date: "8/7/2013")
+
+    visit '/station_dashboard'
+
+    expect(page).to have_content("Station_2")
+  end
+
+  it "returns oldest station" do
+    Station.create(name: "Station_1", dock_count: 8, city: "San Fran", installation_date: "8/6/2013")
+    Station.create(name: "Station_2", dock_count: 6, city: "San Diego", installation_date: "8/7/2013")
+
+    visit '/station_dashboard'
+
+    expect(page).to have_content("Station_1")
+  end
 end

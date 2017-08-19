f01ecd5cf787918c08d0e1667dcaadb96fba30f9
samuelssnider <sam_redins@yahoo.com>
2017-08-19 15:17:06 -0600

    Working on station feature tests

---
+++ spec/features/STATION/user_can_edit_a_station_spec.rb
@@ -0,0 +1,18 @@
+RSpec.describe "User can update a station" do
+	it "When visiting a station's edit page" do
+	Station.create(name: "Turing", city: "Hell", installation_date: "1969/4/20", dock_count: 69)
+
+	visit '/stations'
+	click_on('Edit')
+	expect(page).to have_content("Edit your Station")
+
+	fill_in('station[name]',                with: 'Mushroom')
+	fill_in('station[city]',              with: 'Englewood')
+	fill_in('station[installation_date]', with: '2012/1/1' )
+	fill_in('station[dock_count]',        with: "20"       )
+
+	click_on('edit the station')
+	expect(current_path).to eq("/stations")
+
+	end
+end

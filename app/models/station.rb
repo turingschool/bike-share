1a4cab7920003dbeb1cecd411126625288d11555
sidewinder2020 <sdwinder1@gmail.com>
2017-08-18 19:48:04 -0600

    create methods for station model: oldest station, newest station, installation date, station count

--- app/models/station.rb
+++ app/models/station.rb
@@ -8,5 +8,20 @@ class Station < ActiveRecord::Base
     self.count
   end
 
+  def self.most_recent_installation_date
+    self.maximum(:installation_date)
+  end
+
+  def self.oldest_installation_date
+    self.minimum(:installation_date)
+  end
+
+  def self.oldest_station
+    self.where(installation_date: oldest_installation_date)
+  end
+
+  def self.newest_station
+    self.where(installation_date: most_recent_installation_date)
+  end
 
 end

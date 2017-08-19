b680548cbf901c6139def310b7c747a430d58089
Ricky Amparo <rickyamparo@gmail.com>
2017-08-19 12:24:01 -0600

    create stations dashboard

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -1,3 +1,14 @@
 class BikeShareApp < Sinatra::Base
+  set :root, File.expand_path("..", __dir__)
+  set :method_override, true
+
+  get '/stations' do
+    erb :stations
+  end
+
+  get '/stations/all' do
+    @stations = Station.all
+    erb :all
+  end
 
 end

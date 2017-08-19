571b9c2ba1a157fc31a914fc4f33ab1887356405
Daniel Phillips <danieledward.phillips@gmail.com>
2017-08-19 13:57:32 -0600

    does stuff does stuff

--- spec/features/user_can_delete_stations_spec.rb
+++ spec/features/user_can_delete_stations_spec.rb
@@ -1,9 +1,9 @@
 describe "User can delete station" do
   it "and click on button" do
 
-    Station.create!(name: "Galvanize", dock_count: 10, city: "Denver", installation_date: "11/08/17")
+    Station.create(name: "Galvanize", dock_count: 10, city: "Denver", installation_date: "11/08/17")
 
-    Station.create!(name: "Turing", dock_count: 10, city: "Denver", installation_date: "11/08/17")
+    Station.create(name: "Turing", dock_count: 10, city: "Denver", installation_date: "11/08/17")
 
     visit '/stations/1'
     click_on("delete")

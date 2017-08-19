a788f65b3947fc7a254effd3c45c548266da037c
Ricky Amparo <rickyamparo@gmail.com>
2017-08-19 14:56:47 -0600

    add bike_share.erb, add route in controller

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -2,6 +2,10 @@ class BikeShareApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
   set :method_override, true
 
+  get '/' do
+    erb :bike_share
+  end
+
   get '/stations' do
     erb :stations
   end

caace3148a640b9a41bc6a9caea391a0d507486c
Daniel Phillips <danieledward.phillips@gmail.com>
2017-08-19 12:37:28 -0600

    playing with csv import

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,27 @@
 require 'csv'
 require './app/models/station.rb'
 
-def load_station(data)
-  csv_text = File.read(data)
-  csv = CSV.parse(csv_text, :headers => true)
-  csv.each do |row|
-    s = Station.new
-    s.name = row['name']
-    s.dock_count = row['dock_count']
-    s.city = row['city']
-    s.installation_date = Date.strptime(row['installation_date'], "%m/%d/%Y")
-    s.save!
-  end
-end
+#def load_station(data)
+  #csv_text = File.read(data)
+  #csv = CSV.parse(csv_text, :headers => true)
+  #csv.each do |row|
+    #s = Station.new
+    #s.name = row['name']
+    #s.dock_count = row['dock_count']
+    #s.city = row['city']
+    #s.installation_date = Date.strptime(row['installation_date'], "%m/%d/%Y")
+    #s.save!
+  #end
+#end
 
 load_station("./db/csv/station.csv")
+
+
+  def load_trips(data)
+  #load copy csvs using  COPY FROM
+
+   #format columns
+    ##try benchmarking
+    #
+
+  end

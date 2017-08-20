0fc40232a992dd6001c243ba7f48b7300737e591
Sam Lim <limsammy1@gmail.com>
2017-08-20 08:05:19 -0600

    css edits

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -9,26 +9,26 @@ class BikeShareApp < Sinatra::Base
     erb :'stations/index'
   end
 
+  get '/stations/new' do
+    @cities = City.all
+    erb :'stations/new'
+  end
+
+  post '/stations' do
+    city = City.find(params[:station][:city_id])
+    @station = Station.create(params[:station])
+    city.stations << @station
+    redirect "/stations/#{@station.id}"
+  end
+
   get '/stations/:id' do
     @station = Station.find(params[:id])
     erb :'stations/show'
   end
-
-  # get '/stations/new' do
-  #   @cities = City.all
-  #   erb :'stations/new'
-  # end
 
   get '/cities' do
     @cities = City.all
     @stations = Station.all
     erb :'cities/index'
   end
-
-  # post '/stations' do
-  #   city = City.find(params[:station][:city_id])
-  #   @station = Station.create(params[:station])
-  #   city.stations << @station
-  #   redirect "/stations/#{@station.id}"
-  # end
 end

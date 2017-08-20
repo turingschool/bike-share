f5a0520163434c299c3554c4aa878f38ff3809fd
JunePaloma <kborkoski@gmail.com>
2017-08-19 18:10:41 -0600

    Add more dope erb shit for view trips

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -5,15 +5,24 @@ class BikeShareApp < Sinatra::Base
     erb :trip_index
   end
 
-  get '/trips/:id' do
-    erb :trip_show
 
+  post '/trips' do
+    @trip = Trip.new(params)
+    trip.save
+    redirect'/trips'
   end
 
   get '/trips/new' do
     erb :trip_new
+  end
+
+
+  get '/trips/:id' do
+    @trip = Trip.find(params["id"])
+    erb :show_trip
 
   end
+
 
   get '/trips/:id/edit' do
     erb :trip_edit

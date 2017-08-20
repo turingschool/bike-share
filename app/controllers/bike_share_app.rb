b6899aba6832d150b3ff14aff7b3068e18417d7c
Sam Lim <limsammy1@gmail.com>
2017-08-20 05:35:50 -0600

    added relations and created new station erb

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -7,4 +7,16 @@ class BikeShareApp < Sinatra::Base
     @stations = Station.all
     erb :'stations/index'
   end
+
+  get '/stations/new' do
+    @cities = City.all
+    erb :'stations/new'
+  end
+
+  # post '/stations' do
+  #   city = City.find(params[:station][:city_id])
+  #   @station = Station.create(params[:station])
+  #   city.stations << @station
+  #   redirect "/stations/#{@station.id}"
+  # end
 end
---
+++ spec/features/UserVisitsHomepage_spec.rb
@@ -0,0 +1,23 @@
+RSpec.describe "User can interact with homepage" do
+  describe "visiting the page" do
+    it "shows the navbar" do
+      visit("/")
+
+      expect(page).to have_css(".nav")
+
+      within(:css, ".nav") do
+        expect(page).to have_content("Station Index")
+        expect(page).to have_content("City Index")
+      end
+    end
+
+    it "lets you access station index" do
+      visit("/")
+      within(:css, ".container#button") do
+        click_button("Station Index")
+
+        expect(page).to have_content("Station Index")
+      end
+    end
+  end
+end

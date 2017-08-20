45747d1f7ee1730dba39b87c50d0b2480fc66152
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-08-20 17:24:03 -0600

    fixed station edit and show features to include trip reference

--- spec/features/STATION/user_can_edit_a_station_spec.rb
+++ spec/features/STATION/user_can_edit_a_station_spec.rb
@@ -1,6 +1,8 @@
 RSpec.describe "User can update a station" do
 	it "when visiting a station's edit page" do
 	Station.create(name: "Turing", city: "Hell", installation_date: "1969/4/20", dock_count: 69)
+	Trip.create(duration: 40000, start_date: "1969/4/20", end_date: "1969/4/20", start_station_id: 1, end_station_id: 1, bike_id: 666, subscription_type: "customer")
+
 
 	visit '/stations'
 	click_on('Edit')
--- spec/features/STATION/user_can_see_a_station_spec.rb
+++ spec/features/STATION/user_can_see_a_station_spec.rb
@@ -1,6 +1,8 @@
 RSpec.describe "User can see a specific station" do
 	it "When visiting station / station id" do
 		Station.create(name: "Turing", city: "Hell", installation_date: "1969/4/20", dock_count: 69)
+		Trip.create(duration: 40000, start_date: "1969/4/20", end_date: "1969/4/20", start_station_id: 1, end_station_id: 1, bike_id: 666, subscription_type: "customer")
+
 
 		visit 'stations/1'
 

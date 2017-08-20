06d97fb05cd95dca95ead793823c34c0fd2efe13
amhursh <amhursh@gmail.com>
2017-08-19 20:01:42 -0600

    Fix feature test for all trips

--- spec/features/trips/user_can_see_all_trips_spec.rb
+++ spec/features/trips/user_can_see_all_trips_spec.rb
@@ -12,14 +12,14 @@ describe 'User visits trips' do
                            start_date: "2013-08-29 09:08:00",
                            start_station_name: "2nd at South Park",
                            end_date: "2013-08-29 09:11:00",
-                           end_station_name: "10th at South Park",
+                           end_station_name: "Hello Kitty",
                            bike_id: 288,
                            subscription_type: "Subscriber",
                            zip_code: "94114")
 
     visit '/trips'
 
-    expect(page).to have_content(trip_one)
-    expect(page).to have_content(trip_two)
+    expect(page).to have_content("10th at South Park")
+    expect(page).to have_content("Hello Kitty")
   end
 end

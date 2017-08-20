0934020cc0e084d7c08834c97e7975a7278b6b85
snayrouz <samuel.nayrouz@gmail.com>
2017-08-20 11:49:48 -0600

    Complete part 1 of iteration 6

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -24,25 +24,35 @@ class Trip < ActiveRecord::Base
     Station.find(trip)[:name]
   end
 
-  def month_breakdown
-    Trip.group('(EXTRACT(YEAR FROM end_date))::integer').group('(EXTRACT(MONTH FROM end_date))::integer').order('count_all DESC').count
+  def self.month_breakdown
+    group('(EXTRACT(YEAR FROM end_date))::integer').group('(EXTRACT(MONTH FROM end_date))::integer').order('count_all DESC').count
   end
 
   def self.most_ridden_bike
     group_by(:bike_id).order('bike_id DESC').count(:id)
   end
 
-  def least_ridden_bike
-    Trip.group(:bike_id).order('count_id DESC').limit(1).count(:id).first.first
+  def self.least_ridden_bike
+    group(:bike_id).order('count_id DESC').limit(1).count(:id).first.first
   end
 
-  def subscription_type
+  def self.subscription_info
+    group(:subscription_id).limit(2).count(:id)
   end
 
-  def date_with_highest_trips
+  def self.subscription_percentage(amount)
+    total = Trip.group(:subscription_id).limit(2).count(:id).values.reduce(:+)
+    (amount / total.to_f * 100).round(2)
   end
 
-  def date_with_fewest_trips
+  def self.date_with_highest_trips
+    date = Trip.group('(EXTRACT(MONTH FROM start_date))::integer').group('(EXTRACT(DAY FROM start_date))::integer').group('(EXTRACT(YEAR FROM start_date))::integer').order('count_all DESC').count.first
+    "#{date[0][0]}/#{date[0][1]}/#{date[0][2]}: #{date[1]}"
+  end
+
+  def self.date_with_fewest_trips
+    date = Trip.group('(EXTRACT(MONTH FROM start_date))::integer').group('(EXTRACT(DAY FROM start_date))::integer').group('(EXTRACT(YEAR FROM start_date))::integer').order('count_all').count.first
+    "#{date[0][0]}/#{date[0][1]}/#{date[0][2]}: #{date[1]}"
   end
 
 

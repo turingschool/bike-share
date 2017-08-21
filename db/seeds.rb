fe1892f88e0a3efbc8c206577bf4eece757db6ae
Sam Lim <limsammy1@gmail.com>
2017-08-21 11:11:02 -0600

    fix seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,6 @@ require 'csv'
 require_relative '../app/helpers/csv_loader'
 require_relative '../app/models/station'
 require_relative '../app/models/city'
-require_relative '../app/models/trip'
-
 
 loader = CSVLoader.new
 
@@ -23,25 +21,4 @@ station_data.each do |station|
                   city_id: city.id,
                   installation_date: station[:installation_date]
                   )
-
-trip_data = loader.sanitize_trips('./db/csv/trip.csv')
-trip_data.each do |trip|
-  puts "Seeding db_trips with: #{trip[:start_station_id]}"
-
-  if Station.find_by(id: station[:start_station_id]).nil?
-    station = Station.create(id: station[:start_station_id])
-  else
-    station = Station.find_by(id: station[:start_station_id])
-  end
-
-  Trip.create(duration: trip[:duration],
-              start_date: trip[:start_date],
-              start_station_id: trip[:start_station_id],
-              end_date: trip[:end_date],
-              end_station_id: trip[:end_station_id],
-              bike_id: trip[:bike_id],
-              subscription_type: trip[:subscription_type],
-              zip_code: trip[zip_code]
-              )
 end
-

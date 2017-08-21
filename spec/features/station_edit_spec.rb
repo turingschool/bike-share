510fcb6f7331f4b526d3e363909e6f3feecb3adf
Ricky Amparo <rickyamparo@gmail.com>
2017-08-21 11:59:01 -0600

    add other attributes to the test, testing all necessary attributes

--- spec/features/station_edit_spec.rb
+++ spec/features/station_edit_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "User visits /stations/:id/edit" do
     station = Station.create(name: "Station_1",
                           dock_count: 20,
                           city: "bike town",
-                          installation_date: "3/2/15")
+                          installation_date: "2017-08-04")
 
     visit "stations/#{station.id}"
 
@@ -13,8 +13,15 @@ RSpec.describe "User visits /stations/:id/edit" do
     click_on("Edit this station")
 
     fill_in("station[name]", with: "Station_edit")
+    fill_in("station[dock_count]", with: 30)
+    fill_in("station[city]", with: "cityville")
+    fill_in("station[installation_date]", with: "2017-08-05")
     click_on("edit")
 
     expect(page).to have_content("Station_edit")
+    expect(page).to have_content(30)
+    expect(page).to have_content("cityville")
+    expect(page).to have_content("2017-08-05")
+
   end
 end

f34e31fb1fbb2b66b582d2ab3dc557fdac24345d
samuelssnider <sam_redins@yahoo.com>
2017-08-21 17:07:10 -0600

    tests working again

--- spec/features/TRIP/user_can_edit_a_trip_spec.rb
+++ spec/features/TRIP/user_can_edit_a_trip_spec.rb
@@ -1,5 +1,6 @@
 RSpec.describe "User can edit a trip" do
   it "when user clicks on edit" do
+  Trip.create(duration: 40000, start_date: "1969/4/20", end_date: "1969/4/20", start_station_id: 1, end_station_id: 1, bike_id: 666, subscription_type: "customer")
   Station.create(name: "Turing", city: "Hell", installation_date: "1969/4/20", dock_count: 69)
 
 

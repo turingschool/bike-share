fc1ab6abf998da77782115d44f920dd1ff881e89
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-08-21 11:25:51 -0600

    test for longest ride

--- spec/models/trips/trips_spec.rb
+++ spec/models/trips/trips_spec.rb
@@ -79,10 +79,6 @@ RSpec.describe Trip do
 
 	describe "Class Methods" do
 		it "Gets the average duration of a ride" do
-			station_one   = Station.create(name: "Turing", city: "Hell", installation_date: "1969/4/20", dock_count: 69)
-			station_two   = Station.create(name: "Mushroom", city: "Englewood", installation_date: "1982/3/10", dock_count: 413)
-
-
 			trip_1 = Trip.create(duration: 60, start_date: "1969/4/20", end_date: "1969/4/21",
 												 start_station_id: 1, end_station_id: 2, bike_id: 4,
 			 									 subscription_type: "Some Nonsense", zip_code: "80113")
@@ -101,5 +97,25 @@ RSpec.describe Trip do
 
 			expect(avg_trip_duration).to eq("02:00")
 		end
+
+		it "Gets the longest ride" do
+			trip_1 = Trip.create(duration: 660, start_date: "1969/4/20", end_date: "1969/4/21",
+												 start_station_id: 1, end_station_id: 2, bike_id: 4,
+			 									 subscription_type: "Some Nonsense", zip_code: "80113")
+
+			trip_2 = Trip.create(duration: 1200, start_date: "1969/4/20", end_date: "1969/4/21",
+												 start_station_id: 1, end_station_id: 2, bike_id: 4,
+			 									 subscription_type: "Some Nonsense", zip_code: "80113")
+
+
+			trip_3 = Trip.create(duration: 180000, start_date: "1969/4/20", end_date: "1969/4/21",
+												 start_station_id: 1, end_station_id: 2, bike_id: 4,
+			 									 subscription_type: "Some Nonsense", zip_code: "80113")
+
+
+			expect(Trip.longest_ride).to eq("2 days")
+		end
+
+
 	end
 end

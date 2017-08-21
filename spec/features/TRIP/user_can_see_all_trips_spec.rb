99000032a1bff6b25173152aad1d8ad216dd981f
samuelssnider <sam_redins@yahoo.com>
2017-08-21 16:46:27 -0600

    initial commit

--- spec/features/TRIP/user_can_see_all_trips_spec.rb
+++ spec/features/TRIP/user_can_see_all_trips_spec.rb
@@ -1,7 +1,8 @@
 RSpec.describe "user can see all trips" do
   it "when visiting trips index" do
-    Trip.create(duration: 40000, start_date: "1969/4/20", end_date: "1969/4/20", start_station_id: 69, end_station_id: 666, bike_id: 666, subscription_type: "customer")
-    Trip.create(duration: 60000, start_date: "1969/5/20", end_date: "1969/6/20", start_station_id: 69, end_station_id: 66, bike_id: 69, subscription_type: "subscriber")
+    Station.create(name: "Turing", city: "Hell", installation_date: "1969/4/20", dock_count: 69)
+    Trip.create(duration: 40000, start_date: "1969/4/20", end_date: "1969/4/20", start_station_id: 1, end_station_id: 1, bike_id: 666, subscription_type: "customer")
+    Trip.create(duration: 60000, start_date: "1969/5/20", end_date: "1969/6/20", start_station_id: 1, end_station_id: 1, bike_id: 69, subscription_type: "subscriber")
 
     visit '/trips'
 
--- spec/features/conditions/user_can_view_station_index_spec.rb
+++ spec/features/conditions/user_can_view_station_index_spec.rb
@@ -1,8 +1,10 @@
 RSpec.describe "User can visit" do
 	it "condition index" do
-		Condition.create(date: "1969/4/20", min_temperature: 30, mean_temperature: 40,
-		 								 max_temperature: 50, mean_humidity: 30, mean_visibility: 95,
-										 mean_wind_speed: 8, precipitation: 0.1                      )
+		binding.pry
+		Condition.create(weather_date: "1969/4/20", min_temperature: 30.0, mean_temperature: 40.0,
+		 								 max_temperature: 50.0, mean_humidity: 30.0, mean_visibility: 95.0,
+										 mean_wind_speed: 8.0, precipitation: 0.1                      )
+
 
 		visit "/conditions"
 

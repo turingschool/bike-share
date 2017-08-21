315198389cfdb2fa55129800e8b2b1ad4e093d10
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-08-21 14:49:37 -0600

    most popular ending station test

--- spec/models/trips/trips_spec.rb
+++ spec/models/trips/trips_spec.rb
@@ -161,5 +161,26 @@ RSpec.describe Trip do
 			expect(Station.most_popular_starting_station).to eq(station_one.name)
 		end
 
+		it "Gets the station with the most rides as an ending place" do
+			station_one   = Station.create(name: "Turing", city: "Hell", installation_date: "1969/4/20", dock_count: 69)
+			station_two   = Station.create(name: "Mushroom", city: "Englewood", installation_date: "1982/3/10", dock_count: 413)
+
+			trip_1 = Trip.create(duration: 600, start_date: "1969/4/20", end_date: "1969/4/21",
+												 start_station_id: 1, end_station_id: 2, bike_id: 4,
+												 subscription_type: "Some Nonsense", zip_code: "80113")
+
+			trip_2 = Trip.create(duration: 1200, start_date: "1969/4/20", end_date: "1969/4/21",
+												 start_station_id: 1, end_station_id: 2, bike_id: 4,
+												 subscription_type: "Some Nonsense", zip_code: "80113")
+
+
+			trip_3 = Trip.create(duration: 180000, start_date: "1969/4/20", end_date: "1969/4/21",
+												 start_station_id: 2, end_station_id: 1, bike_id: 4,
+												 subscription_type: "Some Nonsense", zip_code: "80113")
+
+
+			expect(Station.most_popular_ending_station).to eq(station_two.name)
+		end
+
 	end
 end

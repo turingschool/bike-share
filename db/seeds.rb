12df59a0c70473504bf41956a2a78b457fe3aa4b
Brandon Stewart <brandonsstewart88@gmail.com>
2017-08-20 18:16:26 -0600

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,8 @@ require 'CSV'
 require 'pry'
 
 CSV.foreach('./db/csv/station.csv', :headers => true, :encoding => 'ISO-8859-1') do |row|
+  #We may need to review this portion of the code. How does this join the city object to the Station table?
+  
   city = City.find_or_create_by(city: row["city"])
 
   Station.create(name: row["name"],

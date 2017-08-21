d34925df2286d78f383a5d0e1524c95588021292
samuelssnider <sam_redins@yahoo.com>
2017-08-21 12:34:49 -0600

    conditions migration

---
+++ db/migrate/20170821181415_create_conditions.rb
@@ -0,0 +1,14 @@
+class CreateConditions < ActiveRecord::Migration[5.1]
+  def change
+    create_table :conditions do |t|
+      t.date    :date
+      t.float :max_temperature
+      t.float :mean_temperature
+      t.float :min_temperature
+      t.float :mean_humidity
+      t.float :mean_visibility
+      t.float :mean_wind_speed
+      t.float :precipitation
+    end
+  end
+end

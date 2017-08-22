20ba0a521af311dad5aa12dc6ecf268039dba047
Sam Lim <limsammy1@gmail.com>
2017-08-22 13:05:20 -0600

    Uncommented seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,39 +9,39 @@ require 'activerecord-import'
 
 loader = CSVLoader.new
 
-# station_data = loader.sanitize_station('./db/csv/station.csv')
-# station_data.each do |station|
-#   puts "Seeding db_stations with: #{station[:name]}"
+station_data = loader.sanitize_station('./db/csv/station.csv')
+station_data.each do |station|
+  puts "Seeding db_stations with: #{station[:name]}"
 
-#   city = City.where(name: station[:city]).first_or_create
+  city = City.where(name: station[:city]).first_or_create
 
-#   Station.create(id: station[:id],
-#                   name: station[:name],
-#                   dock_count: station[:dock_count],
-#                   city_id: city.id,
-#                   installation_date: station[:installation_date]
-#                   )
-# end
+  Station.create(id: station[:id],
+                  name: station[:name],
+                  dock_count: station[:dock_count],
+                  city_id: city.id,
+                  installation_date: station[:installation_date]
+                  )
+end
 
-# trip_data = loader.sanitize_trips('./db/csv/trip.csv')
-# count = 0
-# trip_data.each do |trip|
-#   count += 1
-#   puts "Seeding db_trips count: #{count}"
+trip_data = loader.sanitize_trips('./db/csv/trip.csv')
+count = 0
+trip_data.each do |trip|
+  count += 1
+  puts "Seeding db_trips count: #{count}"
 
-#   start_station = Station.find(trip[:start_station_id])
-#   end_station = Station.find(trip[:start_station_id])
+  start_station = Station.find(trip[:start_station_id])
+  end_station = Station.find(trip[:start_station_id])
 
-#   Trip.create(duration: trip[:duration],
-#               start_date: trip[:start_date],
-#               start_station_id: start_station.id,
-#               end_date: trip[:end_date],
-#               end_station_id: end_station.id,
-#               bike_id: trip[:bike_id],
-#               subscription_type: trip[:subscription_type],
-#               zip_code: trip[:zip_code]
-#               )
-# end
+  Trip.create(duration: trip[:duration],
+              start_date: trip[:start_date],
+              start_station_id: start_station.id,
+              end_date: trip[:end_date],
+              end_station_id: end_station.id,
+              bike_id: trip[:bike_id],
+              subscription_type: trip[:subscription_type],
+              zip_code: trip[:zip_code]
+              )
+end
 
 weather_data = loader.sanitize_weather('./db/csv/weather.csv')
 count = 0

b607a13dfbc7acf75643950cd87cd815c4df92a4
Aram Anderson <aram.j.anderson@gmail.com>
2017-08-22 07:28:07 -0600

    Add a map to stations/show

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -33,6 +33,8 @@ class BikeShareApp < Sinatra::Base
 
   get '/stations/:id' do
     @station = Station.find(params[:id])
+    @lat = @station.lat
+    @long = @station.long
     erb :'/stations/show'
   end
 

d9c624e62d02abb9b5e7141614a82d0ca9d7ab95
sidewinder2020 <sdwinder1@gmail.com>
2017-08-22 11:20:25 -0600

    finished station feature tests - sal helped fix the station instance tests, still need refactor for methods to provide for nil case

--- spec/features/STATION/user_can_see_most_frequent_bike_id_spec.rb
+++ spec/features/STATION/user_can_see_most_frequent_bike_id_spec.rb
@@ -5,11 +5,10 @@ RSpec.describe "When user goes to the station id page, user can see" do
     station_c = Station.create(name: "SanFranSokyoeki", city: "Tokyo", installation_date: "1987/6/20", dock_count: 82)
   	Trip.create(duration: 40000, start_date: "1969/4/20", end_date: "1969/4/20", start_station_id: 1, end_station_id: 2, bike_id: 666, subscription_type: "customer")
     Trip.create(duration: 60000, start_date: "1969/5/20", end_date: "1969/6/20", start_station_id: 1, end_station_id: 3, bike_id: 666, subscription_type: "customer")
-    Trip.create(duration: 80000, start_date: "1969/6/20", end_date: "1969/7/20", start_station_id: 1, end_station_id: 2, bike_id: 777, subscription_type: "subscriber")
+    Trip.create(duration: 80000, start_date: "1969/6/20", end_date: "1969/7/20", start_station_id: 1, end_station_id: 1, bike_id: 777, subscription_type: "subscriber")
 
-    visit '/stations/1'
+    visit "/stations/#{station_a.id}"
     save_and_open_page
-    
     expect(page).to have_content(station_a.most_frequent_bike_id)
   end
 end
--- spec/features/STATION/user_can_see_most_popular_destination_station_spec.rb
+++ spec/features/STATION/user_can_see_most_popular_destination_station_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "When user goes to the station id page, user can see" do
     station_b = Station.create(name: "Conjunction Junction", city: "Firenze", installation_date: "1984/7/21", dock_count: 40)
     station_c = Station.create(name: "SanFranSokyoeki", city: "Tokyo", installation_date: "1987/6/20", dock_count: 82)
   	Trip.create(duration: 40000, start_date: "1969/4/20", end_date: "1969/4/20", start_station_id: 1, end_station_id: 2, bike_id: 666, subscription_type: "customer")
-    Trip.create(duration: 60000, start_date: "1969/5/20", end_date: "1969/4/20", start_station_id: 2, end_station_id: 3, bike_id: 666, subscription_type: "customer")
+    Trip.create(duration: 60000, start_date: "1969/5/20", end_date: "1969/4/20", start_station_id: 1, end_station_id: 1, bike_id: 666, subscription_type: "customer")
     Trip.create(duration: 80000, start_date: "1969/5/20", end_date: "1969/7/20", start_station_id: 1, end_station_id: 2, bike_id: 777, subscription_type: "subscriber")
 
 

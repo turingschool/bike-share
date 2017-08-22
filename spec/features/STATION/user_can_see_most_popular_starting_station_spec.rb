de38dec409e20e550f3115762f919809e7347ff2
sidewinder2020 <sdwinder1@gmail.com>
2017-08-22 07:57:06 -0600

    Update station feature tests

--- spec/features/STATION/user_can_see_most_popular_starting_station_spec.rb
+++ spec/features/STATION/user_can_see_most_popular_starting_station_spec.rb
@@ -1,13 +1,14 @@
 RSpec.describe "When user goes to the station analytics page, user can see" do
-  it "average bikes available per station" do
+  it "most popular starting station" do
     Station.create(name: "Turing", city: "Hell", installation_date: "1969/4/20", dock_count: 69)
     Station.create(name: "Conjunction Junction", city: "Firenze", installation_date: "1984/7/21", dock_count: 40)
     Station.create(name: "SanFranSokyoeki", city: "Tokyo", installation_date: "1987/6/20", dock_count: 82)
   	Trip.create(duration: 40000, start_date: "1969/4/20", end_date: "1969/4/20", start_station_id: 1, end_station_id: 2, bike_id: 666, subscription_type: "customer")
     Trip.create(duration: 60000, start_date: "1969/5/20", end_date: "1969/6/20", start_station_id: 2, end_station_id: 3, bike_id: 666, subscription_type: "customer")
-    Trip.create(duration: 80000, start_date: "1969/6/20", end_date: "1969/7/20", start_station_id: 3, end_station_id: 1, bike_id: 777, subscription_type: "subscriber")
+    Trip.create(duration: 80000, start_date: "1969/6/20", end_date: "1969/7/20", start_station_id: 1, end_station_id: 1, bike_id: 777, subscription_type: "subscriber")
 
-    visit '/station-dashboard'
-    expect(page).to have_content(40)
+
+    visit '/stations-dashboard'
+    expect(page).to have_content(Station.most_popular_starting_station)
   end
 end
---
+++ spec/features/STATION/user_can_see_station_with_minimum_dock_count_spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe "When user goes to the station analytics page, user can see" do
+  it "station with minimum dock count" do
+    Station.create(name: "Turing", city: "Hell", installation_date: "1969/4/20", dock_count: 69)
+    Station.create(name: "Conjunction Junction", city: "Firenze", installation_date: "1984/7/21", dock_count: 40)
+    Station.create(name: "SanFranSokyoeki", city: "Tokyo", installation_date: "1987/6/20", dock_count: 82)
+  	Trip.create(duration: 40000, start_date: "1969/4/20", end_date: "1969/4/20", start_station_id: 1, end_station_id: 2, bike_id: 666, subscription_type: "customer")
+    Trip.create(duration: 60000, start_date: "1969/5/20", end_date: "1969/6/20", start_station_id: 2, end_station_id: 3, bike_id: 666, subscription_type: "customer")
+    Trip.create(duration: 80000, start_date: "1969/6/20", end_date: "1969/7/20", start_station_id: 3, end_station_id: 1, bike_id: 777, subscription_type: "subscriber")
+
+
+    visit '/stations-dashboard'
+    expect(page).to have_content(Station.minimum_dock_count)
+    expect(page).to have_content(Station.station_with_minimum_dock_count)
+  end
+end

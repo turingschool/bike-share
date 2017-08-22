a1a621e4247b1cf04123404bfa741570a174e6d6
rspringer47 <rspringer47@gmail.com>
2017-08-22 17:47:06 -0600

    fixing database error

--- config/database.rb
+++ config/database.rb
@@ -1,5 +1,5 @@
 # set the database based on the current environment
-database_name = "da4430k5ug62pn"
+database_name = "bike-share-#{BikeShareApp.environment}"
 db = URI.parse(ENV['DATABASE_URL'] || "postgres://localhost/#{database_name}")
 
 # connect ActiveRecord with the current database

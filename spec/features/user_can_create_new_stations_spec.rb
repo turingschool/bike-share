c61ea78296b1a2c6bf87638c7c13144afb29ea36
Boots2291 <buteramv@gmail.com>
2017-08-22 18:52:09 -0600

    Move tests to a new file

---
+++ spec/features/user_can_create_new_stations_spec.rb
@@ -0,0 +1,31 @@
+describe "User visits ''/stations/new'" do
+  it "and can see create page without error" do
+    visit('/stations/new')
+
+    expect(page.status_code).to be(200)
+    expect(page).to have_content("Name")
+    expect(page).to have_content("Dock Count")
+    expect(page).to have_content("City")
+    expect(page).to have_content("Installation Date")
+  end
+
+  it "and can create a new station" do
+    visit('/stations/new')
+
+    within("form") do
+      fill_in 'name', :with => 'Test Station'
+      fill_in 'dockcount', :with => '12'
+      select('San Jose', :from => 'cities')
+      fill_in 'installdate', :with => '2/2/2013'
+      click_button "Create New Station"
+    end
+
+    save_and_open_page
+    expect(page).to have_content("Test Station")
+    expect(page).to have_content("Dock Count")
+    expect(page).to have_content("12")
+    expect(page).to have_content("City")
+    expect(page).to have_content("San Jose")
+    expect(page).to have_content("Installation Date")
+  end
+end

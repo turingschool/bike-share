ba28dfa484d5a9725f557503e82b4bb24f643279
Sam Lim <limsammy1@gmail.com>
2017-08-23 08:18:53 -0600

    It9 Done, but the methods are fucked. Pushing for collaboration today

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -30,7 +30,7 @@ class Condition < ActiveRecord::Base
 
   def self.sort_wind_max(range)
     amount = []
-    where(mean_wind_speed: range).order('mean_wind_speed DESC').each do |condition|
+    where(mean_wind_speed: range).each do |condition|
       amount << condition.trips
     end
     amount.size
@@ -44,7 +44,6 @@ class Condition < ActiveRecord::Base
     amount.size
   end
 
-  #################### NEEDS REFACTOR... TOO MANY SQL QUERIES
   def self.sort_wind_mean(range)
     amount = 0
     length = where(mean_wind_speed: range).count
@@ -52,6 +51,79 @@ class Condition < ActiveRecord::Base
       amount += Trip.where(start_date: condition[:date]).count
     end
     amount / length
+  end
+
+  def self.sort_precip_max(range)
+    amount = []
+    where(precipitation: range).order('precipitation DESC').each do |condition|
+      amount << condition.trips
+    end
+    amount.size
+  end
+
+  def self.sort_precip_min(range)
+    amount = []
+    where(precipitation: range).order('precipitation ASC').each do |condition|
+      amount << condition.trips
+    end
+    amount.size
+  end
+
+  ############## NEEDS REFACTOR, TOO MANY SQL QUERIES SLOWS PAGE DOWN
+  def self.sort_precip_mean(range)
+    max = self.sort_precip_max(range)
+    min = self.sort_precip_min(range)
+
+    max + min / 2
+    # amount = 0
+    # length = where(precipitation: range).count
+    # where(precipitation: range).each do |condition|
+    #   amount += Trip.where(start_date: condition[:date]).count
+    # end
+    # amount / length
+  end
+
+  def self.sort_sight_max(range)
+    dates = []
+    where(mean_visibility: range).each do |condition|
+      dates << condition.date
+    end
+    nums = []
+    dates.each do |date|
+      nums << Trip.where(start_date: date).count
+    end
+    nums.sort.last
+  end
+
+  def self.sort_sight_min(range)
+    
+  end
+
+  def self.sort_sight_mean(range)
+    
+  end
+
+  # def self.sort_sight(range)
+  #   array = Condition.where(mean_visibility: range).all.map {|condition| condition.date}
+  #   trip_nums = array.map do |date|
+  #     Trip.where(start_date: date.beginning_of_day...date.end_of_day).count
+  #   end
+  #   output = {}
+  #   output[:max] = trip_nums.sort.last
+  #   output[:min] = trip_nums.sort.first
+  #   output[:avg] = trip_nums.inject(:+) / trip_nums.length unless trip_nums.length == 0
+  #   output
+  # end
+
+  # Show Methods
+  def self.condition_on_day_with_highest_rides
+    day_with_highest_rides = Trip.date_with_highest_number_of_trips
+    Condition.find_by(date: day_with_highest_rides)
+  end
+
+  def self.condition_on_day_with_lowest_rides
+    day_with_lowest_rides = Trip.date_with_lowest_number_of_trips
+    Condition.find_by(date: day_with_lowest_rides)
   end
 end
 

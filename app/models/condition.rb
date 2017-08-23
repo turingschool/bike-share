92dfdb1baf6ba3bebf6c14ad608e18b19859d451
Ricky Amparo <rickyamparo@gmail.com>
2017-08-23 17:58:03 -0600

    add guard to make sure condition infor is not nil

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,19 +1,28 @@
 class Condition < ActiveRecord::Base
 
-  # validates :date, presence: true
-  # validates :max_temperature_f, presence: true
-  # validates :mean_temperature_f, presence: true
-  # validates :min_temperature_f, presence: true
-  # validates :mean_humidity, presence: true
-  # validates :mean_visibility_miles, presence: true
-  # validates :mean_wind_speed_mph, presence: true
-  # validates :precipitation_inches, presence: true
+  validates :date, presence: true
+  validates :max_temperature_f, presence: true
+  validates :mean_temperature_f, presence: true
+  validates :min_temperature_f, presence: true
+  validates :mean_humidity, presence: true
+  validates :mean_visibility_miles, presence: true
+  validates :mean_wind_speed_mph, presence: true
+  validates :precipitation_inches, presence: true
 
   def self.convert_csv_to_condition_attributes
     a = Time.now
     conditions = []
     CSV.foreach('db/csv/weather.csv', {headers: true, header_converters: :symbol}) do |row|
-    conditions << Condition.new(date:     Date.strptime(row[:date], '%m/%e/%Y'),
+    row[:max_temperature_f] = 0 if row[:max_temperature_f].nil?
+    row[:mean_temperature_f] = 0 if row[:mean_temperature_f].nil?
+    row[:min_temperature_f] = 0 if row[:min_temperature_f].nil?
+    row[:max_humidity] = 0 if row[:max_humidity].nil?
+    row[:mean_humidity] = 0 if row[:mean_humidity].nil?
+    row[:mean_visibility_miles] = 0 if row[:mean_visibility_miles].nil?
+    row[:mean_wind_speed_mph] = 0 if row[:mean_wind_speed_mph].nil?
+    row[:precipitation_inches] = 0 if row[:precipitation_inches].nil?
+    conditions << Condition.new(
+                    date:     Date.strptime(row[:date], '%m/%e/%Y'),
                     max_temperature_f:     row[:max_temperature_f],
                     mean_temperature_f:    row[:mean_temperature_f],
                     min_temperature_f:     row[:min_temperature_f],

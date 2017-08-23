73c241a8654e9a39e7b7d27150a0004a4a827624
djtrujillo <danny.j.trujillo@gmail.com>
2017-08-23 05:07:24 -0600

    Added pagination to conditions page

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -109,7 +109,7 @@ class BikeShareApp < Sinatra::Base
   end
 
   get '/conditions' do
-    @conditions = Condition.all
+    @conditions = Condition.all.paginate(:page => params[:page], :per_page => 30)
     erb :'/conditions/index'
   end
 

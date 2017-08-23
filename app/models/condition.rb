718abf51e419dd6b49ec6a93ffdffde160897c41
Sam Lim <limsammy1@gmail.com>
2017-08-23 06:54:29 -0600

    Dashboard lists max,min,mean temps for different ranges

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -5,9 +5,11 @@ class Condition < ActiveRecord::Base
   validates :date, :max_temperature, :mean_temperature, :min_temperature, :mean_humidity, :mean_visibility, :mean_wind_speed, :precipitation, presence: true
 
   def self.sort_temp_max(range)
+    amount = []
     where(max_temperature: range).each do |condition|
-      condition.trips.count
+      amount << condition.trips
     end
+    amount.size
   end
 
   def self.sort_temp_min(range)
@@ -24,18 +26,6 @@ class Condition < ActiveRecord::Base
       amount << condition.trips
     end
     amount.size
-  end
-
-  def self.sort_temp(range)
-    array = where(max_temperature: range).all.map {|condition| condition.date}
-    trip_nums = array.map do |date|
-      Trip.where(start_date: date.beginning_of_day...date.end_of_day).count
-    end
-    output = {}
-    output[:max] = trip_nums.sort.last
-    output[:min] = trip_nums.sort.reverse.last
-    output[:avg] = trip_nums.inject(:+) / trip_nums.length unless trip_nums.length == 0
-    output
   end
 end
 

816718dfe5d476fa4a8dc659447d181e8f12887a
Sam Lim <limsammy1@gmail.com>
2017-08-23 07:40:29 -0600

    Wind speed funcs working

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -27,6 +27,32 @@ class Condition < ActiveRecord::Base
     end
     amount.size
   end
+
+  def self.sort_wind_max(range)
+    amount = []
+    where(mean_wind_speed: range).order('mean_wind_speed DESC').each do |condition|
+      amount << condition.trips
+    end
+    amount.size
+  end
+
+  def self.sort_wind_min(range)
+    amount = []
+    where(mean_wind_speed: range).order('mean_wind_speed ASC').each do |condition|
+      amount << condition.trips
+    end
+    amount.size
+  end
+
+  #################### NEEDS REFACTOR... TOO MANY SQL QUERIES
+  def self.sort_wind_mean(range)
+    amount = 0
+    length = where(mean_wind_speed: range).count
+    where(mean_wind_speed: range).each do |condition|
+      amount += Trip.where(start_date: condition[:date]).count
+    end
+    amount / length
+  end
 end
 
 

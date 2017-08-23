2dca58a2239a3cbb641f99eb67a392e350986f1a
Sam Lim <limsammy1@gmail.com>
2017-08-23 06:22:50 -0600

    explicit func name

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -4,7 +4,7 @@ class Condition < ActiveRecord::Base
   has_many :trips, :through => :condition_trips
   validates :date, :max_temperature, :mean_temperature, :min_temperature, :mean_humidity, :mean_visibility, :mean_wind_speed, :precipitation, presence: true
 
-  def self.sort_temp(range)
+  def self.sort_temp_max(range)
     amount = []
     where(max_temperature: range).each do |condition|
       amount << condition.trips

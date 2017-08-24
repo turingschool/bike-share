66b7dd5f95db3d8342a8cfe8bbbe6c3020995ee0
amhursh <amhursh@gmail.com>
2017-08-23 21:38:10 -0600

    Fix drop down selects on trip new view and make tests pass

--- spec/features/trips/user_can_access_new_trip_page_spec.rb
+++ spec/features/trips/user_can_access_new_trip_page_spec.rb
@@ -14,21 +14,30 @@ describe "User can access new trip page" do
   end
   it "and create new trip" do
 
+    Station.create(name: "Panera",
+                   dock_count: 15,
+                   city: "Denver",
+                   installation_date: "2014-04-09")
+    Station.create(name: "Turing",
+                   dock_count: 15,
+                   city: "Denver",
+                   installation_date: "2014-04-09")
+
     visit '/trips/new'
 
     fill_in("trip[duration]", with: 173)
     fill_in("trip[start_date]", with: "2014-08-21 09:08:00")
-    fill_in("trip[start_station_name]", with: "Yoyo")
+    select('Panera', :from => 'start-name-drop-down')
     fill_in("trip[end_date]", with: "2015-08-22 09:08:00")
-    fill_in("trip[end_station_name]", with: "Memememe")
+    select('Turing', :from => 'end-name-drop-down')
     fill_in("trip[bike_id]", with: 299)
     fill_in("trip[subscription_type]", with: "Subscriber")
     fill_in("trip[zip_code]", with: "90210")
     click_on("Create New Trip")
 
     expect(page).to have_content("173")
-    expect(page).to have_content("Yoyo")
-    expect(page).to have_content("Memememe")
+    expect(page).to have_content("Panera")
+    expect(page).to have_content("Turing")
     expect(page).to have_content("Subscriber")
     expect(page).to have_content("90210")
     expect(current_path).to eq('/trips/1')

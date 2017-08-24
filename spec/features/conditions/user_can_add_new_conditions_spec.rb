38b0ee9ceb84836478d4724959b7689959023c4e
JoelLindow <joellindow@gmail.com>
2017-08-23 23:08:46 -0600

    test was failing because of form date format. FIXED IT! Ricky will finish test expectations

--- spec/features/conditions/user_can_add_new_conditions_spec.rb
+++ spec/features/conditions/user_can_add_new_conditions_spec.rb
@@ -1,9 +1,11 @@
+require './app/models/condition'
+
 RSpec.describe "User creates condition" do
   it "with valid attributes" do
 
     visit '/conditions/new'
 
-    fill_in("date", with: "10/20/1990")
+    fill_in("date", with: "2013-08-29")
     fill_in("max_temperature_f", with: 10.0)
     fill_in("mean_temperature_f", with: 5.0)
     fill_in("min_temperature_f", with: 0.0)
@@ -14,9 +16,10 @@ RSpec.describe "User creates condition" do
     fill_in("precipitation_inches", with: 20.0)
     click_on("Submit")
 
-    # save_and_open_page
 
-    expect(page).to have_content("10/20/1990")
+    expect(page).to have_content("2013-08-29")
     expect(page).to have_content(10.0)
+
+
   end
 end

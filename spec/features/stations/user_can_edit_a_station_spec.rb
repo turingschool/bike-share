7cbba9c00feae61757f69a520c80b6c70c1eba5d
Becki Srofe <blf_23@yahoo.com>
2017-08-23 18:02:52 -0600

    Fix all test errors

--- spec/features/stations/user_can_edit_a_station_spec.rb
+++ spec/features/stations/user_can_edit_a_station_spec.rb
@@ -2,10 +2,12 @@ require './app/models/station'
 
 RSpec.describe "User visits /stations/:id/edit" do
   it "when visiting /stations" do
-    station = Station.create(name: "Station_1",
-                            dock_count: 20,
-                            city: "bike town",
-                            installation_date: "2017-08-04")
+    subscription = SubscriptionType.create(subscription_type: "Customer")
+    subscription_1 = SubscriptionType.create(subscription_type: "Subscriber")
+    Trip.create(duration: 49, start_date: "2017-08-20 19:20:22", start_station_id: 1, end_date: "2017-08-20 19:20:22", end_station_id: 1, bike_id: 4, subscription_type: subscription, zip_code: 99000)
+    Trip.create(duration: 9487, start_date: "2017-08-25 19:20:22", start_station_id: 2, end_date: "2017-08-29 19:20:22", end_station_id: 2, bike_id: 566, subscription_type: subscription, zip_code: 99000)
+    Trip.create(duration: 49, start_date: "2017-08-20 19:20:22", start_station_id: 3, end_date: "2017-08-20 19:20:22", end_station_id: 3, bike_id: 999, subscription_type: subscription, zip_code: 99000)
+    station = Station.create(name: "Station_1", dock_count: 20, city: "bike town", installation_date: "2017-08-04")
 
     visit "/stations/#{station.id}"
 

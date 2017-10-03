561e4bfe35ea91cc119cfa2d68c76b659734d290
Adrian Lara <adrianblara@gmail.com>
2017-10-03 16:39:28 -0600

    add route/view to create station form and add route to create station

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -12,9 +12,25 @@ class BikeShareApp < Sinatra::Base
     erb :index
   end
 
+  get '/stations/new' do
+    erb :new
+  end
+
+  post '/stations' do
+    Station.create(params[:station])
+    redirect '/stations'
+  end
+
   get '/stations/:id' do
     @station = Station.find(params[:id])
     erb :show
   end
+
+  #   require 'pry'; binding.pry
+
+#TODO route to see form to update station
+#TODO route to actually update station
+#TODO route to delete station
+
 
 end

f03b4f7c13864dfd3880bf0d98363e3a9edb55aa
Erika Smith <erikaannesmith@gmail.com>
2017-10-04 16:32:07 -0600

    Add iteration 3 functionality

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,3 +1,34 @@
 class Station < ActiveRecord::Base
   validates_presence_of :name, :dock_count, :city, :installation_date
-end 
+
+  def self.average_available_bikes
+    average(:dock_count)
+  end
+
+  def self.maximum_by_bikes
+    maximum(:dock_count)
+  end
+
+  def self.stations_with_max_bikes
+    where(dock_count: maximum_by_bikes).map { |station| station.name }
+  end
+
+  def self.minimum_by_bikes
+    minimum(:dock_count)
+  end
+
+  def self.stations_with_min_bikes
+    where(dock_count: minimum_by_bikes).map { |station| station.name }
+  end
+
+  def self.most_recently_installed
+    max = maximum(:installation_date)
+    where(installation_date: max).map { |station| station.name }
+  end
+
+  def self.earliest_installed
+    min = minimum(:installation_date)
+    where(installation_date: min).map { |station| station.name }
+  end
+
+end

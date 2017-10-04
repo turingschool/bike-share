93f7095fbfa372a08fa008c8cca05b7c97cb9755
Adrian Lara <adrianblara@gmail.com>
2017-10-04 15:19:39 -0600

    seed file loads stations.csv into dev database

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+require 'csv'
+require './app/models/station'
+
+class Seed
+
+  def self.station
+    options = {headers: true, header_converters: :symbol, converters: :numeric}
+    CSV.foreach('./db/csv/station.csv', options) do |row|
+      row[:installation_date] = Date.strptime(row[:installation_date], "%m/%d/%Y")
+      row.delete(:long)
+      row.delete(:lat)
+      Station.create!(row.to_h)
+    end
+  end
+
+end
+
+Seed.station

473b790ade23cd4ba72b7e83ef740ac65ad0573b
Josi <junipertmcclellan@gmail.com>
2017-10-04 00:58:40 -0600

    move lines up and down

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,17 @@
 ENV["RACK_ENV"] ||= "test"
 
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec'
-require 'capybara/dsl'
+require "./config/environment"
 require 'bundler'
-Bundler.require(:default, :test)
 require 'database_cleaner'
+require 'capybara/rspec'
+require 'capybara/dsl'
+require 'rspec'
 
+Bundler.require(:default, :test)
+DatabaseCleaner.strategy = :truncation
 Capybara.app = BikeShareApp
-
 RSpec.configure do |c|
   c.include Capybara::DSL
-end
-
-DatabaseCleaner.strategy = :truncation
-
-RSpec.configure do |c|
   c.before(:all) do
     DatabaseCleaner.clean
   end

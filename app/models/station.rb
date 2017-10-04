039f67bd98d4b0fb571fe8d09b12f6f9915e9993
Hugh Morrison <hmorri32@gmail.com>
2017-10-04 15:55:01 -0600

    station class methods done, iteration 2

--- app/models/station.rb
+++ app/models/station.rb
@@ -6,4 +6,40 @@ class Station < ActiveRecord::Base
               presence: true
 
   belongs_to :city
+
+  def self.station_count
+    count
+  end
+
+  def self.average_bikes
+    average(:dock_count).to_i
+  end
+
+  def self.max_bikes_per_station
+    maximum :dock_count
+  end
+
+  def self.min_bikes_per_station
+    minimum :dock_count
+  end
+
+  def self.stations_with_most_bikes
+    where(dock_count: max_bikes_per_station).pluck(:name).join(', ')
+  end 
+
+  def self.stations_with_least_bikes
+    where(dock_count: min_bikes_per_station).pluck(:name).join(', ')
+  end
+
+  def self.newest_station
+    max     = Station.maximum :installation_date
+    station = Station.find_by  installation_date: max
+    "#{station.name}"
+  end
+
+  def self.oldest_station
+    min     = Station.minimum :installation_date
+    station = Station.find_by  installation_date: min
+    "#{station.name}"
+  end
 end

3e6bdf9856ced502fae71c8349ac1f27565b8590
Tyjoo26 <Tyjoo21@gmail.com>
2017-10-04 17:26:48 -0600

    Build out models/trip.rb

---
+++ app/models/trip.rb
@@ -0,0 +1,12 @@
+class Trip < ActiveRecord::Base
+  validates_presence_of :duration
+                        :start_date
+                        :start_station
+                        :end_date
+                        :end_station
+                        :bike_id
+                        :subscription_type
+                        :zip_code
+  belongs_to 
+  belongs_to :end_station
+end

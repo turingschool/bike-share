51678f1d52aafd43295c9bc32b18a4faa68cd6eb
Miguel Silva <msilva0897@gmail.com>
2017-10-03 18:03:04 -0600

    made forms for post and show

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20171003201727) do
     t.text "name"
     t.integer "dock_count"
     t.text "city"
-    t.datetime "installation_date"
+    t.date "installation_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

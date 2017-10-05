d1dd716388d9de69cbbc435138cbfef2a784eefe
Hugh Morrison <hmorri32@gmail.com>
2017-10-05 15:16:50 -0600

    add lat lng to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171004014603) do
+ActiveRecord::Schema.define(version: 20171005211223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "cities", force: :cascade do |t|
-    t.string "name"
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "stations", force: :cascade do |t|
-    t.text "name"
-    t.integer "dock_count"
-    t.integer "city_id"
-    t.date "installation_date"
+    t.text     "name"
+    t.integer  "dock_count"
+    t.integer  "city_id"
+    t.date     "installation_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "lat"
+    t.integer  "lng"
   end
-
 end

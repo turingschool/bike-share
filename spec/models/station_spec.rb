95cf1f2076f93e553b6fbba614917e1d7d615be8
Josi <junipertmcclellan@gmail.com>
2017-10-04 19:04:13 -0600

    test station dashboard helpers

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -41,7 +41,108 @@ RSpec.describe Station do
       expect(station).to respond_to(:class)
     end
 
+  end
+
+  describe 'with_most_bikes' do
+
+    it "returns empty array for for empty collection" do
+      expect(Station.with_most_bikes).to be_nil
+    end
+
+    it "returns one-length array if not tied" do
+      max = Station.create!(name: "Station 42", dock_count: 6, city: "San Francisco", installation_date: Date.strptime("8/15/2017", '%m/%d/%Y'), lat: 37.332808, long: -121.88389099999999)
+      Station.create!(name: "Station 42", dock_count: 5, city: "San Francisco", installation_date: Date.strptime("8/15/2017", '%m/%d/%Y'), lat: 37.332808, long: -121.88389099999999)
+
+      found = Station.with_most_bikes.to_a
+
+      expect(found).to be_eq [max]
+    end
+
+    it "returns multiple stations if tied" do
+      max_1 = Station.create!(name: "a", dock_count: 6, city: "San Francisco", installation_date: Date.strptime("8/15/2017", '%m/%d/%Y'), lat: 37.332808, long: -121.88389099999999)
+      max_2 = Station.create!(name: "b", dock_count: 6, city: "San Francisco", installation_date: Date.strptime("8/15/2017", '%m/%d/%Y'), lat: 37.332808, long: -121.88389099999999)
+
+      found = Station.with_most_bikes.to_a.sort_by &:name
+
+      expect(found).to be_eq [max_1, max_2]
+    end
 
   end
 
-end
+  describe 'with_least_bikes' do
+
+    it "returns empty array for for empty collection" do
+      expect(Station.with_least_bikes).to be_nil
+    end
+
+    it "returns one-length array if not tied" do
+      min = Station.create!(name: "Station 42", dock_count: 6, city: "San Francisco", installation_date: Date.strptime("8/15/2017", '%m/%d/%Y'), lat: 37.332808, long: -121.88389099999999)
+      Station.create!(name: "Station 42", dock_count: 5, city: "San Francisco", installation_date: Date.strptime("8/15/2017", '%m/%d/%Y'), lat: 37.332808, long: -121.88389099999999)
+
+      found = Station.with_least_bikes.to_a
+
+      expect(found).to be_eq [min]
+    end
+
+    it "returns multiple stations if tied" do
+      max_1 = Station.create!(name: "a", dock_count: 6, city: "San Francisco", installation_date: Date.strptime("8/15/2017", '%m/%d/%Y'), lat: 37.332808, long: -121.88389099999999)
+      max_2 = Station.create!(name: "b", dock_count: 6, city: "San Francisco", installation_date: Date.strptime("8/15/2017", '%m/%d/%Y'), lat: 37.332808, long: -121.88389099999999)
+
+      found = Station.with_least_bikes.to_a.sort_by &:name
+
+      expect(found).to be_eq [max_1, max_2]
+    end
+
+  end
+
+  describe 'newest' do
+
+    it "returns empty array for for empty collection" do
+      expect(Station.newest).to be_nil
+    end
+
+    it "returns one-length array if not tied" do
+      most_recent = Station.create!(installation_date: Date.strptime("8/16/2017", '%m/%d/%Y'), name: "Station 42", dock_count: 5, city: "San Francisco", lat: 37.332808, long: -121.88389099999999)
+      Station.create!(installation_date: Date.strptime("8/15/2017", '%m/%d/%Y'), name: "Station 42", dock_count: 5, city: "San Francisco", lat: 37.332808, long: -121.88389099999999)
+
+      found = Station.newest.to_a
+
+      expect(found).to be_eq [most_recent]
+    end
+
+    it "returns multiple stations if tied" do
+      most_recent_1 = Station.create!(installation_date: Date.strptime("8/15/2017", '%m/%d/%Y'), name: "a", dock_count: 6, city: "San Francisco", lat: 37.332808, long: -121.88389099999999)
+      most_recent_2 = Station.create!(installation_date: Date.strptime("8/15/2017", '%m/%d/%Y'), name: "b", dock_count: 6, city: "San Francisco", lat: 37.332808, long: -121.88389099999999)
+
+      found = Station.newest.to_a.sort_by &:name
+
+      expect(found).to be_eq [most_recent_1, most_recent_2]
+    end
+
+  end
+
+  describe 'oldest' do
+
+    it "returns empty array for for empty collection" do
+      expect(Station.oldest).to be_nil
+    end
+
+    it "returns one-length array if not tied" do
+      least_recent = Station.create!(installation_date: Date.strptime("8/15/2017", '%m/%d/%Y'), name: "Station 42", dock_count: 5, city: "San Francisco", lat: 37.332808, long: -121.88389099999999)
+      Station.create!(installation_date: Date.strptime("8/16/2017", '%m/%d/%Y'), name: "Station 42", dock_count: 5, city: "San Francisco", lat: 37.332808, long: -121.88389099999999)
+
+      found = Station.oldest.to_a
+
+      expect(found).to be_eq [least_recent]
+    end
+
+    it "returns multiple stations if tied" do
+      least_recent_1 = Station.create!(installation_date: Date.strptime("8/15/2017", '%m/%d/%Y'), name: "a", dock_count: 6, city: "San Francisco", lat: 37.332808, long: -121.88389099999999)
+      least_recent_2 = Station.create!(installation_date: Date.strptime("8/15/2017", '%m/%d/%Y'), name: "b", dock_count: 6, city: "San Francisco", lat: 37.332808, long: -121.88389099999999)
+
+      found = Station.oldest.to_a.sort_by &:name
+
+      expect(found).to be_eq [least_recent_1, least_recent_2]
+    end
+
+  end

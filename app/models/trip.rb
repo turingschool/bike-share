d69f338bf8547913d1c4a037fe72174a59d9a382
BLaurenB <blbillington1@gmail.com>
2017-10-04 21:26:31 -0600

    initial CRUD functionality started

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,6 @@
 class Trip < ActiveRecord::Base
     belongs_to :start_station, class_name: "Station"
-    belongs_to :end_station,   class_name: "Station"    
+    belongs_to :end_station,   class_name: "Station"
 
     validates_presence_of :duration,
                           :start_date,
@@ -11,4 +11,24 @@ class Trip < ActiveRecord::Base
                           :subscription_type,
                           :zip_code
 
+  def self.
+  end
+
+
+
 end
+
+=begin
+
+Create full CRUD functionality for a trip with the following characteristics:
+
+* Duration - 
+* Start Date
+* Start Station
+* End Date
+* End Station
+* Bike ID
+* Subscription Type
+* Zip Code
+
+=end

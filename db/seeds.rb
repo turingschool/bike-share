3983ddcd42e832902584d7809b994d3fd02c2570
Miguel Silva <msilva0897@gmail.com>
2017-10-05 15:21:32 -0600

    Seeding done and refactored

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,14 +4,9 @@ require 'csv'
 
 
 CSV.foreach('./db/csv/station.csv', headers: true, header_converters: :symbol) do |station|
-  if !station[:installation_date] == ''
-    new_city = City.create(name: station[:city])
-    station[:installation_date] = Date.strptime(station["installation_date"], "%m/%d/%Y"),
-    new_station = Station.new({name: station[:name],
-                              dock_count: station[:dock_count],
-                              installation_date: station[:installation_date]})
-    new_station.city = new_city if !new_city.id.nil?
-    new_station.city = City.find_by(name: new_city.name) if new_city.id.nil?
-    new_station.save
-  end
+    new_city = City.find_or_create_by(name: station[:city])
+    Station.create(name: station[:name],
+                   dock_count: station[:dock_count],
+                   city_id: new_city.id,
+                   installation_date: Date.strptime(station[:installation_date], "%m/%d/%Y"))
 end

fdf888dbeddb6ff70cfba40d10eecdcfb0e11a72
Hugh Morrison <hmorri32@gmail.com>
2017-10-06 16:21:41 -0600

    pair with tim on debugging tests

--- spec/features/stations_index_spec.rb
+++ spec/features/stations_index_spec.rb
@@ -1,7 +1,7 @@
 describe "station index" do
   before do
-    Station.create(name: "San Jose Diridon Station", dock_count: 27, city: "San Jose", installation_date: 2013-08-06))
-    Station.create(name: "San Jose Civic Center", dock_count: 15, city: "San Jose", installation_date: 2013-08-05)
+    Station.create(name: "San Jose Diridon Station", dock_count: 27, city: "San Jose", installation_date: '2013-08-06')
+    Station.create(name: "San Jose Civic Center", dock_count: 15, city: "San Jose", installation_date: '2013-08-05')
     visit '/'
   end
   it "user sees names of all stations" do
@@ -13,7 +13,7 @@ describe "station index" do
   end
 
   it "user sees installation dates of all stations" do
-    expect(page).to have_content(2013-08-05)
+    expect(page).to have_content('2013-08-05')
   end
 
   it "user clicks delete and goes back to stations indedx" do

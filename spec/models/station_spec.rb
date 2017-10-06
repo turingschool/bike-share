5495ac56e9fb6d868f3579b50a3044b4de279c7b
Hugh Morrison <hmorri32@gmail.com>
2017-10-06 16:11:29 -0600

    add params to station test

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -3,59 +3,59 @@
 RSpec.describe Station do
   context 'Class Methods' do
 
-    before do 
-      City.create(name: 'San Francisco') 
-      City.first.stations.create(name: 'chillest',   dock_count: 20, installation_date: "2017-08-06")
-      City.first.stations.create(name: 'surf shack', dock_count: 10, installation_date: "2011-10-06")
-      City.first.stations.create(name: 'burritos',   dock_count: 7,  installation_date: "2012-09-07")
-      City.first.stations.create(name: 'whatever',   dock_count: 32, installation_date: "2015-08-06")
-      City.first.stations.create(name: 'duplicate',  dock_count: 32, installation_date: "2015-08-06")
+    before do
+      City.create(name: 'San Francisco')
+      City.first.stations.create(name: 'chillest',   dock_count: 20, installation_date: "2017-08-06", lat: 100, lng: 200)
+      City.first.stations.create(name: 'surf shack', dock_count: 10, installation_date: "2011-10-06", lat: 100, lng: 200)
+      City.first.stations.create(name: 'burritos',   dock_count: 7,  installation_date: "2012-09-07", lat: 100, lng: 200)
+      City.first.stations.create(name: 'whatever',   dock_count: 32, installation_date: "2015-08-06", lat: 100, lng: 200)
+      City.first.stations.create(name: 'duplicate',  dock_count: 32, installation_date: "2015-08-06", lat: 100, lng: 200)
     end
 
-    describe ".total_station_count" do 
+    describe ".total_station_count" do
       it 'returns total station count' do
         expect(Station.station_count).to eql(5)
       end
     end
 
-    describe ".average_bikes" do 
-      it "returns average bikes per station" do 
+    describe ".average_bikes" do
+      it "returns average bikes per station" do
         expect(Station.average_bikes).to eql(20)
       end
     end
 
-   describe ".max_bikes_per_station" do 
-      it "returns maximum bikes per station" do 
+   describe ".max_bikes_per_station" do
+      it "returns maximum bikes per station" do
         expect(Station.max_bikes_per_station).to eql(32)
       end
     end
 
-   describe ".min_bikes_per_station" do 
-      it "returns minimum bikes per station" do 
+   describe ".min_bikes_per_station" do
+      it "returns minimum bikes per station" do
         expect(Station.min_bikes_per_station).to eql(7)
       end
     end
 
-   describe ".stations_with_most_bikes" do 
-      it "returns stations with most bikes" do 
+   describe ".stations_with_most_bikes" do
+      it "returns stations with most bikes" do
         expect(Station.stations_with_most_bikes).to eql('whatever, duplicate')
       end
     end
 
-   describe ".stations_with_least_bikes" do 
-      it "returns stations with least bikes" do 
+   describe ".stations_with_least_bikes" do
+      it "returns stations with least bikes" do
         expect(Station.stations_with_least_bikes).to eql('burritos')
       end
     end
 
-   describe ".newest_station" do 
-      it "returns most recently built station" do 
+   describe ".newest_station" do
+      it "returns most recently built station" do
         expect(Station.newest_station).to eql('chillest')
       end
     end
 
-   describe ".oldest_station" do 
-      it "returns oldest station" do 
+   describe ".oldest_station" do
+      it "returns oldest station" do
         expect(Station.oldest_station).to eql('surf shack')
       end
     end
@@ -65,7 +65,7 @@ RSpec.describe Station do
     before { City.create(name: 'cool town') }
 
     it "creates a city given valid params" do
-      station = City.first.stations.create(name: 'whatever', dock_count: 27, installation_date: "2014-08-06")
+      station = City.first.stations.create(name: 'whatever', dock_count: 27, installation_date: "2014-08-06", lat: 100, lng: 200)
       expect(station).to be_valid
     end
 

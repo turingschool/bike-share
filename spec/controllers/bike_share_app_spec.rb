40e962c2dcf9327a1ff6e6935b62c5cb83742283
Adrian Lara <adrianblara@gmail.com>
2017-10-07 15:00:36 -0600

    looks for link in test for visit /stations story

--- spec/controllers/bike_share_app_spec.rb
+++ spec/controllers/bike_share_app_spec.rb
@@ -13,6 +13,11 @@ describe BikeShareApp do
 
   describe "the route '/stations'" do
     it "gets you to the view with all stations listed" do
+      Station.create({name: "San Jose Diridon Caltrain Station",
+                      dock_count: 27,
+                      city: "San Jose",
+                      installation_date: "8/6/2013"
+                      })
       visit '/stations'
 
       save_and_open_page
@@ -20,6 +25,7 @@ describe BikeShareApp do
       within("h1") do
         expect(page).to have_content("All Stations")
       end
+      has_link?("San Jose Diridon Caltrain Station")
     end
   end
 

7e2d8913ac10eb56fc34f670566c032d97537cc4
Adrian Lara <adrianblara@gmail.com>
2017-10-07 10:11:13 -0600

    debugged iteration 6 - trip model methods as well as trips-dashboard view

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -29,63 +29,69 @@ class Trip <ActiveRecord::Base
   def self.most_departed_station
     station_data = station_and_departures
     station_data.max_by do |info|
-      info.values.first
-    end.keys.first
+      info.last
+    end.first
   end
 
   def self.least_departed_station
     station_data = station_and_departures
     station_data.min_by do |name_and_count|
-      name_and_count.values.first
-    end.keys.first
+      name_and_count.last
+    end.first
   end
 
   def self.station_and_departures
-    group(:station_start_name).count
+    group(:start_station_name).count
   end
 
   def self.most_ridden_bike
     bike_data = bikes_and_uses
     bike_data.max_by do |id_and_count|
-      id_and_count.values.first
-    end.keys.first
+      id_and_count.last
+    end.first
   end
 
   def self.least_ridden_bike
-    bike_data = bike_and_uses
+    bike_data = bikes_and_uses
     bike_data.min_by do |id_and_count|
-      id_and_count.values.first
-    end.keys.first
+      id_and_count.last
+    end.first
   end
 
   def self.bikes_and_uses
     group(:bike_id).count
   end
 
+	def self.ride_count(bike_id)
+		bikes_and_uses[bike_id]
+	end
+
   def self.subscription_types_counts
-    group(:subscription_types).count
+    group(:subscription_type).count
   end
 
   def self.subscription_types_and_info
-    subscription_data = {}
+#TODO could prob use a reduce method here instead
+		subscription_data = {}
     subscription_types_counts.each do |type, count|
-      pct_info = ((count.to_f/all.count) * 100).truncate(1)
-      subscription_data[type] { count: count, pct: pct_info }
+      pct_info = ((count.to_f/all.count) * 100).truncate
+      subscription_data[type] = { count: count, pct: pct_info }
     end
+		subscription_data
   end
 
   def self.busiest_day
     day_data = days_and_trips
     day_data.max_by do |day_and_count|
-      day_and_count.values.first
-    end.keys.first
+      day_and_count.last
+    end.first
   end
 
   def self.slowest_day
     day_data = days_and_trips
     day_data.min_by do |day_and_count|
-      day_and_count.values.first
-    end.keys.first
+      day_and_count.last
+    end.first
   end
 
   def self.days_and_trips

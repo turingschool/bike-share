45b95cbc9094622f3e533891ccc50a2e6cd287b5
Adrian Lara <adrianblara@gmail.com>
2017-10-07 15:34:51 -0600

    currently, links within views are not being tested correctly

--- spec/controllers/bike_share_app_spec.rb
+++ spec/controllers/bike_share_app_spec.rb
@@ -20,12 +20,34 @@ describe BikeShareApp do
                       })
       visit '/stations'
 
-      save_and_open_page
-
       within("h1") do
         expect(page).to have_content("All Stations")
       end
+#TODO has_link is not being used yet
       has_link?("San Jose Diridon Caltrain Station")
+    end
+  end
+
+  describe "when the user clicks on the station name on the stations page" do
+    it "brings the user to a page for that individual station" do
+      Station.create({name: "San Jose Diridon Caltrain Station",
+                      dock_count: 27,
+                      city: "San Jose",
+                      installation_date: "8/6/2013"
+                      })
+      visit '/stations'
+
+      click_link("San Jose Diridon Caltrain Station")
+
+      # save_and_open_page
+
+      has_link?("All Stations")
+      within("h1") do
+        expect(page).to have_content("Diridon Caltrain")
+      end
+#TODO check that button exists
+      expect have_button('/stations/27/edit')
+      expect have_button?('/stations/27')
     end
   end
 

16525f0be8148e0664dcda30e9f4a2426e105fa6
Aaron Schreck <schreck.aaron@gmail.com>
2017-10-08 10:08:48 -0600

    Added working task model tests

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,12 +2,12 @@ require 'will_paginate'
 require 'will_paginate/active_record'
 
 class Trip <ActiveRecord::Base
-#validates_presence_of :duration,
- #                     :start_date,
-  #                    :start_station,
-   #                   :end_date,
-    #                  :end_station,
-     #                 :bike_id,
-      #                :subscription_type
+validates_presence_of  :duration,
+                       :start_date,
+                       :start_station_name,
+                       :end_date,
+                       :end_station_name,
+                       :bike_id,
+                       :subscription_type
 	self.per_page = 30
 end 

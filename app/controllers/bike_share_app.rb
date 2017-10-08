278e0be99a7b30e3bd471d52bf65657b022e2666
Aaron Schreck <schreck.aaron@gmail.com>
2017-10-08 13:30:10 -0600

    add edit/delete functionality along with individual trip view

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -12,11 +12,11 @@ class BikeShareApp < Sinatra::Base
 
   get '/stations' do
     @stations = Station.all
-    erb :'station-index'
+    erb :'station_index'
   end
 
   get '/stations/new' do
-    erb :'station-new'
+    erb :'station_new'
   end
 
   post '/stations' do
@@ -26,12 +26,12 @@ class BikeShareApp < Sinatra::Base
 
   get '/stations/:id' do
     @station = Station.find(params[:id])
-    erb :'station-show'
+    erb :'station_show'
   end
 
   get '/stations/:id/edit' do
     @station = Station.find(params[:id])
-    erb :'station-edit'
+    erb :'station_edit'
   end
 
   put '/stations/:id' do
@@ -45,19 +45,20 @@ class BikeShareApp < Sinatra::Base
     redirect '/stations'
   end
 
-  get '/station-dashboard' do
+  get '/station_dashboard' do
     @stations = Station.all
-    erb :'station-dashboard'
+    erb :'station_dashboard'
   end
 
   get '/trips' do 
     @trips = Trip.paginate(page: params[:page], per_page: 30)
-    erb :'trip-index'
+    erb :'trip_index'
   end
 
   
   get '/trips/new' do 
-		erb :'trip-new'    
+    
+    erb :'trip_new'    
   end 
   
 	post '/trips' do 
@@ -67,13 +68,13 @@ class BikeShareApp < Sinatra::Base
   
   get '/trips/:id' do
     @trip = Trip.find(params[:id])
-    erb :'trip-show'
+    erb :'trip_show'
   end 
 	
  
 	get '/trips/:id/edit' do
 		@trip = Trip.find(params[:id])
-		erb :'trip-edit'
+		erb :'trip_edit'
   end 
   
   put '/trips/:id' do 

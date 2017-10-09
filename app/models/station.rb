ba433b174119ccb2c1998086ff30fdef6ee2ebd2
Adrian Lara <adrianblara@gmail.com>
2017-10-09 15:14:22 -0600

    ride counts and most popular destination instance methods written for station

--- app/models/station.rb
+++ app/models/station.rb
@@ -32,4 +32,19 @@ class Station < ActiveRecord::Base
     where(installation_date: min).map { |station| station.name }
   end
 
+  def start_ride_count
+    Trip.where(start_station_id: station_id).count
+  end
+
+  def end_ride_count
+    Trip.where(end_station_id: station_id).count
+  end
+
+  def most_popular_destination
+    #of all trips starting at this station, where do they most
+    trips = Trip.where(start_station_id: station_id)
+    station = trips.group(:end_station_id).order("count_all DESC").limit(1).count
+    Station.find_by(station_id: station.keys.first).name
+  end
+
 end

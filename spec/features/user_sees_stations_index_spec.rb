cfd6cbbb2501bcf53d4fe245a6f149da27ed3127
Adrian Lara <adrianblara@gmail.com>
2017-10-10 12:57:42 -0600

    feature test written for user visiting station index page

--- spec/features/user_sees_stations_index_spec.rb
+++ spec/features/user_sees_stations_index_spec.rb
@@ -1,8 +1,27 @@
 describe "When a user visits '/' and clicks on 'All stations'" do
   it "the user can see the station index page containing all stations" do
-    #Setup
-    
-    #Execute
-    #Assert
+    Station.create({name: "San Jose Diridon Caltrain Station",
+                    station_id: "10",
+                    dock_count: 27,
+                    city: "San Jose",
+                    installation_date: "8/6/2013"
+                    })
+    Station.create({name: "Union Station",
+                    station_id: "21",
+                    dock_count: 31,
+                    city: "Denver",
+                    installation_date: "10/10/2017"
+                    })
+
+    visit '/'
+    click_link "All Stations"
+
+    expect(page).to have_content("All Stations")
+    expect(page).to have_content("Denver")
+    expect(page).to have_content(31)
+    expect(page.has_button?("edit")).to be(true)
+    expect(page.has_button?("delete")).to be(true)
+    expect(page.has_link?("San Jose Diridon Caltrain Station")).to be(true)
+    expect(page.has_link?("Home")).to be(true)
   end
 end

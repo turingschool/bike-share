a0fd7cb5c54c915462ff2c1388024e438cb28215
Adrian Lara <adrianblara@gmail.com>
2017-10-10 11:19:43 -0600

    post and put methods written for stations

--- spec/controllers/bike_share_app_spec.rb
+++ spec/controllers/bike_share_app_spec.rb
@@ -31,6 +31,54 @@ describe BikeShareApp do
         get '/stations/1'
       end
     end
+
+    describe "/stations/:id/edit" do
+      it "loads without error given the existence of a station with specified id in URI" do
+        Station.create({name: "San Jose Diridon Caltrain Station",
+                        station_id: "10",
+                        dock_count: 27,
+                        city: "San Jose",
+                        installation_date: "8/6/2013"
+                        })
+
+        get '/stations/1/edit'
+      end
+    end
+  end
+
+  describe 'post methods' do
+    describe "/stations" do
+      it "redirects to '/stations/' without error" do
+        post '/stations'
+
+        expect(last_response).to be_redirect
+        expect(last_response.header["Location"]).to eq("http://example.org/stations")
+      end
+    end
+  end
+
+  describe 'put methods' do
+    describe "/stations/:id" do
+      it "redirects to stations/:id without error if given correct params" do
+        Station.create({name: "San Jose Diridon Caltrain Station",
+                        station_id: "10",
+                        dock_count: 27,
+                        city: "San Jose",
+                        installation_date: "8/6/2013"
+                        })
+        put '/stations/1', params = {station: {name: "San Jose Diridon Caltrain Station",
+                                               station_id: "10",
+                                               dock_count: 1000,
+                                               city: "San Jose",
+                                               installation_date: "8/6/2013"}
+                                    }
+
+        expect(last_response).to be_redirect
+        expect(last_response.header["Location"]).to eq("http://example.org/stations/1")
+      end
+    end
   end
 
 end
+
+# require 'pry'; binding.pry

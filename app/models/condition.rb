925c2c82b8cf320a4183026860e70a06dfd4db49
Aaron Schreck <schreck.aaron@gmail.com>
2017-10-09 23:09:56 -0600

    Add really, really slow solution to iteration-9

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -2,7 +2,7 @@ require 'will_paginate'
 require 'will_paginate/active_record'
 
 class Condition < ActiveRecord::Base
-  has_many :trips, foreign_key: "start_date"
+  has_many :trips, primary_key: "date", foreign_key: "start_date"
   validates :date, uniqueness: { scope: :zip_code }
   validates_presence_of :date
 
@@ -27,18 +27,36 @@ class Condition < ActiveRecord::Base
   end
 
   def self.all_days_in_temp_range
-    temp = 0
+    temp = 30
+		condition_info = []
     while temp < 100
-      all_conditions_in_temp_range(temp, temp+10)
+			loop_info = []
+      conditions = all_conditions_in_temp_range(temp, temp+10)
+			loop_info << average_trips_per_increment(conditions)
+			loop_info << most_rides(conditions)
+			loop_info << least_rides(conditions)
+      condition_info << loop_info
+      
       temp += 10
     end
+		condition_info
   end
 
   def self.average_trips_per_increment(conditions)
-    total_trips = conditions.reduce(0) do |sum, condition|
-      sum + condition.trips.count
-    end
-    total_trips / conditions.count
+  
+    # total_trips = conditions.reduce(0) do |sum, condition|
+    #   sum + condition.trips.count
+    #end
+    
+    total_trips / conditions.count unless total_trips == 0 || conditions.count == 0
   end
 
+	def self.most_rides(conditions)
+		conditions.max_by {|condition| condition.trips.count}
+	end
+
+	def self.least_rides(conditions)
+		conditions.min_by {|condition| condition.trips.count}
+  end
+  
 end

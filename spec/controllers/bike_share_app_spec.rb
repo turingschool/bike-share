cb73ab53b3b1d3fb6d966e044f5b1762ecd6ca93
Adrian Lara <adrianblara@gmail.com>
2017-10-10 10:24:00 -0600

    controller method still wip, merge to pause work in this branch

--- spec/controllers/bike_share_app_spec.rb
+++ spec/controllers/bike_share_app_spec.rb
@@ -8,6 +8,7 @@ describe BikeShareApp do
         expect(last_response).to be_ok
       end
     end
+
     describe "/stations" do
       it "loads without error" do
         get '/stations'
@@ -15,6 +16,7 @@ describe BikeShareApp do
         expect(last_response).to be_ok
       end
     end
+
     describe "/stations/new" do
       it "loads without error" do
         get '/stations/new'
@@ -23,6 +25,20 @@ describe BikeShareApp do
       end
     end
 
+    describe "/stations/:id" do
+      it "loads without error and brings in an instance variable with all stations" do
+        Station.create({name: "San Jose Diridon Caltrain Station",
+                        station_id: "10",
+                        dock_count: 27,
+                        city: "San Jose",
+                        installation_date: "8/6/2013"
+                        })
+        get '/stations/new'
+
+        expect(last_response).to be_ok
+
+      end
+    end
   end
 
 end

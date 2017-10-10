bc5293b66fb6b9b57bb6f11ca17a40a44b2d35a4
seoulstice <jungyoungjoon@gmail.com>
2017-10-10 15:03:23 -0600

    row.delete on Trip seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,29 @@ class Seed
       row.delete(:start_station_name)
       row.delete(:end_station_name)
       Trip.create(row.to_hash)
+    end
 
+    CSV.foreach("./db/csv/weather.csv", options ) do |row|
+      next if Condition.exists?(row[:id])
+      convert_date(row, :date)
+      row.delete(:max_dew_point_f)
+      row.delete(:mean_dew_point_f)
+      row.delete(:min_dew_point_f)
+      row.delete(:max_humidity)
+      row.delete(:min_humidity)
+      row.delete(:max_sea_level_pressure_inches)
+      row.delete(:mean_sea_level_pressure_inches)
+      row.delete(:min_sea_level_pressure_inches)
+      row.delete(:max_visibility_miles)
+      row.delete(:min_visibility_miles)
+      row.delete(:max_wind_Speed_mph)
+      row.delete(:max_gust_speed_mph)
+      row.delete(:cloud_cover)
+      row.delete(:events)
+      row.delete(:wind_dir_degrees)
+      row.delete(:zip_code)
+
+      Condition.create!(row.to_hash)
     end
 
   end

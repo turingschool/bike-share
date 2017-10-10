4d7c8049c4b89e38d661062c599450f7c126cfe2
Tyjoo26 <Tyjoo21@gmail.com>
2017-10-10 15:40:00 -0600

    Feature test robust stations_index_spec.rb'

--- spec/features/stations_index_spec.rb
+++ spec/features/stations_index_spec.rb
@@ -1,7 +1,7 @@
 describe "station index" do
   before do
     Station.create(name: "San Jose Diridon Caltrain Station", dock_count: 27, city: City.create(id:1, name: "San Jose"), installation_date: '2013-08-06', lat:0, lng:0)
-  
+
     visit '/stations'
   end
 
@@ -25,4 +25,15 @@ describe "station index" do
     click_button("DELETE")
     expect(current_path).to eq("/stations")
   end
+
+  it 'user clicks show and goes to show page for specific station' do
+    click_link("New")
+    expect(current_path).to eq("/stations/new")
+  end
+
+  it 'user clicks edit, page loads to edit page for specific station' do
+    click_button("EDIT")
+    expect(current_path).to eq("/stations/1/edit")
+  end
+
 end

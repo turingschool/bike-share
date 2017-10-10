cf263d4740b4b9c3bedd049536c88ffdb85d97ce
Adrian Lara <adrianblara@gmail.com>
2017-10-09 22:23:44 -0600

    commented out all trip class methods in prep for test writing

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -23,102 +23,102 @@ class Trip <ActiveRecord::Base
 	self.per_page = 30
 
 	def self.avg_duration
-    time = average(:duration)
-    Time.at(time).utc.strftime("%H:%M:%S")
+    # time = average(:duration)
+    # Time.at(time).utc.strftime("%H:%M:%S")
   end
 
   def self.longest_ride
-    time = maximum(:duration)
-    Time.at(time).utc.strftime("%H:%M:%S")
+    # time = maximum(:duration)
+    # Time.at(time).utc.strftime("%H:%M:%S")
   end
 
   def self.shortest_ride
-    time = minimum(:duration)
-    Time.at(time).utc.strftime("%H:%M:%S")
+    # time = minimum(:duration)
+    # Time.at(time).utc.strftime("%H:%M:%S")
   end
 
   def self.most_departures
-    trip = group(:start_station_id).order("count_all DESC").limit(1).count
-    Station.find_by(station_id: trip.keys.first).name
+    # trip = group(:start_station_id).order("count_all DESC").limit(1).count
+    # Station.find_by(station_id: trip.keys.first).name
   end
 
   def self.most_arrivals
-    trip = group(:end_station_id).order("count_all DESC").limit(1).count
-    Station.find_by(station_id: trip.keys.first).name
+    # trip = group(:end_station_id).order("count_all DESC").limit(1).count
+    # Station.find_by(station_id: trip.keys.first).name
   end
 
   def self.month_year_breakdown
-    month_break = group("DATE_TRUNC('month', start_date)").count.to_a
-    sorted = month_break.sort_by { |i| i[0]}
-    sorted.map do |i|
-      [i.first.strftime("%B %Y"), i.last]
-    end
+    # month_break = group("DATE_TRUNC('month', start_date)").count.to_a
+    # sorted = month_break.sort_by { |i| i[0]}
+    # sorted.map do |i|
+    #   [i.first.strftime("%B %Y"), i.last]
+    # end
   end
 
   def self.year_breakdown
-    year_breakdown = group("DATE_TRUNC('year', start_date)").count.to_a
-    sorted = year_breakdown.sort_by { |i| i[0]}
-    sorted.map do |i|
-      [i.first.strftime("%Y"), i.last]
-    end
+    # year_breakdown = group("DATE_TRUNC('year', start_date)").count.to_a
+    # sorted = year_breakdown.sort_by { |i| i[0]}
+    # sorted.map do |i|
+    #   [i.first.strftime("%Y"), i.last]
+    # end
   end
 
   def self.station_and_departures
-    group(:start_station_name).count
+    # group(:start_station_name).count
   end
 
   def self.most_ridden_bike
-    bike_data = bikes_and_uses
-    bike_data.max_by do |id_and_count|
-      id_and_count.last
-    end.first
+    # bike_data = bikes_and_uses
+    # bike_data.max_by do |id_and_count|
+    #   id_and_count.last
+    # end.first
   end
 
   def self.least_ridden_bike
-    bike_data = bikes_and_uses
-    bike_data.min_by do |id_and_count|
-      id_and_count.last
-    end.first
+    # bike_data = bikes_and_uses
+    # bike_data.min_by do |id_and_count|
+    #   id_and_count.last
+    # end.first
   end
 
   def self.bikes_and_uses
-    group(:bike_id).count
+    # group(:bike_id).count
   end
 
 	def self.ride_count(bike_id)
-		bikes_and_uses[bike_id]
+		# bikes_and_uses[bike_id]
 	end
 
   def self.subscription_types_counts
-    group(:subscription_type).count
+    # group(:subscription_type).count
   end
 
   def self.subscription_types_and_info
 #TODO could prob use a reduce method here instead
-		subscription_data = {}
-    subscription_types_counts.each do |type, count|
-      pct_info = ((count.to_f/all.count) * 100).truncate
-      subscription_data[type] = { count: count, pct: pct_info }
-    end
-		subscription_data
+		# subscription_data = {}
+    # subscription_types_counts.each do |type, count|
+    #   pct_info = ((count.to_f/all.count) * 100).truncate
+    #   subscription_data[type] = { count: count, pct: pct_info }
+    # end
+		# subscription_data
   end
 
   def self.busiest_day
-    day_data = days_and_trips
-    day_data.max_by do |day_and_count|
-      day_and_count.last
-    end.first
+    # day_data = days_and_trips
+    # day_data.max_by do |day_and_count|
+    #   day_and_count.last
+    # end.first
   end
 
   def self.slowest_day
-    day_data = days_and_trips
-    day_data.min_by do |day_and_count|
-      day_and_count.last
-    end.first
+    # day_data = days_and_trips
+    # day_data.min_by do |day_and_count|
+    #   day_and_count.last
+    # end.first
   end
 
   def self.days_and_trips
-    group(:start_date).count
+    # group(:start_date).count
   end
 
 end

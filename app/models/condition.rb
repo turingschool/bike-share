14811c0cb9f1bf4744f7e458dfe988454bb79451
Aaron Schreck <schreck.aaron@gmail.com>
2017-10-10 13:18:06 -0600

    finished first part of iteration-9

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -8,49 +8,49 @@ class Condition < ActiveRecord::Base
 
   self.per_page = 30
 
-  def self.average_rides_high_temp
-  # isolate a range
-
-
-
-
-    # whatever_for_now = select("conditions.*, COUNT(trip_id) AS trip_count")
-    #   .joins("LEFT JOIN trips ON trips.start_date = conditions.date")
-    #   # .group(:max_temperature_f)
-    #   require 'pry'; binding.pry
-    #   average(whatever_for_now.trip_count)
-  end
-
-
-  def self.all_conditions_in_temp_range(low, high)
-    where(max_temperature_f: low..high)
-  end
-
-  def self.all_days_in_temp_range
-    temp = 30
-		condition_info = []
-    while temp < 100
-			loop_info = []
-      conditions = all_conditions_in_temp_range(temp, temp+10)
-			loop_info << average_trips_per_increment(conditions)
-			loop_info << most_rides(conditions)
-			loop_info << least_rides(conditions)
-      condition_info << loop_info
-      
-      temp += 10
-    end
-		condition_info
-  end
-
-  def self.average_trips_per_increment(conditions)
+  def self.id_by_date(date)
+    find_by(date: date).id
+  end 
   
-    # total_trips = conditions.reduce(0) do |sum, condition|
-    #   sum + condition.trips.count
-    #end
-    
-    total_trips / conditions.count unless total_trips == 0 || conditions.count == 0
-  end
+	def self.trips_in_temp_range(temp_range)
+    #this gives me a table with as many rows as there are conditions
+    #with a column that has the number of trips associated with each. 
+    #each object has a trip count on it
+    where(max_temperature_f: (temp_range...(temp_range + 10)))
+            .joins(:trips)
+            .select("conditions.*, count(trips.id) AS trip_count")
+            .group("conditions.id")
+            .order("count_id DESC").count(:id)    
+  end 
 
+#  def self.all_conditions_in_temp_range(low, high)
+ #   (max_temperature_f: low..high)
+ # end
+
+	def self.breakout(temp_range)
+		conditions_in_range = trips_in_temp_range(temp_range)
+    view_data = Hash.new(0)
+    view_data[:min] = (conditions_in_range.values.last)
+    view_data[:max] = (conditions_in_range.values.first)
+    
+    if conditions_in_range.values.count != 0
+      view_data[:avg] = conditions_in_range.values.sum/ conditions_in_range.values.count
+    else 
+      view_data[:avg] = 0
+    end 
+    view_data
+	end 
+	
+	def self.breakout_temps
+		counter = 40
+		temps = Hash.new(0)
+		while counter <= 100
+			temps.merge!(counter => breakout(counter))
+			counter += 10
+		end 
+	temps
+	end 
+  
 	def self.most_rides(conditions)
 		conditions.max_by {|condition| condition.trips.count}
 	end

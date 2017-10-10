110471ddfa3fc4d85c2f3e187c2f6985080f5597
Josi McClellan <junipertmcclellan@gmail.com>
2017-10-10 17:51:15 -0600

    Update user_visits_condition_edit_spec.rb

--- spec/features/conditions/user_visits_condition_edit_spec.rb
+++ spec/features/conditions/user_visits_condition_edit_spec.rb
@@ -1,7 +1,7 @@
 require './spec/spec_helper'
 
 feature 'When a user visits condition edit page' do
-  context 'for a condition that exists'
+  context 'for a condition that exists' do
     background do
       Condition.create! id: "1"
                         date: "8/29/2013",
@@ -60,55 +60,55 @@ feature 'When a user visits condition edit page' do
 
     context 'when user inputs valid data' do
       background do
-        fill_in "condition[date]"                  with: "4/20/2013"
-        fill_in "condition[max_temperature_f]"     with: "74.0"
-        fill_in "condition[mean_temperature_f]"    with: "68.0"
-        fill_in "condition[min_temperature_f]"     with: "61.0"
-        fill_in "condition[mean_humidity]"         with: "75.0"
-        fill_in "condition[mean_visibility_miles]" with: "10.0"
-        fill_in "condition[mean_wind_speed]'"      with: "11.0"
-        fill_in "condition[precipitation_inches]"  with: "10"
+        fill_in "condition[date]",                  with: "4/20/2013"
+        fill_in "condition[max_temperature_f]",     with: "74.0"
+        fill_in "condition[mean_temperature_f]",    with: "68.0"
+        fill_in "condition[min_temperature_f]",     with: "61.0"
+        fill_in "condition[mean_humidity]",         with: "75.0"
+        fill_in "condition[mean_visibility_miles]", with: "10.0"
+        fill_in "condition[mean_wind_speed]",       with: "11.0"
+        fill_in "condition[precipitation_inches]",  with: "10"
         click_button "submit"
       end
 
-      it 'then the user is redirected to show page with success message'
-        has_current_path?("/conditions/#{condition.id}", only_path: true)
+      it 'then the user is redirected to show page with success message' do
+        has_current_path?("/conditions/1", only_path: true)
         expect(page).to have_content(/saved/i)
       end
     end
 
     context 'when user inputs invalid data' do
       background do
-        fill_in "condition[date]"                  with: "4/20/2013"
-        fill_in "condition[max_temperature_f]"     with: "hot"
-        fill_in "condition[mean_temperature_f]"    with: "68.0"
-        fill_in "condition[min_temperature_f]"     with: "61.0"
-        fill_in "condition[mean_humidity]"         with: "75.0"
-        fill_in "condition[mean_visibility_miles]" with: "10.0"
-        fill_in "condition[mean_wind_speed]'"      with: "11.0"
-        fill_in "condition[precipitation_inches]"  with: "0"
+        fill_in "condition[date]",                  with: "4/20/2013"
+        fill_in "condition[max_temperature_f]",     with: "hot"
+        fill_in "condition[mean_temperature_f]",    with: "68.0"
+        fill_in "condition[min_temperature_f]",     with: "61.0"
+        fill_in "condition[mean_humidity]",         with: "75.0"
+        fill_in "condition[mean_visibility_miles]", with: "10.0"
+        fill_in "condition[mean_wind_speed]",      with: "11.0"
+        fill_in "condition[precipitation_inches]",  with: "0"
         click_button "submit"
       end
 
-      it 'then user is redirected to edit page with error flag on invalid data field'
-        has_current_path?("/conditions/#{condition.id}", only_path: true)
+      it 'then user stays on page and sees error flag on invalid data field' do
+        has_current_path?("/conditions/1", only_path: true)
         expect(page).to have_content(/error/i)
       end
     end
 
-  context 'for a condition that does not exist'
+  context 'for a condition that does not exist' do
     it 'then the user is redirected to 404 error message'
-      visit ("/conditions/#{condition}")
+      visit ("/conditions/0")
       expect(page).to have_content(/not found/i)
     end
   end
 
-  context 'when user clicks on delete button'
+  context 'when user clicks on delete button' do
     background do
       click_button 'delete'
     end
 
-    it 'then user is redirected to index page'
+    it 'then user is redirected to index page' do
       has_current_path?("/conditions", only_path: true)
       expect(page).to have_content(/delete successful/i)
     end

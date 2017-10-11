8ae98605033ebdfc34892eedd09c0f8089becd1e
seoulstice <jungyoungjoon@gmail.com>
2017-10-11 14:18:10 -0600

    condition_model_spec test written

--- spec/models/condition_spec.rb
+++ spec/models/condition_spec.rb
@@ -4,6 +4,11 @@ require './spec/spec_helper'
 RSpec.describe Condition do
   before{ Seed.seed_conditions }
 
+  it 'returns breakout incremented by chunk size'
+    expected = Condition.breakout(:mean_temperature_f, 10)
+    expect(expected).to eq "UNKNOWN"
+  end
+
   it 'returns temperature breakout in 10 degree increments'
     expected = Condition.temperature_breakout
     expect(expected).to eq "UNKNOWN"

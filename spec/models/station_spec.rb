cd873a83134d038bafaf95980aaeb17d5d49ba2c
Tim Tyrrell <timomitchel23@gmail.com>
2017-11-28 19:45:08 -0700

    validations passing

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -2,6 +2,22 @@ RSpec.describe Station do
   describe "Validations" do 
     it "is invalid without a name" do 
       station = Station.new(dock_count: 2, city: "SF", installation_date: "10-20-2013")
+
+      expect(station).to be_invalid
+    end 
+    it "is invalid without a dock_count" do 
+      station = Station.new(name: "Mission", city: "SF", installation_date: "10-20-2013")
+      
+      expect(station).to be_invalid
+    end 
+    it "is invalid without a city" do 
+      station = Station.new(name: "Mission", dock_count: 5, installation_date: "10-20-2013")
+      
+      expect(station).to be_invalid
+    end 
+     it "is invalid without a installation_date" do 
+      station = Station.new(name: "Mission", dock_count: 5, city: "SF")
+      
       expect(station).to be_invalid
     end 
   end

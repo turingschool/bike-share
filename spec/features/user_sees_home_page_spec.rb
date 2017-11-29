937451146183ceb8c8a3cd1eed48d9be2fd74491
Tyjoo26 <Tyjoo21@gmail.com>
2017-11-29 14:54:39 -0700

    Build two feature test, test passes

--- spec/features/user_sees_home_page_spec.rb
+++ spec/features/user_sees_home_page_spec.rb
@@ -2,9 +2,18 @@ require 'spec_helper'
 
 describe "As a user, when I visit the home page" do
   before :each do
-
+    visit '/'
   end
-  context "I see Bike Share title" do
 
+  it "I see Bike Share title" do
+    expect(page.status_code).to eq(200)
+
+    expect(page).to have_content("Bike Share")
+  end
+
+  it "I should also see a link to Station index" do
+    click_link("Station Index")
+
+    expect(current_path).to eq("/stations/index")
   end
 end

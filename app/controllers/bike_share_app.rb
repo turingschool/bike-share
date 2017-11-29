dd5c8dffcf97fe107fcdff0efdaa16aa29053822
katyjane8 <katy.jane8@gmail.com>
2017-11-29 16:50:29 -0700

    add get edit and put edit

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -9,4 +9,24 @@ class BikeShareApp < Sinatra::Base
 
     erb :"/stations/index"
   end
+
+  get '/stations/:id' do
+    @station = Station.find(params[:id])
+
+    erb :"/stations/show"
+  end
+
+  get '/stations/:id/edit' do
+    @station = Station.find(params[:id])
+
+    erb :"/stations/edit"
+  end
+
+  put '/stations/:id' do |id|
+    Station.update(id.to_i, params[:station])
+    redirect '/stations/id'
+  end
+
+  # get '/station/:id/delete'
+  # end
 end

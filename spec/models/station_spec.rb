c06d1d53a22638613d8d0e44cb91338d186e5b0d
abarnes26 <abarnes26@yahoo.com>
2017-11-30 14:00:36 -0700

    newest install date test passing

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -76,6 +76,14 @@ RSpec.describe Station do
       expect(Station.with_fewest_bikes.first.name).to eql("MLK Library")
     end
 
+    it "can find newest install date" do
+      Station.create(name: "San Jose Diridon Caltrain Station", dock_count: 27, city: "San Jose", installation_date: "1/8/2013")
+      Station.create(name: "MLK Library", dock_count: 13, city: "San Jose", installation_date: "5/8/2013")
+      Station.create(name: "Clay at Battery", dock_count: 15, city: "San Francisco", installation_date: "12/8/2013")
+
+      expect((Station.newest_install_date).strftime("%d/%m/%Y")).to eql("12/08/2013")
+    end
+
   end
 
 end

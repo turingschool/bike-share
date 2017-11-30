bcb9f2127ee6da70802eaa77bd3b073e8cc9c3cb
Tim Tyrrell <timomitchel23@gmail.com>
2017-11-29 19:17:45 -0700

    iteration three functional

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class BikeShareApp < Sinatra::Base
 
   get '/' do
@@ -17,6 +18,10 @@ class BikeShareApp < Sinatra::Base
     @station = Station.find(params[:id])
     erb :"stations/show"
   end
+  
+  get '/stations-dashboard' do
+    erb :"stations/dashboard"
+  end
 
   post '/stations' do
     Station.create(params[:station])
@@ -30,7 +35,7 @@ class BikeShareApp < Sinatra::Base
 
   set :method_override, true
   put '/stations/:id' do |id|
-    Station.update(id.to_i, params[:station])
+   Station.update(id.to_i, params[:station])
     redirect "/stations/#{id}"
   end
 

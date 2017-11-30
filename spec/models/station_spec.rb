c18a1587eb558fd5532741cf794dba3a73e51263
abarnes26 <abarnes26@yahoo.com>
2017-11-30 13:39:20 -0700

    station with most bikes test working

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -25,6 +25,33 @@ RSpec.describe Station do
 
       expect(station).to be_invalid
     end
+
+    it "is valid with all the requisite information" do
+      Station.create(name: "San Jose Diridon Caltrain Station", dock_count: 27, city: "San Jose", installation_date: "1/8/2013")
+      Station.create(name: "MLK Library", dock_count: 15, city: "San Jose", installation_date: "5/8/2013")
+      Station.create(name: "Clay at Battery", dock_count: 15, city: "San Francisco", installation_date: "12/8/2013")
+
+      expect(Station.all.count).to eql(3)
+    end
+  end
+
+  describe "class methods" do
+    it "can find average bikes" do
+      Station.create(name: "San Jose Diridon Caltrain Station", dock_count: 27, city: "San Jose", installation_date: "1/8/2013")
+      Station.create(name: "MLK Library", dock_count: 15, city: "San Jose", installation_date: "5/8/2013")
+      Station.create(name: "Clay at Battery", dock_count: 15, city: "San Francisco", installation_date: "12/8/2013")
+
+      expect(Station.average_bikes_per_station).to eql(19)
+    end
+
+    it "can find most bikes" do
+      Station.create(name: "San Jose Diridon Caltrain Station", dock_count: 27, city: "San Jose", installation_date: "1/8/2013")
+      Station.create(name: "MLK Library", dock_count: 15, city: "San Jose", installation_date: "5/8/2013")
+      Station.create(name: "Clay at Battery", dock_count: 15, city: "San Francisco", installation_date: "12/8/2013")
+
+      expect(Station.with_most_bikes.first.name).to eql("San Jose Diridon Caltrain Station")
+    end
+
   end
 
 end

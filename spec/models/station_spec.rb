7afb414b24d27bc2afa0b654e28f01fde05b94b4
abarnes26 <abarnes26@yahoo.com>
2017-11-30 14:04:36 -0700

    find oldest station test passing

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -100,6 +100,14 @@ RSpec.describe Station do
       expect((Station.oldest_install_date).strftime("%d/%m/%Y")).to eql("01/08/2013")
     end
 
+    it "can find oldest station" do
+      Station.create(name: "San Jose Diridon Caltrain Station", dock_count: 27, city: "San Jose", installation_date: "1/8/2013")
+      Station.create(name: "MLK Library", dock_count: 13, city: "San Jose", installation_date: "5/8/2013")
+      Station.create(name: "Clay at Battery", dock_count: 15, city: "San Francisco", installation_date: "12/8/2013")
+
+      expect(Station.oldest_station.first.name).to eql("San Jose Diridon Caltrain Station")
+    end
+
   end
 
 end

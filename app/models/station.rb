e8a12f20b19e2cd8406f6dec72f267490a7cad09
Tyjoo26 <Tyjoo21@gmail.com>
2017-12-01 16:57:25 -0700

    Finalize class method model test and validation tests, all green

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,5 +1,9 @@
 class Station < ActiveRecord::Base
 
+  validates_presence_of :name,
+                        :dock_count,
+                        :city,
+                        :installation_date
 
   def self.station_count
     count
--- spec/models/station/station_spec.rb
+++ spec/models/station/station_spec.rb
@@ -1,19 +1,19 @@
 RSpec.describe Station do
   context "Validations" do
     it "is invalid without name" do
-      station = Station.create(dock_count: 20, city: "San Francisco", installation_date: "2013/08/06")
+      station = Station.create(dock_count: 20, city: "San Francisco", installation_date: "2013-08-06")
 
       expect(station).to be_invalid
     end
 
     it "is invalid without dock count" do
-      station = Station.create(name: "Embarcadero", city: "San Francisco", installation_date: "2013/08/06")
+      station = Station.create(name: "Embarcadero", city: "San Francisco", installation_date: "2013-08-06")
 
       expect(station).to be_invalid
     end
 
     it "is invalid without city" do
-      station = Station.create(name: "Embarcadero", dock_count: 20, installation_date: "2013/08/13")
+      station = Station.create(name: "Embarcadero", dock_count: 20, installation_date: "2013-08-13")
 
       expect(station).to be_invalid
     end

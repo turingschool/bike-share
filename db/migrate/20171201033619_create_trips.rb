4dd9239d5eb1ad347229e8454efee971d7d4dd0a
kelleyjenkins <ckjenkins3@gmail.com>
2017-11-30 20:43:25 -0700

    migrate trips table

---
+++ db/migrate/20171201033619_create_trips.rb
@@ -0,0 +1,14 @@
+class CreateTrips < ActiveRecord::Migration[5.1]
+  def change
+      create_table   :trips do |trip|
+        trip.text         :duration
+        trip.date         :start_date
+        trip.text         :start_station
+        trip.date         :end_date
+        trip.text         :end_station
+        trip.integer      :bike_id
+        trip.text         :subscription
+        trip.integer      :zip_code
+      end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128234150) do
+ActiveRecord::Schema.define(version: 20171201033619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,17 @@ ActiveRecord::Schema.define(version: 20171128234150) do
     t.integer "dock_count"
     t.text "city"
     t.date "installation_date"
+  end
+
+  create_table "trips", force: :cascade do |t|
+    t.text "duration"
+    t.date "start_date"
+    t.text "start_station"
+    t.date "end_date"
+    t.text "end_station"
+    t.integer "bike_id"
+    t.text "subscription"
+    t.integer "zip_code"
   end
 
 end

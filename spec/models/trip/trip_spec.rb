83c7917075ca74918dfe26913d1a2fea56bbcc11
katyjane8 <katy.jane8@gmail.com>
2017-12-01 17:55:54 -0700

    build out template for trip spec

---
+++ spec/models/trip/trip_spec.rb
@@ -0,0 +1,24 @@
+RSpec.describe "trip model test" do
+  describe "Validations" do
+    it "can't save trip without duration" do
+    end
+
+    it "can't save trip without Start Date" do
+    end
+
+    it "can't save trip without Start Station" do
+    end
+
+    it "can't save trip without end date" do
+    end
+
+    it "can't save trip without end station" do
+    end
+
+    it "can't save trip without bike id" do
+    end
+
+    it "can't save trip without Subscription type" do
+    end
+  end
+end

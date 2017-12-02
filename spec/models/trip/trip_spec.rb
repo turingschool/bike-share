2fc6d7f9e4b249b88ea860908d27a82f450a4410
katyjane8 <katy.jane8@gmail.com>
2017-12-02 13:01:32 -0700

    set up model validation test for trips

--- spec/models/trip/trip_spec.rb
+++ spec/models/trip/trip_spec.rb
@@ -1,24 +1,65 @@
 RSpec.describe "trip model test" do
   describe "Validations" do
     it "can't save trip without duration" do
+      Trip.create(start_date: "2013-08-29", start_station_id: 4, 
+      start_station_name: "Embarcadero", end_date: "2013-08-29", end_station_id: 5,
+      end_station_name: "Market St", bike_id: 520, subscription_type: "Subscriber",
+      zip_code: 94127)
     end
 
     it "can't save trip without Start Date" do
+      Trip.create(duration: 63, start_station_id: 4,
+      start_station_name: "Embarcadero", end_date: "2013-08-29", end_station_id: 5,
+      end_station_name: "Market St", bike_id: 520, subscription_type: "Subscriber",
+      zip_code: 94127)
     end
 
-    it "can't save trip without Start Station" do
+    it "can't save trip without Start Station id" do
+      Trip.create(duration: 63, start_date: "2013-08-29",
+      start_station_name: "Embarcadero", end_date: "2013-08-29", end_station_id: 5,
+      end_station_name: "Market St", bike_id: 520, subscription_type: "Subscriber",
+      zip_code: 94127)
+    end
+
+    it "can't save trip without Start Station name" do
+      Trip.create(duration: 63, start_date: "2013-08-29", start_station_id: 4,
+      end_date: "2013-08-29", end_station_id: 5,
+      end_station_name: "Market St", bike_id: 520, subscription_type: "Subscriber",
+      zip_code: 94127)
     end
 
     it "can't save trip without end date" do
+      Trip.create(duration: 63, start_date: "2013-08-29", start_station_id: 4,
+      start_station_name: "Embarcadero", end_station_id: 5,
+      end_station_name: "Market St", bike_id: 520, subscription_type: "Subscriber",
+      zip_code: 94127)
     end
 
-    it "can't save trip without end station" do
+    it "can't save trip without end station id" do
+      Trip.create(duration: 63, start_date: "2013-08-29", start_station_id: 4,
+      start_station_name: "Embarcadero", end_date: "2013-08-29",
+      end_station_name: "Market St", bike_id: 520, subscription_type: "Subscriber",
+      zip_code: 94127)
+    end
+
+    it "can't save trip without end station name" do
+      Trip.create(duration: 63, start_date: "2013-08-29", start_station_id: 4,
+      start_station_name: "Embarcadero", end_date: "2013-08-29", end_station_id: 5,
+      bike_id: 520, subscription_type: "Subscriber",
+      zip_code: 94127)
     end
 
     it "can't save trip without bike id" do
+      Trip.create(duration: 63, start_date: "2013-08-29", start_station_id: 4,
+      start_station_name: "Embarcadero", end_date: "2013-08-29", end_station_id: 5,
+      end_station_name: "Market St", subscription_type: "Subscriber",
+      zip_code: 94127)
     end
 
     it "can't save trip without Subscription type" do
+      Trip.create(duration: 63, start_date: "2013-08-29", start_station_id: 4,
+      start_station_name: "Embarcadero", end_date: "2013-08-29", end_station_id: 5,
+      end_station_name: "Market St", bike_id: 520, zip_code: 94127)
     end
   end
 end

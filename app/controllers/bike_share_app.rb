f4a075e1c7527d240feff6f62aaa082767c34abc
katyjane8 <katy.jane8@gmail.com>
2017-12-03 10:26:59 -0700

    fixed one broken test - all feature tests passing in conditions

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -95,4 +95,38 @@ class BikeShareApp < Sinatra::Base
 
     erb :"/conditions/index"
   end
+
+  get '/conditions/new' do
+    erb :"/conditions/new"
+  end
+
+  post '/conditions' do
+    condition = Condition.create(params[:condition])
+
+    redirect "/conditions/#{condition.id}"
+  end
+
+  get '/conditions/:id' do
+    @trip = Condition.find(params[:id])
+
+    erb :"/conditions/show"
+  end
+
+  get '/conditions/:id/edit' do
+    @condition = Condition.find(params[:id])
+
+    erb :"/conditions/edit"
+  end
+
+  put '/conditions/:id' do |id|
+    Condition.update(id.to_i, params[:condition])
+
+    redirect "/conditions/#{id}"
+  end
+
+  delete '/conditions/:id' do |id|
+    Condition.destroy(id.to_i)
+
+    redirect "/conditions"
+  end
 end

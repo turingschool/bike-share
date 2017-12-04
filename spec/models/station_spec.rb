5b8535e86316ab4938cbae600cb134b660ef033b
Tim Tyrrell <timomitchel23@gmail.com>
2017-12-04 08:46:35 -0700

    oldest station

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -70,5 +70,19 @@ RSpec.describe Station do
       
       expect(Station.oldest_station_date).to eq(Date.parse("2013-08-06"))
     end 
+    it "finds most recent station" do
+       Station.create(name: "San Jose Civic Center", dock_count: 15, city: "San Jose", installation_date: "2013-08-06")
+      Station.create(name: "Timo", dock_count: 10, city: "SF", installation_date: "2014-04-09")
+      Station.create(name: "Lee", dock_count: 5, city: "SD", installation_date: "2015-06-09")
+      
+      expect(Station.most_recent_station).to eq("Lee")
+    end 
+    it "finds oldest station" do
+       Station.create(name: "San Jose Civic Center", dock_count: 15, city: "San Jose", installation_date: "2013-08-06")
+      Station.create(name: "Timo", dock_count: 10, city: "SF", installation_date: "2014-04-09")
+      Station.create(name: "Lee", dock_count: 5, city: "SD", installation_date: "2015-06-09")
+      
+      expect(Station.oldest_station).to eq("San Jose Civic Center")
+    end 
   end
 end

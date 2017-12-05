5a4551c113e8172cace9bb73a7444b4622fee187
abarnes26 <abarnes26@yahoo.com>
2017-12-05 11:07:03 -0700

    still reolving conflicts

--- db/migrate/20171204213153_create_conditions.rb
+++ db/migrate/20171204213153_create_conditions.rb
@@ -1,6 +1,5 @@
 class CreateConditions < ActiveRecord::Migration[5.1]
   def change
-<<<<<<< HEAD
     create_table   :conditions do |t|
     t.date         :date
     t.integer      :max_temperature
@@ -13,20 +12,4 @@ class CreateConditions < ActiveRecord::Migration[5.1]
     end
   end
 
- 
-=======
-      create_table   :conditions do |t|
-      t.integer      :duration
-      t.date         :start_date
-      t.text         :start_station_name
-      t.integer      :start_station_id
-      t.date         :end_date
-      t.text         :end_station_name
-      t.integer      :end_station_id
-      t.integer      :bike_id
-      t.text         :subscription
-      t.integer      :zip_code
-    end
-  end
->>>>>>> it6
 end

333253b1790c5c2df7c41b4ab3e934e8324f99a2
abarnes26 <abarnes26@yahoo.com>
2017-12-04 18:05:07 -0700

    most frequent origination station test passing

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -208,5 +208,14 @@ RSpec.describe Trip do
 
       expect(Trip.most_frequent_destination("Golden Gate at Polk")).to eql("Adobe on Almaden")
     end
+
+    it "can find the most frequent origination by station" do
+      Trip.create(duration: 40, start_date: "14/9/2013 18:54", start_station_name: "Post at Kearney", end_date: "29/8/2013 18:56", end_station_name: "Golden Gate at Polk", bike_id: 527, subscription: "Subscriber", zip_code: 94109)
+      Trip.create(duration: 109, start_date: "28/8/2013 13:25", start_station_name: "Golden Gate at Polk", end_date: "29/8/2013 13:27", end_station_name: "Adobe on Almaden", bike_id: 679, subscription: "Subscriber", zip_code: 95112)
+      Trip.create(duration: 138, start_date: "29/8/2013 16:57", start_station_name: "Golden Gate at Polk", end_date: "29/8/2013 16:59", end_station_name: "Adobe on Almaden", bike_id: 679, subscription: "Customer", zip_code: 94117)
+
+      expect(Trip.most_frequent_origination_station("Adobe on Almaden")).to eql("Golden Gate at Polk")
+    end
+
   end
 end

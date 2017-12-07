7d59a5d0a51a50860666f17324a09a2496c175d7
abarnes26 <abarnes26@yahoo.com>
2017-12-06 21:07:25 -0700

    fixing tests, 3 remaining

--- spec/features/visitor_submits_a_new_trip_spec.rb
+++ spec/features/visitor_submits_a_new_trip_spec.rb
@@ -3,13 +3,15 @@ require 'spec_helper'
 describe "when a visitor presses submit on new trip" do
   it 'visitor sees trip page' do
 
+  Station.create(name: "MLK Library", dock_count: 13, city: "San Jose", installation_date: "5/8/2013")
+
   visit '/trips/new'
 
   fill_in 'trip[duration]', with: 63
   fill_in 'trip[start_date]', with: "30/8/2013"
-  fill_in 'trip[start_station_id]', with: 66
+  fill_in 'trip[start_station_id]', with: 1
   fill_in 'trip[end_date]', with: '2013-08-29'
-  fill_in 'trip[end_station_id]', with: 66
+  fill_in 'trip[end_station_id]', with: 1
   fill_in 'trip[bike_id]', with: 379
   fill_in 'trip[zip_code]', with: 38046
 
